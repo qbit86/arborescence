59d809531928963098e8725a0b6334c62fd5bda1
Viktor T <viktor.t***@gmail.com>
2017-08-05 03:50:49 +0300

    Added comments.

--- src/Ubiquitous.Abstractions/IGraph.cs
+++ src/Ubiquitous.Abstractions/IGraph.cs
@@ -1,5 +1,11 @@
 ﻿namespace Ubiquitous
 {
+    /// <summary>
+    /// Represents a generic graph with defining function.
+    /// </summary>
+    /// <typeparam name="TVertexKey">The type of vertex descriptors.</typeparam>
+    /// <typeparam name="TEdgeKey">The type of edge descriptors.</typeparam>
+    /// <typeparam name="TEdgeValue">The type of edge values.</typeparam>
     interface IGraph<TVertexKey, TEdgeKey, out TEdgeValue>
         where TEdgeValue : IEdgeValue<TVertexKey>
     {
