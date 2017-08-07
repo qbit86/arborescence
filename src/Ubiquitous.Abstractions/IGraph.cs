dd832475cb1256f1d1f52f0cb5a60fe91d56cef0
Viktor T <viktor.t***@gmail.com>
2017-08-07 03:02:02 +0300

    Refactored abstractions.

--- src/Ubiquitous.Abstractions/IGraph.cs
+++ src/Ubiquitous.Abstractions/IGraph.cs
@@ -5,10 +5,12 @@
     /// </summary>
     /// <typeparam name="TVertexKey">The type of vertex descriptors.</typeparam>
     /// <typeparam name="TEdgeKey">The type of edge descriptors.</typeparam>
+    /// <typeparam name="TVertexValue">The type of vertex values.</typeparam>
     /// <typeparam name="TEdgeValue">The type of edge values.</typeparam>
-    interface IGraph<TVertexKey, TEdgeKey, out TEdgeValue>
+    interface IGraph<TVertexKey, TEdgeKey, out TVertexValue, out TEdgeValue>
         where TEdgeValue : IEdgeValue<TVertexKey>
     {
+        TVertexValue GetVertexValue(TVertexKey vertex);
         TEdgeValue GetEdgeValue(TEdgeKey edge);
     }
 }
