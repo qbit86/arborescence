a1e6e82f7b0505a35263bda95a97f7d4d69aaff9
Viktor T <viktor.t***@gmail.com>
2017-08-30 23:32:22 +0300

    Moved `IndexedAdjacencyListGraphInstance` to separate file.

--- src/Ubiquitous.Models/IndexedAdjacencyListGraph.cs
+++ src/Ubiquitous.Models/IndexedAdjacencyListGraph.cs
@@ -70,15 +70,4 @@
 
         public override int GetHashCode() => Endpoints.GetHashCode() ^ OutEdges.GetHashCode();
     }
-
-
-    public struct IndexedAdjacencyListGraphInstance : IGraphConcept<
-        IndexedAdjacencyListGraph, int, int, IEnumerable<int>, SourceTargetPair<int>>
-    {
-        public bool TryGetEdgeData(IndexedAdjacencyListGraph graph, int edge, out SourceTargetPair<int> edgeData)
-            => graph.TryGetEndpoints(edge, out edgeData);
-
-        public bool TryGetVertexData(IndexedAdjacencyListGraph graph, int vertex, out IEnumerable<int> vertexData)
-            => graph.TryGetOutEdges(vertex, out vertexData);
-    }
 }
---
+++ src/Ubiquitous.Models/IndexedAdjacencyListGraphInstance.cs
@@ -0,0 +1,14 @@
+﻿namespace Ubiquitous
+{
+    using System.Collections.Generic;
+
+    public struct IndexedAdjacencyListGraphInstance : IGraphConcept<
+        IndexedAdjacencyListGraph, int, int, IEnumerable<int>, SourceTargetPair<int>>
+    {
+        public bool TryGetEdgeData(IndexedAdjacencyListGraph graph, int edge, out SourceTargetPair<int> edgeData)
+            => graph.TryGetEndpoints(edge, out edgeData);
+
+        public bool TryGetVertexData(IndexedAdjacencyListGraph graph, int vertex, out IEnumerable<int> vertexData)
+            => graph.TryGetOutEdges(vertex, out vertexData);
+    }
+}
