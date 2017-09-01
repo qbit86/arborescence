2d26a843603a22107fad37f56feb440c3994aec8
Viktor T <viktor.t***@gmail.com>
2017-09-02 02:29:01 +0300

    Added items to enum `StepKind`.

--- src/Ubiquitous.Traversal/StepKind.cs
+++ src/Ubiquitous.Traversal/StepKind.cs
@@ -2,5 +2,16 @@
 {
     public enum StepKind
     {
+        None = 0,
+        StartVertex,
+        DiscoverVertex,
+        ExamineVertex,
+        FinishVertex,
+        ExamineEdge,
+        TreeEdge,
+        BackEdge,
+        ForwardOrCrossEdge,
+        NonTreeEdge,
+        FinishEdge,
     }
 }
