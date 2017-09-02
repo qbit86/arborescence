38040e8ea71542bed752ea794132c205026c2b09
Viktor T <viktor.t***@gmail.com>
2017-09-02 15:41:11 +0300

    Added `IIncidenceVertexConcept<...>`.

--- src/Ubiquitous.Abstractions/IIncidenceVertexConcept.cs
+++ src/Ubiquitous.Abstractions/IIncidenceVertexConcept.cs
@@ -1,5 +1,10 @@
 ﻿namespace Ubiquitous
 {
+    public interface IIncidenceVertexConcept<TGraph, TVertex, TEdges>
+    {
+        bool TryGetOutEdges(TGraph graph, TVertex vertex, out TEdges edges);
+    }
+
     /// <summary>Incidence concept for the type of vertex associated data.</summary>
     /// <typeparam name="TVertexData">The type of vertex associated data.</typeparam>
     /// <typeparam name="TEdges">The type of edges sequence.</typeparam>
