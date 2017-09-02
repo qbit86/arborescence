227496b162cc3ca7cb8a3fb72b0f94cdc0135627
Viktor T <viktor.t***@gmail.com>
2017-09-02 15:39:12 +0300

    Added `IEdgeConcept<...>`.

--- src/Ubiquitous.Abstractions/IEdgeConcept.cs
+++ src/Ubiquitous.Abstractions/IEdgeConcept.cs
@@ -1,5 +1,11 @@
 ﻿namespace Ubiquitous
 {
+    public interface IEdgeConcept<TGraph, TVertex, TEdge>
+    {
+        bool TryGetSource(TGraph graph, TEdge edge, out TVertex source);
+        bool TryGetTarget(TGraph graph, TEdge edge, out TVertex target);
+    }
+
     /// <summary>Default concept for the type of edge values.</summary>
     /// <typeparam name="TVertex">The type for vertex representative objects.</typeparam>
     /// <typeparam name="TEdgeData">The type of edge associated data.</typeparam>
