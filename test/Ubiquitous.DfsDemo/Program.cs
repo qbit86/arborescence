19004051aaeaca1101d9db92dccf018e78b79252
Viktor T <viktor.t***@gmail.com>
2017-09-07 03:57:55 +0300

    Fixed target generating.

--- test/Ubiquitous.DfsDemo/Program.cs
+++ test/Ubiquitous.DfsDemo/Program.cs
@@ -7,13 +7,13 @@
         static void Main(string[] args)
         {
             const int vertexCount = 10;
-            var builder = new IndexedAdjacencyListGraphBuilder(vertexCount);
             int edgeCount = (int)Math.Ceiling(Math.Pow(vertexCount, 1.618));
+            var builder = new IndexedAdjacencyListGraphBuilder(vertexCount);
             var prng = new Random(1729);
             for (int e = 0; e != edgeCount; ++e)
             {
                 int source = prng.Next(vertexCount);
-                int target = prng.Next(edgeCount);
+                int target = prng.Next(vertexCount);
                 builder.Add(SourceTargetPair.Create(source, target));
             }
             IndexedAdjacencyListGraph graph = builder.ToIndexedAdjacencyListGraph();
