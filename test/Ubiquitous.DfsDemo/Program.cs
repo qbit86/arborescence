bcb945df4daa346c0960480418a2a30501da64ef
Viktor T <viktor.t***@gmail.com>
2017-09-07 03:32:08 +0300

    Added project `Ubiquitous.DfsDemo`.

---
+++ test/Ubiquitous.DfsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ubiquitous
+{
+    using System;
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
