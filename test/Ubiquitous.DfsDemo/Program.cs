bd467c2cf2650e9635249ed9d4278c8bc00ddd38
Viktor T <viktor.t***@gmail.com>
2017-09-10 14:01:07 +0300

    Added `SerializeGraphByEdges(...)` stub.

--- test/Ubiquitous.DfsDemo/Program.cs
+++ test/Ubiquitous.DfsDemo/Program.cs
@@ -1,24 +1,55 @@
 ﻿namespace Ubiquitous
 {
     using System;
+    using System.IO;
+    using static System.Diagnostics.Debug;
 
     internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             const int vertexCount = 10;
             int edgeCount = (int)Math.Ceiling(Math.Pow(vertexCount, 1.618));
+
+            Console.WriteLine($"{nameof(vertexCount)}: {vertexCount}, {nameof(edgeCount)}: {edgeCount}");
+
             var builder = new IndexedAdjacencyListGraphBuilder(vertexCount);
             var prng = new Random(1729);
-            for (int e = 0; e != edgeCount; ++e)
+
+            // Making sure that each vertex has at least one nontrivial out-edge.
+            for (int v = 0; v < vertexCount; ++v)
+            {
+                int source = v;
+                int target = (v + 1 + prng.Next(vertexCount - 1)) % vertexCount;
+                builder.Add(SourceTargetPair.Create(source, target));
+            }
+
+            // Adding the rest of vertices.
+            for (int e = vertexCount; e < edgeCount; ++e)
             {
                 int source = prng.Next(vertexCount);
                 int target = prng.Next(vertexCount);
                 builder.Add(SourceTargetPair.Create(source, target));
             }
-            IndexedAdjacencyListGraph graph = builder.ToIndexedAdjacencyListGraph();
 
-            Console.WriteLine($"{nameof(vertexCount)}: {vertexCount}, {nameof(edgeCount)}: {edgeCount}");
+            IndexedAdjacencyListGraph graph = builder.ToIndexedAdjacencyListGraph();
+            SerializeGraphByEdges(graph, "Graph", Console.Out);
+        }
+
+        private static void SerializeGraphByEdges(IndexedAdjacencyListGraph graph, string graphName, TextWriter textWriter)
+        {
+            Assert(graphName != null);
+            Assert(textWriter != null);
+
+            textWriter.WriteLine($"digraph {graphName}{Environment.NewLine}{{");
+            try
+            {
+                // TODO:
+            }
+            finally
+            {
+                textWriter.WriteLine("}");
+            }
         }
     }
 }
