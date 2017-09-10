a2e593e62c47a2a144c3df86ed947893f1c901cd
Viktor T <viktor.t***@gmail.com>
2017-09-10 14:30:02 +0300

    Added struct `ColorMapFactoryInstance`.

--- test/Ubiquitous.DfsDemo/Program.cs
+++ test/Ubiquitous.DfsDemo/Program.cs
@@ -1,8 +1,21 @@
 ﻿namespace Ubiquitous
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using static System.Diagnostics.Debug;
+
+    internal struct ColorMapFactoryInstance : IFactoryConcept<IndexedAdjacencyListGraph, IndexedDictionary<Color, Color[]>>
+    {
+        public IndexedDictionary<Color, Color[]> Acquire(IndexedAdjacencyListGraph graph)
+        {
+            return new IndexedDictionary<Color, Color[]>(new Color[graph.VertexCount]);
+        }
+
+        public void Release(IndexedAdjacencyListGraph graph, IndexedDictionary<Color, Color[]> value)
+        {
+        }
+    }
 
     internal static class Program
     {
@@ -34,6 +47,18 @@
 
             IndexedAdjacencyListGraph graph = builder.ToIndexedAdjacencyListGraph();
             SerializeGraphByEdges(graph, "Graph", Console.Out);
+
+            var dfs = new Dfs<IndexedAdjacencyListGraph, int, int, IEnumerable<int>, IndexedAdjacencyListGraphInstance, IndexedAdjacencyListGraphInstance>();
+            var steps = dfs.TraverseRecursively<IndexedDictionary<Color, Color[]>, ColorMapFactoryInstance>(graph, 0);
+
+            var edgeKinds = new StepKind[graph.EdgeCount];
+            foreach (var step in steps)
+            {
+                if (Step.IsEdge(step.Kind))
+                    edgeKinds[step.Edge] = step.Kind;
+            }
+
+            // TODO: Serialize with edge kinds.
         }
 
         private static void SerializeGraphByEdges(IndexedAdjacencyListGraph graph, string graphName, TextWriter textWriter)
@@ -41,10 +66,17 @@
             Assert(graphName != null);
             Assert(textWriter != null);
 
-            textWriter.WriteLine($"digraph {graphName}{Environment.NewLine}{{");
+            textWriter.WriteLine($"digraph \"{graphName}\"{Environment.NewLine}{{");
             try
             {
-                // TODO:
+                for (int e = 0; e < graph.EdgeCount; ++e)
+                {
+                    SourceTargetPair<int> endpoints;
+                    if (graph.TryGetEndpoints(e, out endpoints))
+                    {
+                        textWriter.WriteLine($"    {endpoints.Source} -> {endpoints.Target};");
+                    }
+                }
             }
             finally
             {
