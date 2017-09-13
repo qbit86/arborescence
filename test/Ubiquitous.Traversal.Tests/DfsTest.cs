b276b93b82b5a8fbd84ac314b0535fc28e8550d5
Viktor T <viktor.t***@gmail.com>
2017-09-13 23:54:48 +0300

    Implemented test for non-recursive implementation of DFS.

--- test/Ubiquitous.Traversal.Tests/DfsTest.cs
+++ test/Ubiquitous.Traversal.Tests/DfsTest.cs
@@ -1,6 +1,47 @@
 ﻿namespace Ubiquitous
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Xunit;
+    using ColorMap = IndexedDictionary<Color, Color[]>;
+    using StepMap = IndexedDictionary<DfsStepKind, DfsStepKind[]>;
+
+    internal struct ColorMapFactoryInstance : IFactoryConcept<IndexedAdjacencyListGraph, ColorMap>
+    {
+        public ColorMap Acquire(IndexedAdjacencyListGraph graph)
+        {
+            return IndexedDictionary.Create(new Color[graph.VertexCount]);
+        }
+
+        public void Release(IndexedAdjacencyListGraph graph, ColorMap value)
+        {
+        }
+    }
+
+    internal sealed class DfsStepEqualityComparer : IEqualityComparer<Step<DfsStepKind, int, int>>
+    {
+        internal static DfsStepEqualityComparer Default { get; } = new DfsStepEqualityComparer();
+
+        public bool Equals(Step<DfsStepKind, int, int> x, Step<DfsStepKind, int, int> y)
+        {
+            if (x.Kind != y.Kind)
+                return false;
+
+            if (x.Vertex != y.Vertex)
+                return false;
+
+            if (x.Edge != y.Edge)
+                return false;
+
+            return true;
+        }
+
+        public int GetHashCode(Step<DfsStepKind, int, int> obj)
+        {
+            return obj.Kind.GetHashCode() ^ obj.Vertex.GetHashCode() ^ obj.Edge.GetHashCode();
+        }
+    }
 
     public class DfsTest
     {
@@ -8,8 +49,36 @@
         public void Recursive_and_non_recursive_implementations_should_match()
         {
             // Arrange
+
+            const int vertexCount = 100;
+            int edgeCount = (int)Math.Ceiling(Math.Pow(vertexCount, 1.5));
+
+            var builder = new IndexedAdjacencyListGraphBuilder(vertexCount);
+            var prng = new Random(1729);
+
+            for (int e = 0; e < edgeCount; ++e)
+            {
+                int source = prng.Next(vertexCount);
+                int target = prng.Next(vertexCount);
+                builder.Add(SourceTargetPair.Create(source, target));
+            }
+
+            IndexedAdjacencyListGraph graph = builder.MoveToIndexedAdjacencyListGraph();
+
+            var dfs = new Dfs<IndexedAdjacencyListGraph, int, int, IEnumerable<int>,
+                IndexedAdjacencyListGraphInstance, IndexedAdjacencyListGraphInstance>();
+
             // Act
+
+            var recursiveSteps = dfs.TraverseRecursively<IEnumerable<int>, ColorMap, ColorMapFactoryInstance>(
+                graph, Enumerable.Range(0, graph.VertexCount)).ToList();
+
+            var nonRecursiveSteps = dfs.TraverseNonRecursively<IEnumerable<int>, ColorMap, ColorMapFactoryInstance>(
+                graph, Enumerable.Range(0, graph.VertexCount)).ToList();
+
             // Assert
+
+            Assert.Equal(recursiveSteps, nonRecursiveSteps, DfsStepEqualityComparer.Default);
         }
     }
 }
