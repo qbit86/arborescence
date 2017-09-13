de08027c1f2d94137e0a76129f45106fc666f3d7
Viktor T <viktor.t***@gmail.com>
2017-09-13 23:33:32 +0300

    Added project Ubiquitous.Traversal.Tests.

---
+++ test/Ubiquitous.Traversal.Tests/DfsTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Ubiquitous
+{
+    using Xunit;
+
+    public class DfsTest
+    {
+        [Fact]
+        public void Recursive_and_non_recursive_implementations_should_match()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
