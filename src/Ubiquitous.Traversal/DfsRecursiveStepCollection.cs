762e459d325623dd4bacdb9caf22705fd76ff305
Viktor T <viktor.t***@gmail.com>
2017-09-22 03:19:40 +0300

    Added `DfsRecursiveStepCollection<...>` stub.

---
+++ src/Ubiquitous.Traversal/DfsRecursiveStepCollection.cs
@@ -0,0 +1,48 @@
+﻿namespace Ubiquitous
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using static System.Diagnostics.Debug;
+
+    internal struct DfsRecursiveStepCollection<TGraph, TVertex, TEdge, TEdges, TColorMap,
+        TVertexConcept, TEdgeConcept>
+
+        : IEnumerable<Step<DfsStepKind, TVertex, TEdge>>
+    {
+        private TGraph Graph { get; }
+
+        private TVertex StartVertex { get; }
+
+        private TColorMap ColorMap { get; }
+
+        private TVertexConcept VertexConcept { get; }
+
+        private TEdgeConcept EdgeConcept { get; }
+
+        public DfsRecursiveStepCollection(TGraph graph, TVertex startVertex, TColorMap colorMap,
+            TVertexConcept vertexConcept, TEdgeConcept edgeConcept)
+        {
+            Assert(colorMap != null);
+            Assert(vertexConcept != null);
+            Assert(edgeConcept != null);
+
+            Graph = graph;
+            StartVertex = startVertex;
+            ColorMap = colorMap;
+            VertexConcept = vertexConcept;
+            EdgeConcept = edgeConcept;
+        }
+
+        public IEnumerator<Step<DfsStepKind, TVertex, TEdge>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            IEnumerator<Step<DfsStepKind, TVertex, TEdge>> result = GetEnumerator();
+            return result;
+        }
+    }
+}
