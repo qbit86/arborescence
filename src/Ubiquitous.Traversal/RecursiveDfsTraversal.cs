b1f053f31a151661c6c3c8b79f09743ef42b0af2
Viktor T <viktor.t***@gmail.com>
2017-09-23 03:45:10 +0300

    Refactored `RecursiveDfsTraversal`.

--- src/Ubiquitous.Traversal/RecursiveDfsTraversal.cs
+++ src/Ubiquitous.Traversal/RecursiveDfsTraversal.cs
@@ -35,64 +35,9 @@
         {
             Assert(colorMap != null);
 
-            return ProcessVertexCoroutine(vertex, colorMap);
-        }
-
-        private IEnumerable<Step<DfsStepKind, TVertex, TEdge>> ProcessVertexCoroutine(
-            TVertex vertex, TColorMap colorMap)
-        {
-            Assert(colorMap != null);
-
-            colorMap[vertex] = Color.Gray;
-            yield return Step.Create(DfsStepKind.DiscoverVertex, vertex, default(TEdge));
-
-            TEdges edges;
-            if (VertexConcept.TryGetOutEdges(Graph, vertex, out edges) && edges != null)
-            {
-                foreach (TEdge edge in edges)
-                {
-                    var steps = ProcessEdgeCoroutine(edge, colorMap);
-                    foreach (var step in steps)
-                        yield return step;
-                }
-            }
-
-            colorMap[vertex] = Color.Black;
-            yield return Step.Create(DfsStepKind.FinishVertex, vertex, default(TEdge));
-        }
-
-        private IEnumerable<Step<DfsStepKind, TVertex, TEdge>> ProcessEdgeCoroutine(TEdge edge, TColorMap colorMap)
-        {
-            Assert(colorMap != null);
-
-            yield return Step.Create(DfsStepKind.ExamineEdge, default(TVertex), edge);
-
-            TVertex target;
-            if (EdgeConcept.TryGetTarget(Graph, edge, out target))
-            {
-                Color neighborColor;
-                if (!colorMap.TryGetValue(target, out neighborColor))
-                    neighborColor = Color.None;
-
-                switch (neighborColor)
-                {
-                    case Color.None:
-                    case Color.White:
-                        yield return Step.Create(DfsStepKind.TreeEdge, default(TVertex), edge);
-                        var steps = ProcessVertexCoroutine(target, colorMap);
-                        foreach (var step in steps)
-                            yield return step;
-                        break;
-                    case Color.Gray:
-                        yield return Step.Create(DfsStepKind.BackEdge, default(TVertex), edge);
-                        break;
-                    default:
-                        yield return Step.Create(DfsStepKind.ForwardOrCrossEdge, default(TVertex), edge);
-                        break;
-                }
-            }
-
-            yield return Step.Create(DfsStepKind.FinishEdge, default(TVertex), edge);
+            return new DfsRecursiveStepCollection<TGraph, TVertex, TEdge, TEdges, TColorMap,
+                TVertexConcept, TEdgeConcept>(
+                Graph, vertex, colorMap, VertexConcept, EdgeConcept);
         }
     }
 }
