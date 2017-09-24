529a9326c5cb7d37b4d88c456325269423c770b9
Viktor T <viktor.t***@gmail.com>
2017-09-25 02:55:12 +0300

    Removed `ITraversal<...>` and implementations.

--- src/Ubiquitous.Traversal/ITraversal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ubiquitous
-{
-    using System.Collections.Generic;
-
-    internal interface ITraversal<TStepKind, TVertex, TEdge, TColorMap>
-    {
-        IEnumerator<Step<TStepKind, TVertex, TEdge>> CreateEnumerator(TVertex vertex, TColorMap colorMap);
-    }
-}
