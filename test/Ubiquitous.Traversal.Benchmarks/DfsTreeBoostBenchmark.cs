0f4efce702b5ebc2bd7da1b0e10cf93df64ad85c
Viktor T <viktor.t***@gmail.com>
2017-10-04 21:38:07 +0300

    Added `DfsTreeBoostBenchmark` class.

---
+++ test/Ubiquitous.Traversal.Benchmarks/DfsTreeBoostBenchmark.cs
@@ -0,0 +1,54 @@
+﻿namespace Ubiquitous
+{
+    using System.Linq;
+    using ColorMap = IndexedDictionary<Color, Color[]>;
+    using ColorMapFactoryInstance = IndexedDictionaryFactoryInstance<Color>;
+
+    [BenchmarkDotNet.Attributes.MemoryDiagnoser]
+    public abstract class DfsTreeBoostBenchmark
+    {
+        [BenchmarkDotNet.Attributes.Params(10, 100, 1000)]
+        public int VertexCount { get; set; }
+
+        private Dfs<IndexedAdjacencyListGraph, int, int, ImmutableArrayEnumeratorAdapter<int>, ColorMap,
+            IndexedAdjacencyListGraphInstance, IndexedAdjacencyListGraphInstance, ColorMapFactoryInstance> Dfs { get; }
+
+        private IndexedAdjacencyListGraph Graph { get; set; }
+
+        public DfsTreeBoostBenchmark()
+        {
+            Dfs = new Dfs<IndexedAdjacencyListGraph, int, int, ImmutableArrayEnumeratorAdapter<int>, ColorMap,
+                IndexedAdjacencyListGraphInstance, IndexedAdjacencyListGraphInstance, ColorMapFactoryInstance>();
+        }
+
+        [BenchmarkDotNet.Attributes.GlobalSetup]
+        public void GlobalSetup()
+        {
+            Graph = GraphHelper.Default.GetGraph(VertexCount);
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark(Baseline = true)]
+        public int DfsTreeBaseline()
+        {
+            var steps = Dfs.TraverseBaseline(Graph, 0);
+
+            return steps.Count();
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public int DfsTreeNonRecursive()
+        {
+            var steps = Dfs.TraverseNonRecursively(Graph, 0);
+
+            return steps.Count();
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public int DfsTreeBoost()
+        {
+            var steps = Dfs.TraverseBoost(Graph, 0);
+
+            return steps.Count();
+        }
+    }
+}
