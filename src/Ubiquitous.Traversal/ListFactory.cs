8cfd2ee54ef3cb9d698b47a4a7fce99efcf540b3
Viktor T <qbit86@gmail.com>
2018-01-06 01:30:05 +0300

    Added `ListFactory<...>`.

---
+++ src/Ubiquitous.Traversal/ListFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Ubiquitous
+{
+    using System.Collections.Generic;
+
+    public struct ListFactory<TContext, TElem> : IFactory<TContext, List<TElem>>
+    {
+        public List<TElem> Acquire(TContext context)
+        {
+            return new List<TElem>();
+        }
+
+        public void Release(TContext context, List<TElem> value)
+        {
+            value?.Clear();
+        }
+    }
+}
