a2abb247b6dbcad11442120b1bc9069edd613b33
Viktor T <qbit86@gmail.com>
2018-01-13 15:02:58 +0300

    Added Enumeratur stub.

---
+++ src/Ubiquitous.Traversal/Dfs/DfsTreeStepCollection.Enumerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace Ubiquitous
+{
+    using System.Collections.Generic;
+
+    internal partial struct DfsTreeStepCollection<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TStack,
+        TVertexConcept, TEdgeConcept, TColorMapFactory, TStackFactory>
+    {
+        public struct Enumerator : IEnumerator<Step<DfsStepKind, TVertex, TEdge>>
+        {
+            private Step<DfsStepKind, TVertex, TEdge> _current;
+            private int _state;
+            private DfsTreeStepCollection<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TStack,
+                TVertexConcept, TEdgeConcept, TColorMapFactory, TStackFactory> _collection;
+
+            public bool MoveNext()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Reset()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public Step<DfsStepKind, TVertex, TEdge> Current { get; }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            public void Dispose()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
