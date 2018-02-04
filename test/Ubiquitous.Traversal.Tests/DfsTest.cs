134966c3e9b8a7bcf6531123ca924c20086c44dc
Viktor T <qbit86@gmail.com>
2018-02-05 01:55:27 +0300

    Fixed temporarily broken semantics.

--- test/Ubiquitous.Traversal.Tests/DfsTest.cs
+++ test/Ubiquitous.Traversal.Tests/DfsTest.cs
@@ -122,7 +122,7 @@
             // Act
 
             var baselineSteps = BaselineDfs.Traverse(graph, vertices).ToList();
-            var vertexEnumerator = vertices.GetConventionalEnumerator();
+            var vertexEnumerator = vertices.GetConventionalEnumerator() as IEnumerator<int>;
             var boostSteps = Dfs.Traverse(graph, vertexEnumerator).ToList();
             var discoveredVertexCount = boostSteps.Count(s => s.Kind == DfsStepKind.DiscoverVertex);
             var expectedStartVertexCount = baselineSteps.Count(s => s.Kind == DfsStepKind.StartVertex);
