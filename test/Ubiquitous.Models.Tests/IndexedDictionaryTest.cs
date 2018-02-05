820bc21be74cf6e19faba93cf0afb3666c5337ac
Viktor T <qbit86@gmail.com>
2018-02-06 01:00:29 +0300

    Added `IndexedDictionaryTest`.

---
+++ test/Ubiquitous.Models.Tests/IndexedDictionaryTest.cs
@@ -0,0 +1,12 @@
+namespace Ubiquitous
+{
+    using Xunit;
+
+    public class IndexedDictionaryTest
+    {
+        [Fact]
+        public void Struct_backing_store_should_be_modifiable()
+        {
+        }
+    }
+}
