61d1aed67ed4ebcd4c1a57f47e3428265a04e462
Viktor T <qbit86@gmail.com>
2018-07-01 23:26:28 +0300

    Added Pragmatic.

---
+++ src/Ubiquitous.Pragmatic/BaselineIndexedBfs.cs
@@ -0,0 +1,20 @@
+﻿namespace Ubiquitous.Pragmatic
+{
+    using System;
+    using System.Collections.Generic;
+
+    public readonly struct BaselineIndexedBfs<TGraph, TEdge, TEdgeEnumerator, TGraphConcept>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraphConcept : IGetTargetConcept<TGraph, int, TEdge>, IGetOutEdgesConcept<TGraph, int, TEdgeEnumerator>
+    {
+        public BaselineIndexedBfs(TGraphConcept graphConcept)
+        {
+            if (graphConcept == null)
+                throw new ArgumentNullException(nameof(graphConcept));
+
+            GraphConcept = graphConcept;
+        }
+
+        public TGraphConcept GraphConcept { get; }
+    }
+}
