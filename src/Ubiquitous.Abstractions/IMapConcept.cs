8eb13365922d690a97b8d04bc2597fd041c3eb7a
Viktor T <qbit86@gmail.com>
2018-07-02 02:40:58 +0300

    Added IMapConcept.

---
+++ src/Ubiquitous.Abstractions/IMapConcept.cs
@@ -0,0 +1,8 @@
+﻿namespace Ubiquitous
+{
+    public interface IMapConcept<in TMap, in TKey, TValue>
+    {
+        bool TryGet(TMap map, TKey key, out TValue value);
+        void Put(TMap map, TKey key, TValue value);
+    }
+}
