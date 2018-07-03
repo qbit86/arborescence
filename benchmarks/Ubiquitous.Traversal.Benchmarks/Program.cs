e3b65e5a40b8b7bba13e66760456953925d2bbb2
Viktor T <qbit86@gmail.com>
2018-07-04 01:38:54 +0300

    Cleaned up Benchmarks/Program.cs.

--- benchmarks/Ubiquitous.Traversal.Benchmarks/Program.cs
+++ benchmarks/Ubiquitous.Traversal.Benchmarks/Program.cs
@@ -3,19 +3,20 @@
     using BenchmarkDotNet.Configs;
     using BenchmarkDotNet.Jobs;
     using BenchmarkDotNet.Reports;
+    using BenchmarkDotNet.Running;
 
     internal static class Program
     {
         private static void Main()
         {
             // http://benchmarkdotnet.org/Configs/Configs.htm
-            var job = new Job(Job.Default)
+            Job job = new Job(Job.Default)
                 .ApplyAndFreeze(RunMode.Short);
 
             IConfig config = ManualConfig.Create(DefaultConfig.Instance)
                 .With(job);
 
-            Summary unused = BenchmarkDotNet.Running.BenchmarkRunner.Run<DfsTreeBoostBenchmark>(config);
+            Summary unused = BenchmarkRunner.Run<DfsTreeBoostBenchmark>(config);
         }
     }
 }
