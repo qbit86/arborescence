50bbd73b1b05363a7e30bd780e216b4e9b93bd88
Viktor T <qbit86@gmail.com>
2018-07-07 00:29:05 +0300

    Implemented IMapConcept temporarily.

--- src/Ubiquitous.Models/IndexedDictionaryFactory.cs
+++ src/Ubiquitous.Models/IndexedDictionaryFactory.cs
@@ -1,7 +1,22 @@
 ﻿namespace Ubiquitous
 {
-    public struct IndexedDictionaryFactory<T> : IFactory<IndexedAdjacencyListGraph, IndexedDictionary<T, T[]>>
+    public readonly struct IndexedDictionaryFactory<T> : IMapConcept<IndexedDictionary<T, T[]>, int, T>,
+        IFactory<IndexedAdjacencyListGraph, IndexedDictionary<T, T[]>>
     {
+        public bool TryGet(IndexedDictionary<T, T[]> map, int key, out T value)
+        {
+            return map.TryGetValue(key, out value);
+        }
+
+        public bool TryPut(IndexedDictionary<T, T[]> map, int key, T value)
+        {
+            if ((uint)key >= (uint)map.Count)
+                return false;
+
+            map[key] = value;
+            return true;
+        }
+
         public IndexedDictionary<T, T[]> Acquire(IndexedAdjacencyListGraph context)
         {
             return IndexedDictionary.Create(new T[context.VertexCount]);
