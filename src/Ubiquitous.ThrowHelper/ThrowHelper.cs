2b268fd081106f6438dcb69caef77f98c229eeda
Viktor T <qbit86@gmail.com>
2018-10-28 15:19:22 +0300

    Add Ubiquitous.ThrowHelper shared project.

---
+++ src/Ubiquitous.ThrowHelper/ThrowHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Ubiquitous
+{
+    using System;
+
+    // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/src/System/ThrowHelper.cs
+    // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/src/Resources/Strings.resx
+    internal static class ThrowHelper
+    {
+        internal static void ThrowInvalidOperationException_InvalidOperation_EnumNotStarted()
+        {
+            throw new InvalidOperationException("Enumeration has not started. Call MoveNext.");
+        }
+
+        internal static void ThrowInvalidOperationException_InvalidOperation_EnumEnded()
+        {
+            throw new InvalidOperationException("Enumeration already finished.");
+        }
+    }
+}
