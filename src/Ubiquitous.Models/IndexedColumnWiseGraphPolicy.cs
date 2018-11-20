9e9239db9784188a99eb47a48e08ac1255cbbccc
Viktor T <qbit86@gmail.com>
2018-11-21 01:30:19 +0300

    Add IndexedColumnWiseGraphPolicy<...> and IndexedRowWiseGraphPolicy<...>.

---
+++ src/Ubiquitous.Models/IndexedColumnWiseGraphPolicy.cs
@@ -0,0 +1,24 @@
+namespace Ubiquitous
+{
+    public readonly struct IndexedColumnWiseGraphPolicy<TGraph, TEdges> :
+        IGetSourcePolicy<TGraph, int, int>,
+        IGetTargetPolicy<TGraph, int, int>,
+        IGetOutEdgesPolicy<TGraph, int, TEdges>
+        where TGraph : IGetSource<int, int>, IGetTarget<int, int>, IGetOutEdges<int, TEdges>
+    {
+        public bool TryGetSource(TGraph graph, int edge, out int source)
+        {
+            return graph.TryGetSource(edge, out source);
+        }
+
+        public bool TryGetTarget(TGraph graph, int edge, out int target)
+        {
+            return graph.TryGetTarget(edge, out target);
+        }
+
+        public bool TryGetOutEdges(TGraph graph, int vertex, out TEdges edges)
+        {
+            return graph.TryGetOutEdges(vertex, out edges);
+        }
+    }
+}
