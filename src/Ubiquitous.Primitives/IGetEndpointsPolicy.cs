1ef375450fb200533b168cdddd8ec1d8cddb5806
Viktor T <qbit86@gmail.com>
2018-11-22 00:02:08 +0300

    Remove IGetEndpointsPolicy.

--- src/Ubiquitous.Primitives/IGetEndpointsPolicy.cs
+++
@@ -1,7 +0,0 @@
-namespace Ubiquitous
-{
-    public interface IGetEndpointsPolicy<in TGraph, TVertex, in TEdge>
-    {
-        bool TryGetEndpoints(TGraph graph, TEdge edge, out SourceTargetPair<TVertex> endpoints);
-    }
-}
