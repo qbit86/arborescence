5b57f264b23c03f6f731b3584dd51e8c7bd0da1a
Viktor T <qbit86@gmail.com>
2018-11-24 02:43:51 +0300

    Add Edge*GraphPolicy.

---
+++ src/Ubiquitous.Models/GraphPolicies/EdgeBidirectionalGraphPolicy.cs
@@ -0,0 +1,32 @@
+namespace Ubiquitous
+{
+    public readonly struct EdgeBidirectionalGraphPolicy<TGraph, TEdges> :
+        IGetSourcePolicy<TGraph, int, SourceTargetPair<int>>,
+        IGetTargetPolicy<TGraph, int, SourceTargetPair<int>>,
+        IGetOutEdgesPolicy<TGraph, int, TEdges>,
+        IGetInEdgesPolicy<TGraph, int, TEdges>
+        where TGraph : IBidirectionalGraph<int, int, TEdges>
+    {
+        public bool TryGetSource(TGraph graph, SourceTargetPair<int> edge, out int source)
+        {
+            source = edge.Source;
+            return true;
+        }
+
+        public bool TryGetTarget(TGraph graph, SourceTargetPair<int> edge, out int target)
+        {
+            target = edge.Target;
+            return true;
+        }
+
+        public bool TryGetOutEdges(TGraph graph, int vertex, out TEdges edges)
+        {
+            return graph.TryGetOutEdges(vertex, out edges);
+        }
+
+        public bool TryGetInEdges(TGraph graph, int vertex, out TEdges edges)
+        {
+            return graph.TryGetInEdges(vertex, out edges);
+        }
+    }
+}
