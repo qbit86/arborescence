a56339def43ca82f0a7908ddeb6a8bea39890b9e
Viktor T <qbit86@gmail.com>
2018-11-24 13:34:10 +0300

    Fix EdgeIncidenceGraphPolicy.

--- src/Ubiquitous.Models/GraphPolicies/EdgeIncidenceGraphPolicy.cs
+++ src/Ubiquitous.Models/GraphPolicies/EdgeIncidenceGraphPolicy.cs
@@ -4,18 +4,16 @@ namespace Ubiquitous
         IGetSourcePolicy<TGraph, int, SourceTargetPair<int>>,
         IGetTargetPolicy<TGraph, int, SourceTargetPair<int>>,
         IGetOutEdgesPolicy<TGraph, int, TEdges>
-        where TGraph : IIncidenceGraph<int, int, TEdges>
+        where TGraph : IIncidenceGraph<int, SourceTargetPair<int>, TEdges>
     {
         public bool TryGetSource(TGraph graph, SourceTargetPair<int> edge, out int source)
         {
-            source = edge.Source;
-            return true;
+            return graph.TryGetSource(edge, out source);
         }
 
         public bool TryGetTarget(TGraph graph, SourceTargetPair<int> edge, out int target)
         {
-            target = edge.Target;
-            return true;
+            return graph.TryGetTarget(edge, out target);
         }
 
         public bool TryGetOutEdges(TGraph graph, int vertex, out TEdges edges)
