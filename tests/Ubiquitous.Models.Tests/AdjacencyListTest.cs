5930c51f4b793be7e14c15c955dc99863d6bbfc9
Viktor T <qbit86@gmail.com>
2018-12-01 12:56:53 +0300

    Add parsing graphs.

--- tests/Ubiquitous.Models.Tests/AdjacencyListTest.cs
+++ tests/Ubiquitous.Models.Tests/AdjacencyListTest.cs
@@ -1,26 +1,51 @@
 namespace Ubiquitous
 {
+    using System.Collections.Generic;
     using System.IO;
+    using Models;
     using Workbench;
     using Xunit;
     using Xunit.Abstractions;
 
     public sealed class AdjacencyListTest
     {
+        private const int VertexUpperBound = 10;
+
         public AdjacencyListTest(ITestOutputHelper output)
         {
             Output = output;
         }
 
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
         private ITestOutputHelper Output { get; }
 
         [Theory]
         [ClassData(typeof(IndexedGraphTestCollection))]
         public void ShouldNotBeLess(string testName)
         {
+            var jaggedAdjacencyListBuilder = new JaggedAdjacencyListIncidenceGraphBuilder(VertexUpperBound);
+
             using (TextReader textReader = IndexedGraphs.GetTextReader(testName))
             {
+                var parser = new IndexedEdgeListParser();
+                IEnumerable<SourceTargetPair<int>> edges = parser.ParseEdges(textReader);
+                foreach (SourceTargetPair<int> edge in edges)
+                    jaggedAdjacencyListBuilder.TryAdd(edge.Source, edge.Target, out _);
             }
+
+            JaggedAdjacencyListIncidenceGraph jaggedAdjacencyList = jaggedAdjacencyListBuilder.ToGraph();
+
+            var adjacencyListBuilder = new AdjacencyListIncidenceGraphBuilder(VertexUpperBound);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader(testName))
+            {
+                var parser = new IndexedEdgeListParser();
+                IEnumerable<SourceTargetPair<int>> edges = parser.ParseEdges(textReader);
+                foreach (SourceTargetPair<int> edge in edges)
+                    adjacencyListBuilder.TryAdd(edge.Source, edge.Target, out _);
+            }
+
+            AdjacencyListIncidenceGraph adjacencyList = adjacencyListBuilder.ToGraph();
 
             Assert.True(true, testName);
         }
