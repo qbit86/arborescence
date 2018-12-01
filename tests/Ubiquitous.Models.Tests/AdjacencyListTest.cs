004a615229d9fcc2b6e6a8bbacf51941b0d16d48
Viktor T <qbit86@gmail.com>
2018-12-01 13:35:24 +0300

    Add comparing out edges.

--- tests/Ubiquitous.Models.Tests/AdjacencyListTest.cs
+++ tests/Ubiquitous.Models.Tests/AdjacencyListTest.cs
@@ -2,6 +2,7 @@ namespace Ubiquitous
 {
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using Models;
     using Workbench;
     using Xunit;
@@ -23,6 +24,7 @@ namespace Ubiquitous
         [ClassData(typeof(IndexedGraphTestCollection))]
         public void ShouldNotBeLess(string testName)
         {
+            // Arrange
             var jaggedAdjacencyListBuilder = new JaggedAdjacencyListIncidenceGraphBuilder(VertexUpperBound);
 
             using (TextReader textReader = IndexedGraphs.GetTextReader(testName))
@@ -47,7 +49,24 @@ namespace Ubiquitous
 
             AdjacencyListIncidenceGraph adjacencyList = adjacencyListBuilder.ToGraph();
 
-            Assert.True(true, testName);
+            // Act
+            for (int v = 0; v < jaggedAdjacencyList.VertexUpperBound; ++v)
+            {
+                if (!jaggedAdjacencyList.TryGetOutEdges(v, out ArrayPrefixEnumerator<int> jaggedOutEdgesEnumerator))
+                    continue;
+
+                var jaggedOutEdges = new List<int>(OneTimeEnumerable<int>.Create(jaggedOutEdgesEnumerator));
+
+                bool hasOutEdges = adjacencyList.TryGetOutEdges(v, out ArraySegmentEnumerator<int> outEdgesEnumerator);
+                Assert.True(hasOutEdges);
+
+                var outEdges = new List<int>(OneTimeEnumerable<int>.Create(outEdgesEnumerator));
+
+                IEnumerable<int> leftDifference = jaggedOutEdges.Except(outEdges);
+
+                // Assert
+                Assert.Empty(leftDifference);
+            }
         }
     }
 }
