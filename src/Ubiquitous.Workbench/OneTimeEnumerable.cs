4d814a18f61bee8599b16997daf247f38f1ffeb4
Viktor T <qbit86@gmail.com>
2018-12-01 13:28:59 +0300

    Fix static OneTimeEnumerable companion class.

--- src/Ubiquitous.Workbench/OneTimeEnumerable.cs
+++ src/Ubiquitous.Workbench/OneTimeEnumerable.cs
@@ -3,9 +3,9 @@ namespace Ubiquitous.Workbench
     using System.Collections;
     using System.Collections.Generic;
 
-    public static class OneTimeEnumerable
+    public static class OneTimeEnumerable<T>
     {
-        public static OneTimeEnumerable<T, TEnumerator> Create<T, TEnumerator>(TEnumerator enumerator)
+        public static OneTimeEnumerable<T, TEnumerator> Create<TEnumerator>(TEnumerator enumerator)
             where TEnumerator : IEnumerator<T>
         {
             return new OneTimeEnumerable<T, TEnumerator>(enumerator);
