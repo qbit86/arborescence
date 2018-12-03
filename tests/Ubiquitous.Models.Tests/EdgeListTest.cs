f3446625dd477d5a2b0e17ee1a6b7415294f6303
Viktor T <qbit86@gmail.com>
2018-12-04 01:08:28 +0300

    Add passing EdgeListTest.EdgeList_ShouldNotBeGreater().

--- tests/Ubiquitous.Models.Tests/EdgeListTest.cs
+++ tests/Ubiquitous.Models.Tests/EdgeListTest.cs
@@ -49,5 +49,45 @@ namespace Ubiquitous
                 Assert.Empty(difference);
             }
         }
+
+        [Theory]
+        [ClassData(typeof(IndexedGraphTestCollection))]
+        public void EdgeList_ShouldNotBeGreater(string testName)
+        {
+            // Arrange
+            var jaggedAdjacencyListBuilder = new JaggedAdjacencyListIncidenceGraphBuilder(VertexUpperBound);
+            JaggedAdjacencyListIncidenceGraph jaggedAdjacencyList =
+                BuildHelpers<JaggedAdjacencyListIncidenceGraph, int>.CreateGraph(ref jaggedAdjacencyListBuilder,
+                    testName);
+
+            var edgeListBuilder = new EdgeListIncidenceGraphBuilder(VertexUpperBound);
+            EdgeListIncidenceGraph edgeList =
+                BuildHelpers<EdgeListIncidenceGraph, SourceTargetPair<int>>.CreateGraph(ref edgeListBuilder, testName);
+
+            // Act
+            for (int v = 0; v < edgeList.VertexUpperBound; ++v)
+            {
+                if (!edgeList.TryGetOutEdges(v, out ArraySegmentEnumerator<SourceTargetPair<int>> outEdgesEnumerator))
+                    continue;
+
+                List<SourceTargetPair<int>> outEdges =
+                    OneTimeEnumerable<SourceTargetPair<int>>.Create(outEdgesEnumerator).ToList();
+
+                bool hasOutEdges =
+                    jaggedAdjacencyList.TryGetOutEdges(v, out ArrayPrefixEnumerator<int> jaggedOutEdgesEnumerator);
+                Assert.True(hasOutEdges, $"Should have edges for {nameof(v)}: {v}");
+
+                List<SourceTargetPair<int>> jaggedOutEndpoints = OneTimeEnumerable<int>
+                    .Create(jaggedOutEdgesEnumerator)
+                    .Select(e => new { Success = jaggedAdjacencyList.TryGetTarget(e, out int target), Target = target })
+                    .Where(p => p.Success).Select(p => SourceTargetPair.Create(v, p.Target))
+                    .ToList();
+
+                IEnumerable<SourceTargetPair<int>> difference = outEdges.Except(jaggedOutEndpoints);
+
+                // Assert
+                Assert.Empty(difference);
+            }
+        }
     }
 }
