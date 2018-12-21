e944c5c092add449332e0e5b0e10bff16c8215c7
Viktor T <qbit86@gmail.com>
2018-12-21 21:13:05 +0300

    Add UncheckedEdge*GraphPolicy.

---
+++ src/Ubiquitous.Models/GraphPolicies/UncheckedEdgeIncidenceGraphPolicy.cs
@@ -0,0 +1,26 @@
+namespace Ubiquitous.Models
+{
+    public readonly struct UncheckedEdgeIncidenceGraphPolicy<TGraph, TEdges> :
+        IGetSourcePolicy<TGraph, int, SourceTargetPair<int>>,
+        IGetTargetPolicy<TGraph, int, SourceTargetPair<int>>,
+        IGetOutEdgesPolicy<TGraph, int, TEdges>
+        where TGraph : IIncidenceGraph<int, SourceTargetPair<int>, TEdges>
+    {
+        public bool TryGetSource(TGraph graph, SourceTargetPair<int> edge, out int source)
+        {
+            source = edge.Source;
+            return true;
+        }
+
+        public bool TryGetTarget(TGraph graph, SourceTargetPair<int> edge, out int target)
+        {
+            target = edge.Target;
+            return true;
+        }
+
+        public bool TryGetOutEdges(TGraph graph, int vertex, out TEdges edges)
+        {
+            return graph.TryGetOutEdges(vertex, out edges);
+        }
+    }
+}
