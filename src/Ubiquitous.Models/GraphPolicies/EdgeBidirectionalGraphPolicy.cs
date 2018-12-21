0f97eeb4bbb967eb82ccc5bff4689f7742b17884
Viktor T <qbit86@gmail.com>
2018-12-21 21:06:54 +0300

    Fix EdgeBidirectionalGraphPolicy<>.

--- src/Ubiquitous.Models/GraphPolicies/EdgeBidirectionalGraphPolicy.cs
+++ src/Ubiquitous.Models/GraphPolicies/EdgeBidirectionalGraphPolicy.cs
@@ -5,18 +5,16 @@ namespace Ubiquitous.Models
         IGetTargetPolicy<TGraph, int, SourceTargetPair<int>>,
         IGetOutEdgesPolicy<TGraph, int, TEdges>,
         IGetInEdgesPolicy<TGraph, int, TEdges>
-        where TGraph : IBidirectionalGraph<int, int, TEdges>
+        where TGraph : IBidirectionalGraph<int, SourceTargetPair<int>, TEdges>
     {
         public bool TryGetSource(TGraph graph, SourceTargetPair<int> edge, out int source)
         {
-            source = edge.Source;
-            return true;
+            return graph.TryGetSource(edge, out source);
         }
 
         public bool TryGetTarget(TGraph graph, SourceTargetPair<int> edge, out int target)
         {
-            target = edge.Target;
-            return true;
+            return graph.TryGetTarget(edge, out target);
         }
 
         public bool TryGetOutEdges(TGraph graph, int vertex, out TEdges edges)
