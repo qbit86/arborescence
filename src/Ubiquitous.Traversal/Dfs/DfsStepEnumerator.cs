08dbe45579887a2d031e0d3a0306342624edfd59
Viktor T <qbit86@gmail.com>
2019-01-10 01:44:44 +0300

    Move DfsStepEnumerator<> from Advanced subnamespace.

--- src/Ubiquitous.Traversal/Dfs/DfsStepEnumerator.cs
+++ src/Ubiquitous.Traversal/Dfs/DfsStepEnumerator.cs
@@ -1,6 +1,6 @@
 ﻿// ReSharper disable FieldCanBeMadeReadOnly.Local
 
-namespace Ubiquitous.Traversal.Advanced
+namespace Ubiquitous.Traversal
 {
     using System.Collections.Generic;
     using System.Runtime.CompilerServices;
