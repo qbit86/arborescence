22ff17bebfe5be5fdbd05810ecb5567306e604c1
Viktor T <qbit86@gmail.com>
2019-03-15 22:41:32 +0300

    Suppress CA1028 in Traversal

--- src/Ubiquitous.Traversal/Color.cs
+++ src/Ubiquitous.Traversal/Color.cs
@@ -1,6 +1,8 @@
-﻿namespace Ubiquitous.Traversal
+namespace Ubiquitous.Traversal
 {
+#pragma warning disable CA1028 // Enum Storage should be Int32
     public enum Color : byte
+#pragma warning restore CA1028 // Enum Storage should be Int32
     {
         None = 0,
         White,
