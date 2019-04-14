b6f813ffb89703dfb0c2bd10636af2c5c7e5bfc5
Viktor T <qbit86@gmail.com>
2019-04-14 22:56:15 +0300

    Add BaselineUndirectedDfs<> companion class

--- src/Ubiquitous.Traversal.Baseline/Dfs/BaselineUndirectedDfs.cs
+++ src/Ubiquitous.Traversal.Baseline/Dfs/BaselineUndirectedDfs.cs
@@ -3,17 +3,41 @@ namespace Ubiquitous.Traversal
     using System;
     using System.Collections.Generic;
 
+    public static class BaselineUndirectedDfs<TGraph, TVertex, TEdge, TEdgeEnumerator,
+        TVertexColorMap, TEdgeColorMap, TStep>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+    {
+        public static BaselineUndirectedDfs<TGraph, TVertex, TEdge, TEdgeEnumerator,
+                TVertexColorMap, TEdgeColorMap, TStep,
+                TGraphPolicy, TVertexColorMapPolicy, TEdgeColorMapPolicy, TStepPolicy>
+            Create<TGraphPolicy, TVertexColorMapPolicy, TEdgeColorMapPolicy, TStepPolicy>(
+                TGraphPolicy graphPolicy, TVertexColorMapPolicy vertexColorMapPolicy,
+                TEdgeColorMapPolicy edgeColorMapPolicy, TStepPolicy stepPolicy)
+            where TGraphPolicy :
+            IGetOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>, IGetInEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+            IGetTargetPolicy<TGraph, TVertex, TEdge>, IGetSourcePolicy<TGraph, TVertex, TEdge>
+            where TVertexColorMapPolicy : IMapPolicy<TVertexColorMap, TVertex, Color>, IFactory<TVertexColorMap>
+            where TEdgeColorMapPolicy : IMapPolicy<TEdgeColorMap, TEdge, Color>, IFactory<TVertexColorMap>
+            where TStepPolicy : IStepPolicy<DfsStepKind, TVertex, TEdge, TStep>
+        {
+            return new BaselineUndirectedDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TVertexColorMap, TEdgeColorMap,
+                TStep, TGraphPolicy, TVertexColorMapPolicy, TEdgeColorMapPolicy, TStepPolicy>(
+                graphPolicy, vertexColorMapPolicy, edgeColorMapPolicy, stepPolicy);
+        }
+    }
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly struct BaselineUndirectedDfs<TGraph, TVertex, TEdge, TEdgeEnumerator,
         TVertexColorMap, TEdgeColorMap, TStep,
         TGraphPolicy, TVertexColorMapPolicy, TEdgeColorMapPolicy, TStepPolicy>
         where TEdgeEnumerator : IEnumerator<TEdge>
-        where TGraphPolicy : IGetOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
-            IGetInEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
-            IGetTargetPolicy<TGraph, TVertex, TEdge>,
-            IGetSourcePolicy<TGraph, TVertex, TEdge>
+        where TGraphPolicy :
+        IGetOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>, IGetInEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+        IGetTargetPolicy<TGraph, TVertex, TEdge>, IGetSourcePolicy<TGraph, TVertex, TEdge>
         where TVertexColorMapPolicy : IMapPolicy<TVertexColorMap, TVertex, Color>, IFactory<TVertexColorMap>
         where TEdgeColorMapPolicy : IMapPolicy<TEdgeColorMap, TEdge, Color>, IFactory<TVertexColorMap>
         where TStepPolicy : IStepPolicy<DfsStepKind, TVertex, TEdge, TStep>
+#pragma warning restore CA1815 // Override equals and operator equals on value types
     {
         private readonly TGraphPolicy _graphPolicy;
         private readonly TVertexColorMapPolicy _vertexColorMapPolicy;
