738da5ff2c0bcc01513618c6827d34330f389a5e
Viktor T <qbit86@gmail.com>
2019-04-20 01:45:19 +0300

    Implement BaselineUndirectedDfsStepCollection<> ctor

--- src/Ubiquitous.Traversal.Baseline/Dfs/BaselineUndirectedDfsStepCollection.cs
+++ src/Ubiquitous.Traversal.Baseline/Dfs/BaselineUndirectedDfsStepCollection.cs
@@ -2,6 +2,7 @@ namespace Ubiquitous.Traversal
 {
     using System.Collections;
     using System.Collections.Generic;
+    using static System.Diagnostics.Debug;
 
     internal struct BaselineUndirectedDfsStepCollection<TGraph, TVertex, TEdge, TEdgeEnumerator,
         TVertexColorMap, TEdgeColorMap, TStep,
@@ -13,13 +14,37 @@ namespace Ubiquitous.Traversal
         where TEdgeColorMapPolicy : IMapPolicy<TEdgeColorMap, TEdge, Color>
         where TStepPolicy : IStepPolicy<DfsStepKind, TVertex, TEdge, TStep>
     {
-        internal BaselineUndirectedDfsStepCollection(TGraph graph, TVertex startVertex,
-            TVertexColorMap vertexColorMap, TEdgeColorMap edgeColorMap,
-            TGraphPolicy graphPolicy, TVertexColorMapPolicy vertexColorMapPolicy,
-            TEdgeColorMapPolicy edgeColorMapPolicy, TStepPolicy stepPolicy)
-        {
+        private TVertexColorMap _vertexColorMap;
+        private TEdgeColorMap _edgeColorMap;
 
+        internal BaselineUndirectedDfsStepCollection(TGraph graph, TVertex startVertex,
+            TVertexColorMap vertexColorMap, TEdgeColorMap edgeColorMap, TGraphPolicy graphPolicy,
+            TVertexColorMapPolicy vertexColorMapPolicy, TEdgeColorMapPolicy edgeColorMapPolicy, TStepPolicy stepPolicy)
+        {
+            Assert(vertexColorMap != null);
+            Assert(edgeColorMap != null);
+            Assert(graphPolicy != null);
+            Assert(vertexColorMapPolicy != null);
+            Assert(edgeColorMapPolicy != null);
+            Assert(stepPolicy != null);
+
+            _vertexColorMap = vertexColorMap;
+            _edgeColorMap = edgeColorMap;
+
+            Graph = graph;
+            StartVertex = startVertex;
+            GraphPolicy = graphPolicy;
+            VertexColorMapPolicy = vertexColorMapPolicy;
+            EdgeColorMapPolicy = edgeColorMapPolicy;
+            StepPolicy = stepPolicy;
         }
+
+        private TGraph Graph { get; }
+        private TVertex StartVertex { get; }
+        private TGraphPolicy GraphPolicy { get; }
+        private TVertexColorMapPolicy VertexColorMapPolicy { get; }
+        private TEdgeColorMapPolicy EdgeColorMapPolicy { get; }
+        private TStepPolicy StepPolicy { get; }
 
         public IEnumerator<TStep> GetEnumerator()
         {
