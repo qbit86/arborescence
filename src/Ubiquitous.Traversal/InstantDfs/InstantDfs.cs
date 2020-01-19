f0cc07cc3fe80c77a3052c2d2927f3de19e1b06d
Viktor T <qbit86@gmail.com>
2020-01-19 15:23:36 +0300

    Add InstantDfs stub

---
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.cs
@@ -0,0 +1,30 @@
+namespace Ubiquitous.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly struct InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+        IGetTargetPolicy<TGraph, TVertex, TEdge>
+        where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>, IFactory<TColorMap>
+    {
+        private TGraphPolicy GraphPolicy { get; }
+        private TColorMapPolicy ColorMapPolicy { get; }
+
+        public InstantDfs(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
+        {
+            if (graphPolicy == null)
+                throw new ArgumentNullException(nameof(graphPolicy));
+
+            if (colorMapPolicy == null)
+                throw new ArgumentNullException(nameof(colorMapPolicy));
+
+            GraphPolicy = graphPolicy;
+            ColorMapPolicy = colorMapPolicy;
+        }
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
