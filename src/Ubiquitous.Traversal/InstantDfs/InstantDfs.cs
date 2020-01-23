a50dac925e2ce7a16f7c0b7f32a6acb5f83103b6
Viktor T <qbit86@gmail.com>
2020-01-24 02:12:09 +0300

    Add factory method

--- src/Ubiquitous.Traversal/InstantDfs/InstantDfs.cs
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.cs
@@ -3,6 +3,22 @@ namespace Ubiquitous.Traversal
     using System;
     using System.Collections.Generic;
 
+    public static class InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+    {
+#pragma warning disable CA1000 // Do not declare static members on generic types
+        public static InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
+            Create<TGraphPolicy, TColorMapPolicy>(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
+            where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+            IGetTargetPolicy<TGraph, TVertex, TEdge>
+            where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>, IFactory<TColorMap>
+        {
+            return new InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                graphPolicy, colorMapPolicy);
+        }
+#pragma warning restore CA1000 // Do not declare static members on generic types
+    }
+
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly partial struct InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
