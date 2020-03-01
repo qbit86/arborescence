524c6e63eb4dbc6cc3c6eab51c5326b8e0da3434
Viktor T <qbit86@gmail.com>
2020-03-02 01:44:43 +0300

    Add traversing from start vertex

--- src/Ubiquitous.Traversal/InstantDfs/InstantDfs.MultipleSource.cs
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.MultipleSource.cs
@@ -6,12 +6,19 @@ namespace Ubiquitous.Traversal
     public readonly partial struct InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
     {
-        public void Traverse<TVertexEnumerator>(
-            TGraph graph, TVertexEnumerator vertices, TVertex startVertex, TColorMap colorMap)
+        public void Traverse<TVertexEnumerator, THandler>(
+            TGraph graph, TVertexEnumerator vertices, TVertex startVertex, TColorMap colorMap, THandler handler)
             where TVertexEnumerator : IEnumerator<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
         {
             if (vertices == null)
                 throw new ArgumentNullException(nameof(vertices));
+
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            handler.StartVertex(graph, startVertex);
+            TraverseCore(graph, startVertex, colorMap, handler, s_false);
 
             throw new NotImplementedException();
         }
