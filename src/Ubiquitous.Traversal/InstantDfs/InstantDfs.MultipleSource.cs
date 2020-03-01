016865e3b980750a980740d69f474a5f2adab0df
Viktor T <qbit86@gmail.com>
2020-03-02 02:19:57 +0300

    Add another multiple source Traverse() overload

--- src/Ubiquitous.Traversal/InstantDfs/InstantDfs.MultipleSource.cs
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.MultipleSource.cs
@@ -7,7 +7,32 @@ namespace Ubiquitous.Traversal
         TGraphPolicy, TColorMapPolicy>
     {
         public void Traverse<TVertexEnumerator, THandler>(
-            TGraph graph, TVertexEnumerator vertices, TVertex startVertex, TColorMap colorMap, THandler handler)
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, THandler handler)
+            where TVertexEnumerator : IEnumerator<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                if (!ColorMapPolicy.TryGetValue(colorMap, u, out Color color))
+                    color = Color.None;
+
+                if (color != Color.None || color != Color.White)
+                    continue;
+
+                handler.StartVertex(graph, u);
+                TraverseCore(graph, u, colorMap, handler, s_false);
+            }
+        }
+
+        public void Traverse<TVertexEnumerator, THandler>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, THandler handler, TVertex startVertex)
             where TVertexEnumerator : IEnumerator<TVertex>
             where THandler : IDfsHandler<TGraph, TVertex, TEdge>
         {
