3bf84f4d39a5e17354003c0921784de94901d6b1
Viktor T <qbit86@gmail.com>
2020-03-07 02:00:59 +0300

    Cleanup

--- src/Ubiquitous.Traversal/Color.cs
+++ src/Ubiquitous.Traversal/Color.cs
@@ -2,11 +2,11 @@ namespace Ubiquitous.Traversal
 {
 #pragma warning disable CA1028 // Enum Storage should be Int32
     public enum Color : byte
-#pragma warning restore CA1028 // Enum Storage should be Int32
     {
         None = 0,
         White,
         Gray,
         Black
     }
+#pragma warning restore CA1028 // Enum Storage should be Int32
 }
