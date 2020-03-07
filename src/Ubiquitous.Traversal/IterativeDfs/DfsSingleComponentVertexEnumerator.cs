bbe469d1ccdae3695a3a6ac0de4dc372041ea816
Viktor T <qbit86@gmail.com>
2020-03-07 13:41:15 +0300

    Implement EnumerateVertices<>()

--- src/Ubiquitous.Traversal/IterativeDfs/DfsSingleComponentVertexEnumerator.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/DfsSingleComponentVertexEnumerator.cs
@@ -1,8 +1,11 @@
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+
 namespace Ubiquitous.Traversal
 {
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using static System.Diagnostics.Debug;
 
 #pragma warning disable CA1710 // Identifiers should have correct suffix
     public struct DfsSingleComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
@@ -12,6 +15,33 @@ namespace Ubiquitous.Traversal
         IGetTargetPolicy<TGraph, TVertex, TEdge>
         where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
     {
+        private DfsVertexStep<TVertex> _current;
+        private int _state;
+
+        private TGraphPolicy _graphPolicy;
+        private TColorMapPolicy _colorMapPolicy;
+
+        private readonly TGraph _graph;
+        private readonly TVertex _startVertex;
+        private readonly TColorMap _colorMap;
+
+        internal DfsSingleComponentVertexEnumerator(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy,
+            TGraph graph, TVertex startVertex, TColorMap colorMap)
+        {
+            Assert(graphPolicy != null, "graphPolicy != null");
+            Assert(colorMapPolicy != null, "colorMapPolicy != null");
+
+            _current = default;
+            _state = 0;
+
+            _graphPolicy = graphPolicy;
+            _colorMapPolicy = colorMapPolicy;
+
+            _graph = graph;
+            _startVertex = startVertex;
+            _colorMap = colorMap;
+        }
+
         public IEnumerator<DfsVertexStep<TVertex>> GetEnumerator()
         {
             DfsSingleComponentVertexEnumerator<
@@ -28,11 +58,18 @@ namespace Ubiquitous.Traversal
 
         public void Reset() => throw new NotImplementedException();
 
-        public DfsVertexStep<TVertex> Current => throw new NotImplementedException();
+        // ReSharper disable once ConvertToAutoPropertyWithPrivateSetter
+        public DfsVertexStep<TVertex> Current => _current;
 
         object IEnumerator.Current => Current;
 
-        public void Dispose() => throw new NotImplementedException();
+        public void Dispose()
+        {
+            _current = default;
+            _state = -1;
+        }
     }
 #pragma warning restore CA1710 // Identifiers should have correct suffix
 }
+
+// ReSharper restore FieldCanBeMadeReadOnly.Local
--- src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.SingleSource.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.SingleSource.cs
@@ -1,14 +1,13 @@
 namespace Ubiquitous.Traversal
 {
-    using System;
-
     public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
     {
         public DfsSingleComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
             TGraphPolicy, TColorMapPolicy> EnumerateVertices(TGraph graph, TVertex startVertex, TColorMap colorMap)
         {
-            throw new NotImplementedException();
+            return new DfsSingleComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+                TGraphPolicy, TColorMapPolicy>(GraphPolicy, ColorMapPolicy, graph, startVertex, colorMap);
         }
     }
 }
