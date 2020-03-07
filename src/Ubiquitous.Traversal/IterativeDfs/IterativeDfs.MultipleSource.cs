28230c0d8fea3fe528ab23bc0dc7b1e94e808ab5
Viktor T <qbit86@gmail.com>
2020-03-07 12:18:50 +0300

    Add EnumerateVertices() stubs

--- src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.MultipleSource.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.MultipleSource.cs
@@ -1,7 +1,25 @@
 namespace Ubiquitous.Traversal
 {
+    using System;
+    using System.Collections.Generic;
+
     public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
     {
+        public DfsSingleComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+            TGraphPolicy, TColorMapPolicy> EnumerateVertices<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            throw new NotImplementedException();
+        }
+
+        public DfsSingleComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+            TGraphPolicy, TColorMapPolicy> EnumerateVertices<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.SingleSource.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.SingleSource.cs
@@ -1,7 +1,14 @@
 namespace Ubiquitous.Traversal
 {
+    using System;
+
     public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
     {
+        public DfsSingleComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+            TGraphPolicy, TColorMapPolicy> EnumerateVertices(TGraph graph, TVertex startVertex, TColorMap colorMap)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
