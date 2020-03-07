9232bb8cb491c19d90b0abc7fd8862245131ac00
Viktor T <qbit86@gmail.com>
2020-03-07 13:53:57 +0300

    Implement interfaces for DfsCrossComponentVertexEnumerator<>

--- src/Ubiquitous.Traversal/IterativeDfs/DfsCrossComponentVertexEnumerator.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/DfsCrossComponentVertexEnumerator.cs
@@ -2,12 +2,14 @@
 
 namespace Ubiquitous.Traversal
 {
+    using System;
+    using System.Collections;
     using System.Collections.Generic;
     using static System.Diagnostics.Debug;
 
 #pragma warning disable CA1710 // Identifiers should have correct suffix
     public struct DfsCrossComponentVertexEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
-        TGraphPolicy, TColorMapPolicy>
+        TGraphPolicy, TColorMapPolicy> : IEnumerable<DfsVertexStep<TVertex>>, IEnumerator<DfsVertexStep<TVertex>>
         where TEdgeEnumerator : IEnumerator<TEdge>
         where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
         IGetTargetPolicy<TGraph, TVertex, TEdge>
@@ -29,6 +31,33 @@ namespace Ubiquitous.Traversal
 
             _graphPolicy = graphPolicy;
             _colorMapPolicy = colorMapPolicy;
+        }
+
+        public IEnumerator<DfsVertexStep<TVertex>> GetEnumerator()
+        {
+            DfsCrossComponentVertexEnumerator<
+                TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy> ator = this;
+            ator.Reset();
+            return ator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        IEnumerator<DfsVertexStep<TVertex>> IEnumerable<DfsVertexStep<TVertex>>.GetEnumerator() => GetEnumerator();
+
+        public bool MoveNext() => throw new NotImplementedException();
+
+        public void Reset() => throw new NotImplementedException();
+
+        // ReSharper disable once ConvertToAutoPropertyWithPrivateSetter
+        public DfsVertexStep<TVertex> Current => _current;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            _current = default;
+            _state = -1;
         }
     }
 #pragma warning restore CA1710 // Identifiers should have correct suffix
