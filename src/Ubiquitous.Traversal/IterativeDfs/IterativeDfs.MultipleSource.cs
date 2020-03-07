8b3453646b75a0f9fb836008f2d0de1afbd5b228
Viktor T <qbit86@gmail.com>
2020-03-07 12:00:26 +0300

    Add partial structs

---
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.MultipleSource.cs
@@ -0,0 +1,7 @@
+namespace Ubiquitous.Traversal
+{
+    public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+    {
+    }
+}
