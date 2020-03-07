e96bf3c515a4402d924457fddc47320753db6a2b
Viktor T <qbit86@gmail.com>
2020-03-07 11:41:36 +0300

    Add IterativeDfs stub

---
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.cs
@@ -0,0 +1,46 @@
+namespace Ubiquitous.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+    {
+#pragma warning disable CA1000 // Do not declare static members on generic types
+        public static IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
+            Create<TGraphPolicy, TColorMapPolicy>(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
+            where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+            IGetTargetPolicy<TGraph, TVertex, TEdge>
+            where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
+        {
+            return new IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                graphPolicy, colorMapPolicy);
+        }
+#pragma warning restore CA1000 // Do not declare static members on generic types
+    }
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+        IGetTargetPolicy<TGraph, TVertex, TEdge>
+        where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
+    {
+        private TGraphPolicy GraphPolicy { get; }
+        private TColorMapPolicy ColorMapPolicy { get; }
+
+        public IterativeDfs(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
+        {
+            if (graphPolicy == null)
+                throw new ArgumentNullException(nameof(graphPolicy));
+
+            if (colorMapPolicy == null)
+                throw new ArgumentNullException(nameof(colorMapPolicy));
+
+            GraphPolicy = graphPolicy;
+            ColorMapPolicy = colorMapPolicy;
+        }
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
