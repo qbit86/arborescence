207f59b898845d05f69d55114f314a66c882b80c
Viktor T <qbit86@gmail.com>
2020-04-25 18:26:33 +0300

    Add couple of methods

--- src/Ubiquitous.Traversal/IterativeDfs/DfsVertexIterator.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/DfsVertexIterator.cs
@@ -4,6 +4,7 @@ namespace Ubiquitous.Traversal
 {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
     using static System.Diagnostics.Debug;
 
     internal struct DfsVertexIterator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
@@ -20,6 +21,7 @@ namespace Ubiquitous.Traversal
         private TGraph _graph;
         private TVertex _currentVertex;
         private TColorMap _colorMap;
+        private List<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>> _stack;
 
         internal DfsVertexIterator(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy,
             TGraph graph, TVertex startVertex, TColorMap colorMap)
@@ -36,6 +38,7 @@ namespace Ubiquitous.Traversal
             _graph = graph;
             _currentVertex = startVertex;
             _colorMap = colorMap;
+            _stack = null;
         }
 
         internal DfsVertexIterator<
@@ -50,19 +53,57 @@ namespace Ubiquitous.Traversal
         internal bool MoveNext()
         {
             Assert(_state > 0, "_state > 0");
-            switch (_state)
+            while (true)
             {
-                case 1:
-                    _current = new DfsVertexStep<TVertex>(DfsStepKind.None, _currentVertex);
-                    _state = 2;
-                    return true;
-                case 2:
-                    _current = new DfsVertexStep<TVertex>(DfsStepKind.DiscoverVertex, _currentVertex);
-                    _state = 3;
-                    return true;
-                default:
-                    throw new NotImplementedException();
+                switch (_state)
+                {
+                    case 1:
+                        _current = new DfsVertexStep<TVertex>(DfsStepKind.None, _currentVertex);
+                        _state = 2;
+                        return true;
+                    case 2:
+                        _current = new DfsVertexStep<TVertex>(DfsStepKind.DiscoverVertex, _currentVertex);
+                        _state = 3;
+                        return true;
+                    case 3:
+                        TEdgeEnumerator edges = _graphPolicy.EnumerateOutEdges(_graph, _currentVertex);
+                        _stack = new List<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>();
+                        PushVertexStackFrame(_currentVertex, edges);
+                        _state = 4;
+                        continue;
+                    default:
+                        throw new NotImplementedException();
+                }
             }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool Terminate(out DfsVertexStep<TVertex> current)
+        {
+            current = new DfsVertexStep<TVertex>(DfsStepKind.None, _currentVertex);
+            _state = -1;
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void PushVertexStackFrame(TVertex vertex, TEdgeEnumerator edgeEnumerator)
+        {
+            var stackFrame = new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(vertex, false, default, edgeEnumerator);
+            _stack.Add(stackFrame);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool TryPopStackFrame(out DfsStackFrame<TVertex, TEdge, TEdgeEnumerator> stackFrame)
+        {
+            if (_stack.Count == 0)
+            {
+                stackFrame = default;
+                return false;
+            }
+
+            stackFrame = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            return true;
         }
     }
 }
