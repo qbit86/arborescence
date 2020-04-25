942485c5e63c91d759ce96e395005222022d5ae7
Viktor T <qbit86@gmail.com>
2020-04-25 14:31:20 +0300

    Implement MoveNext()

--- src/Ubiquitous.Traversal/IterativeDfs/DfsSingleComponentVertexEnumerator.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/DfsSingleComponentVertexEnumerator.cs
@@ -68,7 +68,16 @@ namespace Ubiquitous.Traversal
         {
             ThrowIfNotValid();
 
-            throw new NotImplementedException();
+            Assert(_state > 0, "_state > 0");
+            switch (_state)
+            {
+                case 1:
+                    _state = 2;
+                    return true;
+                default:
+                    _state = 3;
+                    return _vertexIterator.MoveNext();
+            }
         }
 
         public void Reset()
@@ -98,8 +107,8 @@ namespace Ubiquitous.Traversal
 
         public void Dispose()
         {
-            _vertexIterator = default;
             _state = -1;
+            _vertexIterator = default;
         }
 
         private void ThrowIfNotValid()
