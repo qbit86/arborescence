173e6216be5b3ccd61f801dbb5bde4513af6941a
Viktor Tselov <lightmap.tselov@gmail.com>
2020-04-28 01:45:34 +0300

    Implement TryGetSource() and TryGetTarget()

--- src/Ubiquitous.Models/UndirectedAdjacencyListIncidenceGraph.cs
+++ src/Ubiquitous.Models/UndirectedAdjacencyListIncidenceGraph.cs
@@ -5,7 +5,7 @@ namespace Ubiquitous.Models
     using static System.Diagnostics.Debug;
 
     public readonly struct UndirectedAdjacencyListIncidenceGraph :
-        IIncidenceGraph<int, int, ArraySegmentEnumerator<int>>, IEquatable<AdjacencyListIncidenceGraph>
+        IIncidenceGraph<int, int, ArraySegmentEnumerator<int>>, IEquatable<UndirectedAdjacencyListIncidenceGraph>
     {
         private readonly int[] _storage;
 
@@ -24,25 +24,45 @@ namespace Ubiquitous.Models
 
         public int EdgeCount => _storage == null ? 0 : (_storage.Length - 1 - 2 * GetVertexCount()) / 3;
 
-        public bool TryGetSource(int edge, out int source) => throw new NotImplementedException();
+        public bool TryGetSource(int edge, out int source)
+        {
+            int actualEdge = edge < 0 ? ~edge : edge;
+            ReadOnlySpan<int> actualSources = edge < 0 ? GetTargets() : GetSources();
+            if (actualEdge >= actualSources.Length)
+            {
+                source = default;
+                return false;
+            }
 
-        public bool TryGetTarget(int edge, out int target) => throw new NotImplementedException();
+            source = actualSources[actualEdge];
+            return true;
+        }
+
+        public bool TryGetTarget(int edge, out int target)
+        {
+            int actualEdge = edge < 0 ? ~edge : edge;
+            ReadOnlySpan<int> actualTargets = edge < 0 ? GetSources() : GetTargets();
+            if (actualEdge >= actualTargets.Length)
+            {
+                target = default;
+                return false;
+            }
+
+            target = actualTargets[actualEdge];
+            return true;
+        }
 
         public ArraySegmentEnumerator<int> EnumerateOutEdges(int vertex) => throw new NotImplementedException();
 
-        public bool Equals(AdjacencyListIncidenceGraph other) => throw new NotImplementedException();
+        public bool Equals(UndirectedAdjacencyListIncidenceGraph other) => _storage == other._storage;
+
+        public override bool Equals(object obj) => obj is UndirectedAdjacencyListIncidenceGraph other && Equals(other);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private ReadOnlySpan<int> GetSources()
-        {
-            return _storage.AsSpan(1 + 2 * VertexCount + 2 * EdgeCount, EdgeCount);
-        }
+        private ReadOnlySpan<int> GetSources() => throw new NotImplementedException();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private ReadOnlySpan<int> GetTargets()
-        {
-            return _storage.AsSpan(1 + 2 * VertexCount + EdgeCount, EdgeCount);
-        }
+        private ReadOnlySpan<int> GetTargets() => throw new NotImplementedException();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private int GetVertexCount()
