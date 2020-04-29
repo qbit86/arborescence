77dabd793708a6236d3ba5283e6b4fb209fbc5d6
Viktor Tselov <lightmap.tselov@gmail.com>
2020-04-29 09:51:30 +0300

    Add UndirectedAdjacencyListIncidenceGraphBuilder stub

---
+++ src/Ubiquitous.Models/UndirectedAdjacencyListIncidenceGraphBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace Ubiquitous.Models
+{
+    using System;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public struct UndirectedAdjacencyListIncidenceGraphBuilder :
+        IGraphBuilder<UndirectedAdjacencyListIncidenceGraph, int, int>
+    {
+        public bool TryAdd(int source, int target, out int edge) => throw new NotImplementedException();
+
+        public UndirectedAdjacencyListIncidenceGraph ToGraph() => throw new NotImplementedException();
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
