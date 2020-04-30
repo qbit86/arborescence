5795ebaf61a77d69387ce1a5f48aa66c6f3f4cfe
Viktor Tselov <lightmap.tselov@gmail.com>
2020-04-30 09:36:44 +0300

    Add ctor and some properties to UndirectedAdjacencyListIncidenceGraphBuilder

--- src/Ubiquitous.Models/UndirectedAdjacencyListIncidenceGraphBuilder.cs
+++ src/Ubiquitous.Models/UndirectedAdjacencyListIncidenceGraphBuilder.cs
@@ -1,11 +1,52 @@
 ﻿namespace Ubiquitous.Models
 {
     using System;
+    using System.Buffers;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public struct UndirectedAdjacencyListIncidenceGraphBuilder :
         IGraphBuilder<UndirectedAdjacencyListIncidenceGraph, int, int>
     {
+        private const int DefaultInitialOutDegree = 8;
+
+        private int _initialOutDegree;
+        private ArrayBuilder<int> _sources;
+        private ArrayBuilder<int> _targets;
+        private ArrayPrefix<ArrayBuilder<int>> _outEdges;
+
+        public UndirectedAdjacencyListIncidenceGraphBuilder(int initialVertexCount, int edgeCapacity = 0)
+        {
+            if (initialVertexCount < 0)
+                throw new ArgumentOutOfRangeException(nameof(initialVertexCount));
+
+            if (edgeCapacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(edgeCapacity));
+
+            _initialOutDegree = DefaultInitialOutDegree;
+            int effectiveEdgeCapacity = Math.Max(2 * edgeCapacity, DefaultInitialOutDegree);
+            _sources = new ArrayBuilder<int>(effectiveEdgeCapacity);
+            _targets = new ArrayBuilder<int>(effectiveEdgeCapacity);
+            ArrayBuilder<int>[] outEdges = Pool.Rent(initialVertexCount);
+            Array.Clear(outEdges, 0, initialVertexCount);
+            _outEdges = ArrayPrefix.Create(outEdges, initialVertexCount);
+        }
+
+        private static ArrayPool<ArrayBuilder<int>> Pool => ArrayPool<ArrayBuilder<int>>.Shared;
+
+        public int VertexCount => _outEdges.Count;
+
+        public int InitialOutDegree
+        {
+            get => _initialOutDegree <= 0 ? DefaultInitialOutDegree : _initialOutDegree;
+            set => _initialOutDegree = 2 * value;
+        }
+
+        public void EnsureVertexCount(int vertexCount)
+        {
+            if (vertexCount > VertexCount)
+                _outEdges = ArrayPrefixBuilder.Resize(_outEdges, vertexCount, true);
+        }
+
         public bool TryAdd(int source, int target, out int edge) => throw new NotImplementedException();
 
         public UndirectedAdjacencyListIncidenceGraph ToGraph() => throw new NotImplementedException();
