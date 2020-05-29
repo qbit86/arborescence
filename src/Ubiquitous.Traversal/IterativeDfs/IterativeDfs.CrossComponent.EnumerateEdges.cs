ad979ddd2c9f008c1f510e29a225ee063eab9674
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-30 00:31:59 +0300

    Add EnumerateEdges<>() stub

---
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.CrossComponent.EnumerateEdges.cs
@@ -0,0 +1,29 @@
+﻿namespace Ubiquitous.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+
+    public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy,
+        TColorMapPolicy>
+    {
+        public IEnumerator<DfsStep<TEdge>> EnumerateEdges<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<DfsStep<TEdge>> EnumerateEdges<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            throw new NotImplementedException();
+        }
+    }
+}
