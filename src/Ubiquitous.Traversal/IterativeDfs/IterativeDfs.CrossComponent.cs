0e5cf4936396391d0068ebb3f7315e25412b8e86
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-29 21:15:15 +0300

    Add implementation stub via coroutines

--- src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.CrossComponent.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.CrossComponent.cs
@@ -1,29 +1,49 @@
 namespace Ubiquitous.Traversal
 {
+    using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
     {
-        public DfsCrossComponentVertexEnumerator<TGraph, TVertex, TEdge, TVertexEnumerator, TEdgeEnumerator, TColorMap,
-            TGraphPolicy, TColorMapPolicy> EnumerateVertices<TVertexEnumerator>(
+        public IEnumerator<DfsStep<TVertex>> EnumerateVertices<TVertexEnumerator>(
             TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
             where TVertexEnumerator : IEnumerator<TVertex>
         {
-            return new DfsCrossComponentVertexEnumerator<
-                TGraph, TVertex, TEdge, TVertexEnumerator, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
-                GraphPolicy, ColorMapPolicy, graph, vertices, colorMap, false, default);
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            return EnumerateVerticesCore(graph, vertices, colorMap);
         }
 
-        public DfsCrossComponentVertexEnumerator<TGraph, TVertex, TEdge, TVertexEnumerator, TEdgeEnumerator, TColorMap,
-            TGraphPolicy, TColorMapPolicy> EnumerateVertices<TVertexEnumerator>(
+        public IEnumerator<DfsStep<TVertex>> EnumerateVertices<TVertexEnumerator>(
             TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
             where TVertexEnumerator : IEnumerator<TVertex>
         {
-            return new DfsCrossComponentVertexEnumerator<
-                TGraph, TVertex, TEdge, TVertexEnumerator, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
-                GraphPolicy, ColorMapPolicy, graph, vertices, colorMap, true, startVertex);
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            return EnumerateVerticesCore(graph, vertices, colorMap, startVertex);
+        }
+
+        private IEnumerator<DfsStep<TVertex>> EnumerateVerticesCore<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            Debug.Assert(vertices != null, "vertices != null");
+
+            throw new NotImplementedException();
+        }
+
+        private IEnumerator<DfsStep<TVertex>> EnumerateVerticesCore<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            Debug.Assert(vertices != null, "vertices != null");
+
+            throw new NotImplementedException();
         }
     }
 #pragma warning restore CA1815 // Override equals and operator equals on value types
