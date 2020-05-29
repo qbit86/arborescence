9daa8a1b1df1ca81457102538c598d77eab25872
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-30 02:06:13 +0300

    Split SingleComponent

---
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.SingleComponent.EnumerateEdges.cs
@@ -0,0 +1,13 @@
+﻿namespace Ubiquitous.Traversal
+{
+    public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy,
+        TColorMapPolicy>
+    {
+        public DfsSingleComponentEdgeEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+            TGraphPolicy, TColorMapPolicy> EnumerateEdges(TGraph graph, TVertex startVertex, TColorMap colorMap)
+        {
+            return new DfsSingleComponentEdgeEnumerator<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+                TGraphPolicy, TColorMapPolicy>(GraphPolicy, ColorMapPolicy, graph, startVertex, colorMap);
+        }
+    }
+}
