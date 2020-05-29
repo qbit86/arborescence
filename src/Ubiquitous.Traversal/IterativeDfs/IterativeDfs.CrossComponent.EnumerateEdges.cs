9f1586d4346955e62577420b6bd77ba28cff6603
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-30 01:01:48 +0300

    Implement EnumerateEdgesCore<>()

--- src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.CrossComponent.EnumerateEdges.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.CrossComponent.EnumerateEdges.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
 
     public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy,
         TColorMapPolicy>
@@ -30,14 +31,52 @@
             TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
             where TVertexEnumerator : IEnumerator<TVertex>
         {
-            throw new NotImplementedException();
+            Debug.Assert(vertices != null, "vertices != null");
+
+            ColorMapPolicy.Clear(colorMap);
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                Color color = GetColorOrDefault(colorMap, u);
+                if (color != Color.None && color != Color.White)
+                    continue;
+
+                var iterator = new DfsEdgeIterator<
+                    TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                    GraphPolicy, ColorMapPolicy, graph, u, colorMap);
+                while (iterator.MoveNext())
+                    yield return iterator.Current;
+            }
         }
 
         private IEnumerator<DfsStep<TEdge>> EnumerateEdgesCore<TVertexEnumerator>(
             TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
             where TVertexEnumerator : IEnumerator<TVertex>
         {
-            throw new NotImplementedException();
+            Debug.Assert(vertices != null, "vertices != null");
+
+            ColorMapPolicy.Clear(colorMap);
+
+            var startIterator = new DfsEdgeIterator<
+                TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                GraphPolicy, ColorMapPolicy, graph, startVertex, colorMap);
+            while (startIterator.MoveNext())
+                yield return startIterator.Current;
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                Color color = GetColorOrDefault(colorMap, u);
+                if (color != Color.None && color != Color.White)
+                    continue;
+
+                var iterator = new DfsEdgeIterator<
+                    TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                    GraphPolicy, ColorMapPolicy, graph, u, colorMap);
+                while (iterator.MoveNext())
+                    yield return iterator.Current;
+            }
         }
     }
 }
