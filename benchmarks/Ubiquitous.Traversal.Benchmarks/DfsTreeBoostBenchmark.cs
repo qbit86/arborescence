494eb0429d04581a2fc6d4fdc4015fe57e543f9a
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-30 21:18:06 +0300

    Add benchmark for EnumerableDfs

--- benchmarks/Ubiquitous.Traversal.Benchmarks/DfsTreeBoostBenchmark.cs
+++ benchmarks/Ubiquitous.Traversal.Benchmarks/DfsTreeBoostBenchmark.cs
@@ -22,6 +22,10 @@ namespace Ubiquitous
                 IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy>
             InstantDfs { get; set; }
 
+        private IterativeDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy>
+            EnumerableDfs { get; set; }
+
         private Dfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[], IndexedDfsStep,
                 IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy, IndexedDfsStepPolicy>
             DefaultDfs { get; set; }
@@ -33,17 +37,21 @@ namespace Ubiquitous
         {
             Graph = GraphHelper.Default.GetGraph(VertexCount);
 
+            var graphPolicy = default(IndexedAdjacencyListGraphPolicy);
             var colorMapPolicy = default(IndexedColorMapPolicy);
 
             InstantDfs = InstantDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[]>
-                .Create(default(IndexedAdjacencyListGraphPolicy), colorMapPolicy);
+                .Create(graphPolicy, colorMapPolicy);
+
+            EnumerableDfs = IterativeDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[]>
+                .Create(graphPolicy, colorMapPolicy);
 
             DefaultDfs = Dfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[], IndexedDfsStep>
-                .Create(default(IndexedAdjacencyListGraphPolicy), colorMapPolicy, default(IndexedDfsStepPolicy));
+                .Create(graphPolicy, colorMapPolicy, default(IndexedDfsStepPolicy));
         }
 
         [Benchmark(Baseline = true)]
-        public int BaselineDfsTree()
+        public int InstantDfsTree()
         {
             var handler = new DummyDfsHandler<AdjacencyListIncidenceGraph>();
             byte[] colorMap = ArrayPool<byte>.Shared.Rent(Graph.VertexCount);
@@ -51,6 +59,20 @@ namespace Ubiquitous
             InstantDfs.Traverse(Graph, 0, colorMap, handler);
             ArrayPool<byte>.Shared.Return(colorMap);
             return handler.Count;
+        }
+
+        [Benchmark]
+        public int EnumerableDfsTree()
+        {
+            byte[] colorMap = ArrayPool<byte>.Shared.Rent(Graph.VertexCount);
+            Array.Clear(colorMap, 0, colorMap.Length);
+            var steps = EnumerableDfs.EnumerateEdges(Graph, 0, colorMap);
+            int count = 0;
+            foreach (IndexedDfsStep _ in steps)
+                ++count;
+
+            ArrayPool<byte>.Shared.Return(colorMap);
+            return count;
         }
 
         [Benchmark]
