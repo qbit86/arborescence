6bdccf35d3910f8b1599d611e379907247bac284
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-31 13:35:14 +0300

    Move TraverseCore<>() to InstantDfs

--- src/Ubiquitous.Traversal/InstantDfs/InstantDfs.SingleComponent.cs
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.SingleComponent.cs
@@ -1,10 +1,6 @@
 namespace Ubiquitous.Traversal
 {
     using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Runtime.CompilerServices;
-    using Internal;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly partial struct InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
@@ -32,83 +28,6 @@ namespace Ubiquitous.Traversal
             handler.OnStartVertex(graph, startVertex);
             TraverseCore(graph, startVertex, colorMap, handler, terminationCondition ?? s_false);
         }
-
-        private void TraverseCore<THandler>(TGraph graph, TVertex u, TColorMap colorMap, THandler handler,
-            Func<TGraph, TVertex, bool> terminationCondition)
-            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
-        {
-            Debug.Assert(handler != null, "handler != null");
-            Debug.Assert(terminationCondition != null, "terminationCondition != null");
-
-            ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Gray);
-            handler.OnDiscoverVertex(graph, u);
-
-            if (terminationCondition(graph, u))
-            {
-                ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Black);
-                handler.OnFinishVertex(graph, u);
-                return;
-            }
-
-            List<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>> stack =
-                ListCache<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>.Acquire();
-
-            TEdgeEnumerator outEdges = GraphPolicy.EnumerateOutEdges(graph, u);
-            stack.Add(new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(u, false, default, outEdges));
-
-            while (stack.Count > 0)
-            {
-                DfsStackFrame<TVertex, TEdge, TEdgeEnumerator> stackFrame = stack[stack.Count - 1];
-                stack.RemoveAt(stack.Count - 1);
-                u = stackFrame.Vertex;
-                if (stackFrame.HasEdge)
-                    handler.OnFinishEdge(graph, stackFrame.Edge);
-
-                TEdgeEnumerator edges = stackFrame.EdgeEnumerator;
-                while (edges.MoveNext())
-                {
-                    TEdge e = edges.Current;
-                    if (!GraphPolicy.TryGetTarget(graph, e, out TVertex v))
-                        continue;
-
-                    handler.OnExamineEdge(graph, e);
-                    Color color = GetColorOrDefault(colorMap, v);
-                    if (color == Color.None || color == Color.White)
-                    {
-                        handler.OnTreeEdge(graph, e);
-                        stack.Add(new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(u, true, e, edges));
-                        u = v;
-                        ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Gray);
-                        handler.OnDiscoverVertex(graph, u);
-
-                        edges = GraphPolicy.EnumerateOutEdges(graph, u);
-                        if (terminationCondition(graph, u))
-                        {
-                            ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Black);
-                            handler.OnFinishVertex(graph, u);
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        if (color == Color.Gray)
-                            handler.OnBackEdge(graph, e);
-                        else
-                            handler.OnForwardOrCrossEdge(graph, e);
-                        handler.OnFinishEdge(graph, e);
-                    }
-                }
-
-                ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Black);
-                handler.OnFinishVertex(graph, u);
-            }
-
-            ListCache<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>.Release(stack);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private Color GetColorOrDefault(TColorMap colorMap, TVertex vertex) =>
-            ColorMapPolicy.TryGetValue(colorMap, vertex, out Color result) ? result : Color.None;
     }
 #pragma warning restore CA1815 // Override equals and operator equals on value types
 }
--- src/Ubiquitous.Traversal/InstantDfs/InstantDfs.cs
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.cs
@@ -2,6 +2,9 @@ namespace Ubiquitous.Traversal
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Runtime.CompilerServices;
+    using Internal;
 
     public static class InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap>
         where TEdgeEnumerator : IEnumerator<TEdge>
@@ -41,6 +44,83 @@ namespace Ubiquitous.Traversal
             GraphPolicy = graphPolicy;
             ColorMapPolicy = colorMapPolicy;
         }
+
+        private void TraverseCore<THandler>(TGraph graph, TVertex u, TColorMap colorMap, THandler handler,
+            Func<TGraph, TVertex, bool> terminationCondition)
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            Debug.Assert(handler != null, "handler != null");
+            Debug.Assert(terminationCondition != null, "terminationCondition != null");
+
+            ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Gray);
+            handler.OnDiscoverVertex(graph, u);
+
+            if (terminationCondition(graph, u))
+            {
+                ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Black);
+                handler.OnFinishVertex(graph, u);
+                return;
+            }
+
+            List<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>> stack =
+                ListCache<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>.Acquire();
+
+            TEdgeEnumerator outEdges = GraphPolicy.EnumerateOutEdges(graph, u);
+            stack.Add(new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(u, false, default, outEdges));
+
+            while (stack.Count > 0)
+            {
+                DfsStackFrame<TVertex, TEdge, TEdgeEnumerator> stackFrame = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                u = stackFrame.Vertex;
+                if (stackFrame.HasEdge)
+                    handler.OnFinishEdge(graph, stackFrame.Edge);
+
+                TEdgeEnumerator edges = stackFrame.EdgeEnumerator;
+                while (edges.MoveNext())
+                {
+                    TEdge e = edges.Current;
+                    if (!GraphPolicy.TryGetTarget(graph, e, out TVertex v))
+                        continue;
+
+                    handler.OnExamineEdge(graph, e);
+                    Color color = GetColorOrDefault(colorMap, v);
+                    if (color == Color.None || color == Color.White)
+                    {
+                        handler.OnTreeEdge(graph, e);
+                        stack.Add(new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(u, true, e, edges));
+                        u = v;
+                        ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Gray);
+                        handler.OnDiscoverVertex(graph, u);
+
+                        edges = GraphPolicy.EnumerateOutEdges(graph, u);
+                        if (terminationCondition(graph, u))
+                        {
+                            ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Black);
+                            handler.OnFinishVertex(graph, u);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (color == Color.Gray)
+                            handler.OnBackEdge(graph, e);
+                        else
+                            handler.OnForwardOrCrossEdge(graph, e);
+                        handler.OnFinishEdge(graph, e);
+                    }
+                }
+
+                ColorMapPolicy.AddOrUpdate(colorMap, u, Color.Black);
+                handler.OnFinishVertex(graph, u);
+            }
+
+            ListCache<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>.Release(stack);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Color GetColorOrDefault(TColorMap colorMap, TVertex vertex) =>
+            ColorMapPolicy.TryGetValue(colorMap, vertex, out Color result) ? result : Color.None;
     }
 #pragma warning restore CA1815 // Override equals and operator equals on value types
 }
