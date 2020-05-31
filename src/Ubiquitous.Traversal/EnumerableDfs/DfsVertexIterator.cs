98ca91bba9a265aaa5bb18bc883b6d5a407d8dc0
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-31 20:52:26 +0300

    Rename directory to EnumerableDfs

---
+++ src/Ubiquitous.Traversal/EnumerableDfs/DfsVertexIterator.cs
@@ -0,0 +1,226 @@
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+
+namespace Ubiquitous.Traversal
+{
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using Internal;
+    using static System.Diagnostics.Debug;
+
+    // States:
+    // • -2: "After" — the iterator has finished
+    // • -1: disposed
+    // • 0: not initialized
+    // • 1: "Before" — MoveNext() hasn't been called yet
+    // • Anything positive: indicates where to resume from
+    // --- https://csharpindepth.com/Articles/IteratorBlockImplementation
+
+    internal struct DfsVertexIterator<
+        TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+        IGetTargetPolicy<TGraph, TVertex, TEdge>
+        where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
+    {
+        private TGraphPolicy _graphPolicy;
+        private TColorMapPolicy _colorMapPolicy;
+        private TGraph _graph;
+        private TColorMap _colorMap;
+
+        private DfsStep<TVertex> _current;
+        private TEdgeEnumerator _edgeEnumerator; // Corresponds to iterator range in Boost implementation.
+        private TVertex _neighborVertex; // Corresponds to `v` in Boost implementation.
+        private TVertex _currentVertex; // Corresponds to `u` in Boost implementation.
+        private List<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>> _stack;
+        private int _state;
+
+        internal DfsVertexIterator(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy,
+            TGraph graph, TVertex startVertex, TColorMap colorMap)
+        {
+            Assert(graphPolicy != null, "graphPolicy != null");
+            Assert(colorMapPolicy != null, "colorMapPolicy != null");
+
+            _graphPolicy = graphPolicy;
+            _colorMapPolicy = colorMapPolicy;
+            _graph = graph;
+            _colorMap = colorMap;
+
+            _current = new DfsStep<TVertex>(DfsStepKind.None, startVertex);
+            _edgeEnumerator = default;
+            _neighborVertex = default;
+            _currentVertex = startVertex;
+            _stack = default;
+            _state = 1;
+        }
+
+        internal DfsStep<TVertex> Current => _current;
+
+        internal bool MoveNext()
+        {
+            if (_state <= 0)
+                return false;
+
+            // With `while (true)` we can avoid `goto label`,
+            // simulating the latter with `_state = label; continue;`.
+            while (true)
+            {
+                switch (_state)
+                {
+                    case 1:
+                        return UpdateColorMapAndCreateDiscoverVertexStep(_currentVertex, 2);
+                    case 2:
+                        EnsureStackInitialized();
+                        TEdgeEnumerator edges = _graphPolicy.EnumerateOutEdges(_graph, _currentVertex);
+                        PushVertexStackFrame(_currentVertex, edges);
+                        _state = 3;
+                        continue;
+                    case 3:
+                        if (!TryPopStackFrame(out DfsStackFrame<TVertex, TEdge, TEdgeEnumerator> stackFrame))
+                            return Terminate();
+
+                        _currentVertex = stackFrame.Vertex;
+                        _edgeEnumerator = stackFrame.EdgeEnumerator;
+                        _state = 4;
+                        continue;
+                    case 4:
+                        if (!_edgeEnumerator.MoveNext())
+                            return UpdateColorMapAndCreateFinishVertexStep(_currentVertex, 3);
+
+                        if (!_graphPolicy.TryGetTarget(_graph, _edgeEnumerator.Current, out _neighborVertex))
+                            continue;
+
+                        _state = 5;
+                        continue;
+                    case 5:
+                        Color neighborColor = GetColorOrDefault(_colorMap, _neighborVertex);
+                        switch (neighborColor)
+                        {
+                            case Color.None:
+                            case Color.White:
+                                _state = 6;
+                                continue;
+                            case Color.Gray:
+                                _state = 4;
+                                continue;
+                            default:
+                                _state = 4;
+                                continue;
+                        }
+                    case 6:
+                        PushEdgeStackFrame(_currentVertex, _edgeEnumerator.Current, _edgeEnumerator);
+                        _currentVertex = _neighborVertex;
+                        return UpdateColorMapAndCreateDiscoverVertexStep(_currentVertex, 7);
+                    case 7:
+                        _edgeEnumerator = _graphPolicy.EnumerateOutEdges(_graph, _currentVertex);
+                        _state = 4;
+                        continue;
+                }
+
+                return Terminate();
+            }
+        }
+
+        internal void Reset(TVertex startVertex)
+        {
+            Assert(_state > 0, "_state > 0");
+
+            _current = new DfsStep<TVertex>(DfsStepKind.None, startVertex);
+            _edgeEnumerator = default;
+            _neighborVertex = default;
+            _currentVertex = startVertex;
+            EnsureStackReleased();
+            _state = 1;
+        }
+
+        internal void Dispose()
+        {
+            if (_state == -1)
+                return;
+
+            _current = default;
+            _edgeEnumerator = default;
+            _neighborVertex = default;
+            _currentVertex = default;
+            EnsureStackReleased();
+            _state = -1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool UpdateColorMapAndCreateDiscoverVertexStep(TVertex vertex, int newState)
+        {
+            _colorMapPolicy.AddOrUpdate(_colorMap, _currentVertex, Color.Gray);
+            _current = new DfsStep<TVertex>(DfsStepKind.DiscoverVertex, vertex);
+            _state = newState;
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool UpdateColorMapAndCreateFinishVertexStep(TVertex vertex, int newState)
+        {
+            _colorMapPolicy.AddOrUpdate(_colorMap, _currentVertex, Color.Black);
+            _current = new DfsStep<TVertex>(DfsStepKind.FinishVertex, vertex);
+            _state = newState;
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool Terminate()
+        {
+            _current = new DfsStep<TVertex>(DfsStepKind.None, _currentVertex);
+            _state = -2;
+            EnsureStackReleased();
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void EnsureStackInitialized()
+        {
+            if (_stack is null)
+                _stack = ListCache<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>.Acquire();
+            else
+                _stack.Clear();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void EnsureStackReleased()
+        {
+            if (_stack != null)
+                ListCache<DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>>.Release(_stack);
+            _stack = default;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void PushVertexStackFrame(TVertex vertex, TEdgeEnumerator edgeEnumerator)
+        {
+            var stackFrame = new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(vertex, false, default, edgeEnumerator);
+            _stack.Add(stackFrame);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void PushEdgeStackFrame(TVertex vertex, TEdge edge, TEdgeEnumerator edgeEnumerator)
+        {
+            var stackFrame = new DfsStackFrame<TVertex, TEdge, TEdgeEnumerator>(vertex, true, edge, edgeEnumerator);
+            _stack.Add(stackFrame);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool TryPopStackFrame(out DfsStackFrame<TVertex, TEdge, TEdgeEnumerator> stackFrame)
+        {
+            if (_stack.Count == 0)
+            {
+                stackFrame = default;
+                return false;
+            }
+
+            stackFrame = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Color GetColorOrDefault(TColorMap colorMap, TVertex vertex) =>
+            _colorMapPolicy.TryGetValue(colorMap, vertex, out Color result) ? result : Color.None;
+    }
+}
+
+// ReSharper restore FieldCanBeMadeReadOnly.Local
---
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.CrossComponent.EnumerateVertices.cs
@@ -0,0 +1,87 @@
+namespace Ubiquitous.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly partial struct EnumerableDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+    {
+        public IEnumerator<DfsStep<TVertex>> EnumerateVertices<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            return EnumerateVerticesCore(graph, vertices, colorMap);
+        }
+
+        public IEnumerator<DfsStep<TVertex>> EnumerateVertices<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            return EnumerateVerticesCore(graph, vertices, colorMap, startVertex);
+        }
+
+        private IEnumerator<DfsStep<TVertex>> EnumerateVerticesCore<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            Debug.Assert(vertices != null, "vertices != null");
+
+            ColorMapPolicy.Clear(colorMap);
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                Color color = GetColorOrDefault(colorMap, u);
+                if (color != Color.None && color != Color.White)
+                    continue;
+
+                yield return new DfsStep<TVertex>(DfsStepKind.StartVertex, u);
+                var iterator = new DfsVertexIterator<
+                    TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                    GraphPolicy, ColorMapPolicy, graph, u, colorMap);
+                while (iterator.MoveNext())
+                    yield return iterator.Current;
+            }
+        }
+
+        private IEnumerator<DfsStep<TVertex>> EnumerateVerticesCore<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, TVertex startVertex)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            Debug.Assert(vertices != null, "vertices != null");
+
+            ColorMapPolicy.Clear(colorMap);
+
+            yield return new DfsStep<TVertex>(DfsStepKind.StartVertex, startVertex);
+            var startIterator = new DfsVertexIterator<
+                TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                GraphPolicy, ColorMapPolicy, graph, startVertex, colorMap);
+            while (startIterator.MoveNext())
+                yield return startIterator.Current;
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                Color color = GetColorOrDefault(colorMap, u);
+                if (color != Color.None && color != Color.White)
+                    continue;
+
+                yield return new DfsStep<TVertex>(DfsStepKind.StartVertex, u);
+                var vertexIterator = new DfsVertexIterator<
+                    TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                    GraphPolicy, ColorMapPolicy, graph, u, colorMap);
+                while (vertexIterator.MoveNext())
+                    yield return vertexIterator.Current;
+            }
+        }
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
