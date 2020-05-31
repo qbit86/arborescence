4f9ef8b4e683941944cdb983df1f94291e2350a8
Viktor Tselov <lightmap.tselov@gmail.com>
2020-05-31 20:35:14 +0300

    Move static class IterativeDfs<> to separate file

--- src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.cs
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs.cs
@@ -4,22 +4,6 @@ namespace Ubiquitous.Traversal
     using System.Collections.Generic;
     using System.Runtime.CompilerServices;
 
-    public static class IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap>
-        where TEdgeEnumerator : IEnumerator<TEdge>
-    {
-#pragma warning disable CA1000 // Do not declare static members on generic types
-        public static IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
-            Create<TGraphPolicy, TColorMapPolicy>(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
-            where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
-            IGetTargetPolicy<TGraph, TVertex, TEdge>
-            where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
-        {
-            return new IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
-                graphPolicy, colorMapPolicy);
-        }
-#pragma warning restore CA1000 // Do not declare static members on generic types
-    }
-
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly partial struct IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
         TGraphPolicy, TColorMapPolicy>
---
+++ src/Ubiquitous.Traversal/IterativeDfs/IterativeDfs_5.cs
@@ -0,0 +1,20 @@
+﻿namespace Ubiquitous.Traversal
+{
+    using System.Collections.Generic;
+
+    public static class IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+    {
+#pragma warning disable CA1000 // Do not declare static members on generic types
+        public static IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
+            Create<TGraphPolicy, TColorMapPolicy>(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
+            where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>,
+            IGetTargetPolicy<TGraph, TVertex, TEdge>
+            where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
+        {
+            return new IterativeDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
+                graphPolicy, colorMapPolicy);
+        }
+#pragma warning restore CA1000 // Do not declare static members on generic types
+    }
+}
