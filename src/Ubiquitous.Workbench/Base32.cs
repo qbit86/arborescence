0ac3b48ab4ae2470b30f91d096ff359d1d5d3518
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-02 01:08:33 +0300

    Add stub for Base32.TryParse()

--- src/Ubiquitous.Workbench/Base32.cs
+++ src/Ubiquitous.Workbench/Base32.cs
@@ -7,6 +7,7 @@
     public static class Base32
     {
         private const string Alphabet = "abcdefghijklmnopqrstuvwxyz234567";
+        private const int MaxLength = 7;
 
         public static string ToString(int value)
         {
@@ -14,7 +15,7 @@
                 return "a";
 
             uint remainingBits = (uint)value;
-            Span<char> buffer = stackalloc char[7];
+            Span<char> buffer = stackalloc char[MaxLength];
             int length = 0;
             for (int i = 0; i != buffer.Length; ++i)
             {
@@ -31,6 +32,35 @@
             Span<char> span = buffer.Slice(0, length);
             span.Reverse();
             return span.ToString();
+        }
+
+        public static bool TryParse(ReadOnlySpan<char> value, out int result)
+        {
+            ReadOnlySpan<char> input = value.Length > MaxLength
+                ? value.Slice(value.Length - MaxLength, MaxLength)
+                : value;
+            Span<byte> buffer = stackalloc byte[MaxLength];
+
+            for (int i = 0; i != input.Length; ++i)
+            {
+                char c = char.ToLowerInvariant(input[i]);
+                if (c >= 'a' && c <= 'z')
+                {
+                    buffer[i] = (byte)(c - 'a');
+                    continue;
+                }
+
+                if (c >= '2' && c <= '7')
+                {
+                    buffer[i] = (byte)(26 + c - '2');
+                    continue;
+                }
+
+                result = default;
+                return false;
+            }
+
+            throw new NotImplementedException();
         }
     }
 }
