ca41b75aa024b4f87d856f0759222ab4286c6af5
Viktor T <qbit86@gmail.com>
2020-06-05 22:41:34 +0300

    Fix build warning

--- src/Ubiquitous.Collections/IContainer.cs
+++ src/Ubiquitous.Collections/IContainer.cs
@@ -1,9 +1,11 @@
 ﻿namespace Ubiquitous.Collections
 {
+    using System.Diagnostics.CodeAnalysis;
+
     public interface IContainer<T>
     {
         bool IsEmpty { get; }
         void Add(T item);
-        bool TryTake(out T result);
+        bool TryTake([MaybeNullWhen(false)] out T result);
     }
 }
