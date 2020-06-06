7db269479310b783bb0a632c4aae12c94feb4950
Viktor T <qbit86@gmail.com>
2020-06-06 23:50:15 +0300

    Fix Internal.Stack<> to support default initialization

--- src/Ubiquitous.Internal/Stack.cs
+++ src/Ubiquitous.Internal/Stack.cs
@@ -12,29 +12,23 @@ namespace Ubiquitous.Internal
         private T[] _arrayFromPool;
         private int _count;
 
-        private Stack(T[] array)
-        {
-            Debug.Assert(array != null, "array != null");
-            _arrayFromPool = array;
-            _count = 0;
-        }
-
         private static ArrayPool<T> Pool => ArrayPool<T>.Shared;
-
-        internal static Stack<T> Create() => new Stack<T>(Array.Empty<T>());
 
         public void Dispose()
         {
             _count = 0;
-            if (_arrayFromPool.Length == 0)
+            if (_arrayFromPool is null)
                 return;
 
             Pool.Return(_arrayFromPool, ShouldClear());
-            _arrayFromPool = Array.Empty<T>();
+            _arrayFromPool = null;
         }
 
         internal void Add(T item)
         {
+            if (_arrayFromPool is null)
+                _arrayFromPool = Pool.Rent(DefaultCapacity);
+
             int count = _count;
             T[] array = _arrayFromPool;
 
@@ -52,7 +46,7 @@ namespace Ubiquitous.Internal
         internal bool TryTake(out T result)
         {
             int newCount = _count - 1;
-            T[] array = _arrayFromPool;
+            T[] array = _arrayFromPool ?? Array.Empty<T>();
 
             if ((uint)newCount >= (uint)array.Length)
             {
@@ -71,13 +65,15 @@ namespace Ubiquitous.Internal
         [MethodImpl(MethodImplOptions.NoInlining)]
         private void ResizeThenAdd(T item)
         {
+            Debug.Assert(_arrayFromPool != null, "_arrayFromPool != null");
+            Debug.Assert(_arrayFromPool.Length > 0, "_arrayFromPool.Length > 0");
+
             int count = _count;
-            int newCapacity = count == 0 ? DefaultCapacity : count << 1;
+            int newCapacity = count << 1;
             T[] newArray = Pool.Rent(newCapacity);
             Array.Copy(_arrayFromPool, newArray, count);
             newArray[count] = item;
-            if (_arrayFromPool.Length != 0)
-                Pool.Return(_arrayFromPool, ShouldClear());
+            Pool.Return(_arrayFromPool, ShouldClear());
             _arrayFromPool = newArray;
             _count = count + 1;
         }
