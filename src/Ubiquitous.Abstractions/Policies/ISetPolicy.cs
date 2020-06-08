6ebdb88615a265420c3c0f2a9f37d8285dfb79fe
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-09 02:57:51 +0300

    Add ISetPolicy and IndexedSetPolicy

---
+++ src/Ubiquitous.Abstractions/Policies/ISetPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace Ubiquitous
+{
+    public interface ISetPolicy<in TSet, in TItem>
+    {
+        bool Contains(TSet items, TItem item);
+        void Add(TSet items, TItem item);
+        void Clear(TSet items);
+    }
+}
---
+++ src/Ubiquitous.Traversal/IndexedSetPolicy.cs
@@ -0,0 +1,27 @@
+﻿namespace Ubiquitous.Traversal
+{
+    using System.Collections;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly struct IndexedSetPolicy : ISetPolicy<BitArray, int>
+    {
+        public bool Contains(BitArray items, int item)
+        {
+            if (items == null || (uint)item >= (uint)items.Length)
+                return false;
+
+            return items.Get(item);
+        }
+
+        public void Add(BitArray items, int item)
+        {
+            if (items == null || (uint)item >= (uint)items.Length)
+                return;
+
+            items.Set(item, true);
+        }
+
+        public void Clear(BitArray items) => items?.SetAll(false);
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
