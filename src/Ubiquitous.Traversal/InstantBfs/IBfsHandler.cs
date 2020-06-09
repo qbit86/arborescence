809ba1b1e5834e9d6a6e01796ceb0905d433d26f
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-09 03:20:04 +0300

    Add IBfsHandler

---
+++ src/Ubiquitous.Traversal/InstantBfs/IBfsHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Ubiquitous.Traversal
+{
+    public interface IBfsHandler<in TGraph, in TVertex, in TEdge>
+    {
+        void OnDiscoverVertex(TGraph g, TVertex v);
+        void OnExamineVertex(TGraph g, TVertex v);
+        void OnFinishVertex(TGraph g, TVertex v);
+        void OnExamineEdge(TGraph g, TEdge e);
+        void OnTreeEdge(TGraph g, TEdge e);
+        void OnNonTreeGrayHeadEdge(TGraph g, TEdge e);
+        void OnNonTreeBlackHeadEdge(TGraph g, TEdge e);
+    }
+}
