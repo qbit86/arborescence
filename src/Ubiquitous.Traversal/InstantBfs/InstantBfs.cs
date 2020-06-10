39010c083c1823d2b253d71566f676c8b5a961d5
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-10 03:01:31 +0300

    Add InstantBfs stub

---
+++ src/Ubiquitous.Traversal/InstantBfs/InstantBfs.cs
@@ -0,0 +1,29 @@
+﻿namespace Ubiquitous.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly struct InstantBfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>, IHeadPolicy<TGraph, TVertex, TEdge>
+        where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
+    {
+        private TGraphPolicy GraphPolicy { get; }
+        private TColorMapPolicy ColorMapPolicy { get; }
+
+        public InstantBfs(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
+        {
+            if (graphPolicy == null)
+                throw new ArgumentNullException(nameof(graphPolicy));
+
+            if (colorMapPolicy == null)
+                throw new ArgumentNullException(nameof(colorMapPolicy));
+
+            GraphPolicy = graphPolicy;
+            ColorMapPolicy = colorMapPolicy;
+        }
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
