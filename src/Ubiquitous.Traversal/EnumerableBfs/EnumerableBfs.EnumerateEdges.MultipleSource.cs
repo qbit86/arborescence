3af8de75004e9e7c50752cf18d4c4a7d3a1bba5f
Viktor T <qbit86@gmail.com>
2020-06-14 02:24:48 +0300

    Add multiple-source EnumerateEdges()

---
+++ src/Ubiquitous.Traversal/EnumerableBfs/EnumerableBfs.EnumerateEdges.MultipleSource.cs
@@ -0,0 +1,36 @@
+namespace Ubiquitous.Traversal
+{
+    using System.Collections.Generic;
+    using Collections;
+    using Internal;
+
+    // ReSharper disable UnusedTypeParameter
+    public readonly partial struct EnumerableBfs<
+        TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy, TExploredSetPolicy>
+    {
+        public IEnumerator<TEdge> EnumerateEdges<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator sources, TExploredSet exploredSet)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            Queue<TVertex> queue = QueueCache<TVertex>.Acquire();
+            try
+            {
+                var queueAdapter = new QueueAdapter<TVertex>(queue);
+
+                while (sources.MoveNext())
+                {
+                    TVertex s = sources.Current;
+                    ExploredSetPolicy.Add(exploredSet, s);
+                    queueAdapter.Add(s);
+                }
+
+                return EnumerateEdgesCore(graph, queueAdapter, exploredSet);
+            }
+            finally
+            {
+                QueueCache<TVertex>.Release(queue);
+            }
+        }
+    }
+    // ReSharper restore UnusedTypeParameter
+}
