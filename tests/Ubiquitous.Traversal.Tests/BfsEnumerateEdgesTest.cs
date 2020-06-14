fa1fc628ff913f5fe31fa5bd6be98c3da2c741ba
Viktor T <qbit86@gmail.com>
2020-06-14 19:55:11 +0300

    Add testing for single source BFS

--- tests/Ubiquitous.Traversal.Tests/BfsEnumerateEdgesTest.cs
+++ tests/Ubiquitous.Traversal.Tests/BfsEnumerateEdgesTest.cs
@@ -1,9 +1,16 @@
 namespace Ubiquitous
 {
+    using System;
+    using System.Buffers;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using Misnomer;
     using Models;
     using Traversal;
+    using Workbench;
+    using Xunit;
     using EdgeEnumerator = ArraySegmentEnumerator<int>;
     using IndexedAdjacencyListGraphPolicy =
         Models.IndexedIncidenceGraphPolicy<Models.AdjacencyListIncidenceGraph, ArraySegmentEnumerator<int>>;
@@ -34,5 +41,96 @@ namespace Ubiquitous
             EnumerableBfs { get; }
 
         public static IEnumerable<object[]> TestCases => s_testCases ??= GraphHelper.CreateTestCases();
+
+        private void EnumerableEdgesSingleSourceCore(AdjacencyListIncidenceGraph graph)
+        {
+            Debug.Assert(graph != null, "graph != null");
+
+            // Arrange
+
+            Debug.Assert(graph.VertexCount > 0, "graph.VertexCount > 0");
+            int source = graph.VertexCount - 1;
+
+            byte[] instantColorMap = ArrayPool<byte>.Shared.Rent(graph.VertexCount);
+            Array.Clear(instantColorMap, 0, instantColorMap.Length);
+            var enumerableExploredSet = new BitArray(graph.VertexCount);
+
+            var instantSteps = new Rist<int>(graph.VertexCount);
+            var enumerableSteps = new Rist<int>(graph.VertexCount);
+            BfsHandler<AdjacencyListIncidenceGraph, int, int> bfsHandler = CreateBfsHandler(instantSteps);
+
+            // Act
+
+            InstantBfs.Traverse(graph, source, instantColorMap, bfsHandler);
+            IEnumerator<int> edges = EnumerableBfs.EnumerateEdges(graph, source, enumerableExploredSet);
+            while (edges.MoveNext())
+                enumerableSteps.Add(edges.Current);
+
+            // Assert
+
+            int instantStepCount = instantSteps.Count;
+            int enumerableStepCount = enumerableSteps.Count;
+            Assert.Equal(instantStepCount, enumerableStepCount);
+
+            int count = instantStepCount;
+            for (int i = 0; i != count; ++i)
+            {
+                int instantStep = instantSteps[i];
+                int enumerableStep = enumerableSteps[i];
+
+                if (instantStep == enumerableStep)
+                    continue;
+
+                Assert.Equal(instantStep, enumerableStep);
+            }
+
+            // Cleanup
+
+            enumerableSteps.Dispose();
+            instantSteps.Dispose();
+            ArrayPool<byte>.Shared.Return(instantColorMap);
+        }
+
+        private static BfsHandler<AdjacencyListIncidenceGraph, int, int> CreateBfsHandler(IList<int> treeEdges)
+        {
+            Debug.Assert(treeEdges != null, "treeEdges != null");
+
+            var result = new BfsHandler<AdjacencyListIncidenceGraph, int, int>();
+            result.TreeEdge += (g, e) => treeEdges.Add(e);
+            return result;
+        }
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+        [Theory]
+        [CombinatorialData]
+        public void Enumerable_edges_single_source_combinatorial(
+            [CombinatorialValues(1, 10, 100)] int vertexCount,
+            [CombinatorialValues(1.0, 1.414, 1.618, 2.0)]
+            double densityPower)
+        {
+            AdjacencyListIncidenceGraph graph = GraphHelper.GenerateAdjacencyListIncidenceGraph(
+                vertexCount, densityPower);
+            EnumerableEdgesSingleSourceCore(graph);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Enumerable_edges_single_source_member(string testCase)
+        {
+            Assert.NotNull(testCase);
+            var builder = new AdjacencyListIncidenceGraphBuilder(10);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader(testCase))
+            {
+                Assert.NotEqual(TextReader.Null, textReader);
+                IEnumerable<Endpoints<int>> edges = IndexedEdgeListParser.ParseEdges(textReader);
+                foreach (Endpoints<int> edge in edges)
+                    builder.TryAdd(edge.Tail, edge.Head, out _);
+            }
+
+            AdjacencyListIncidenceGraph graph = builder.ToGraph();
+            EnumerableEdgesSingleSourceCore(graph);
+        }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
     }
 }
