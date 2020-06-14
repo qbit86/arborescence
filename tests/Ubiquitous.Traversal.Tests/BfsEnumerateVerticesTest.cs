130cf682098df127730364272056181bf4f60fe1
Viktor T <qbit86@gmail.com>
2020-06-14 23:03:52 +0300

    Add BfsEnumerateVerticesTest stub

---
+++ tests/Ubiquitous.Traversal.Tests/BfsEnumerateVerticesTest.cs
@@ -0,0 +1,38 @@
+namespace Ubiquitous
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Models;
+    using Traversal;
+    using EdgeEnumerator = ArraySegmentEnumerator<int>;
+    using IndexedAdjacencyListGraphPolicy =
+        Models.IndexedIncidenceGraphPolicy<Models.AdjacencyListIncidenceGraph, ArraySegmentEnumerator<int>>;
+
+    public sealed class BfsEnumerateVerticesTest
+    {
+        private static IEnumerable<object[]> s_testCases;
+
+        public BfsEnumerateVerticesTest()
+        {
+            IndexedAdjacencyListGraphPolicy graphPolicy = default;
+            IndexedColorMapPolicy colorMapPolicy = default;
+
+            InstantBfs = InstantBfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[]>.Create(
+                graphPolicy, colorMapPolicy);
+
+            IndexedSetPolicy exploredSetPolicy = default;
+            EnumerableBfs = EnumerableBfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, BitArray>.Create(
+                graphPolicy, exploredSetPolicy);
+        }
+
+        private InstantBfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy>
+            InstantBfs { get; }
+
+        private EnumerableBfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, BitArray,
+                IndexedAdjacencyListGraphPolicy, IndexedSetPolicy>
+            EnumerableBfs { get; }
+
+        public static IEnumerable<object[]> TestCases => s_testCases ??= GraphHelper.CreateTestCases();
+    }
+}
