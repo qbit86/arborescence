252a76ddbfc1ddaf5f9ea1f7f7d7e36059d32e5b
Viktor T <qbit86@gmail.com>
2020-06-17 01:23:01 +0300

    Guard Queue<T> with namespace alias

--- src/Ubiquitous.Internal/QueueCache.cs
+++ src/Ubiquitous.Internal/QueueCache.cs
@@ -1,26 +1,26 @@
 ﻿namespace Ubiquitous.Internal
 {
     using System;
-    using System.Collections.Generic;
+    using Scg = System.Collections.Generic;
 
     internal static class QueueCache<T>
     {
-        [ThreadStatic] private static WeakReference<Queue<T>> s_cachedInstanceReference;
+        [ThreadStatic] private static WeakReference<Scg.Queue<T>> s_cachedInstanceReference;
 
-        private static WeakReference<Queue<T>> CachedInstanceReference => s_cachedInstanceReference
-            ?? (s_cachedInstanceReference = new WeakReference<Queue<T>>(null, false));
+        private static WeakReference<Scg.Queue<T>> CachedInstanceReference => s_cachedInstanceReference
+            ?? (s_cachedInstanceReference = new WeakReference<Scg.Queue<T>>(null, false));
 
-        public static Queue<T> Acquire(int desiredCapacity = 0)
+        public static Scg.Queue<T> Acquire(int desiredCapacity = 0)
         {
             if (desiredCapacity > 0)
-                return new Queue<T>(desiredCapacity);
+                return new Scg.Queue<T>(desiredCapacity);
 
-            bool isCached = CachedInstanceReference.TryGetTarget(out Queue<T> result);
+            bool isCached = CachedInstanceReference.TryGetTarget(out Scg.Queue<T> result);
             CachedInstanceReference.SetTarget(null);
-            return isCached ? result : new Queue<T>();
+            return isCached ? result : new Scg.Queue<T>();
         }
 
-        public static void Release(Queue<T> queue)
+        public static void Release(Scg.Queue<T> queue)
         {
             if (queue == null)
                 throw new ArgumentNullException(nameof(queue));
