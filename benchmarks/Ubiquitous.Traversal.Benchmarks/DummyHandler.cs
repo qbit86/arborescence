e8b556b40ed3958cf3df3889c026d03c93d1bdc3
Viktor T <qbit86@gmail.com>
2020-06-16 21:11:06 +0300

    Cleanup DummyHandler<TGraph>

--- benchmarks/Ubiquitous.Traversal.Benchmarks/DummyHandler.cs
+++ benchmarks/Ubiquitous.Traversal.Benchmarks/DummyHandler.cs
@@ -1,18 +1,20 @@
 namespace Ubiquitous
 {
-    using System;
     using Traversal;
 
-    internal sealed class DummyHandler<TGraph> :
-        IDfsHandler<TGraph, int, int>, IBfsHandler<TGraph, int, int>, IDisposable
+    internal sealed class DummyHandler<TGraph> : IDfsHandler<TGraph, int, int>, IBfsHandler<TGraph, int, int>
     {
         internal int Count { get; private set; }
+
+        public void OnExamineVertex(TGraph g, int v) => ++Count;
+
+        public void OnNonTreeGrayHeadEdge(TGraph g, int e) => ++Count;
+
+        public void OnNonTreeBlackHeadEdge(TGraph g, int e) => ++Count;
 
         public void OnStartVertex(TGraph g, int v) => ++Count;
 
         public void OnDiscoverVertex(TGraph g, int v) => ++Count;
-
-        public void OnExamineVertex(TGraph g, int v) => ++Count;
 
         public void OnFinishVertex(TGraph g, int v) => ++Count;
 
@@ -20,19 +22,12 @@ namespace Ubiquitous
 
         public void OnTreeEdge(TGraph g, int e) => ++Count;
 
-        public void OnNonTreeGrayHeadEdge(TGraph g, int e) => ++Count;
-
-        public void OnNonTreeBlackHeadEdge(TGraph g, int e) => ++Count;
-
         public void OnBackEdge(TGraph g, int e) => ++Count;
 
         public void OnForwardOrCrossEdge(TGraph g, int e) => ++Count;
 
         public void OnFinishEdge(TGraph g, int e) => ++Count;
 
-        public void Dispose()
-        {
-            Count = 0;
-        }
+        public void Reset() => Count = 0;
     }
 }
