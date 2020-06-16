e5d001ff3a4b163af313c9a4ee87267653322e5d
Viktor T <qbit86@gmail.com>
2020-06-16 21:47:17 +0300

    Add IndexedSetPolicy

---
+++ src/Ubiquitous.Traversal/IndexedSetPolicy.cs
@@ -0,0 +1,33 @@
+﻿namespace Ubiquitous.Traversal
+{
+    using System;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly struct IndexedSetPolicy : ISetPolicy<byte[], int>
+    {
+        public bool Contains(byte[] items, int item)
+        {
+            if (items == null || (uint)item >= (uint)items.Length)
+                return false;
+
+            return items[item] != 0;
+        }
+
+        public void Add(byte[] items, int item)
+        {
+            if (items == null || (uint)item >= (uint)items.Length)
+                return;
+
+            items[item] = 0x01;
+        }
+
+        public void Clear(byte[] items)
+        {
+            if (items == null)
+                return;
+
+            Array.Clear(items, 0, items.Length);
+        }
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
