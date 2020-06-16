0bd40dcf41ffae8309ca4ac75b4a052c6b9b56ad
Viktor T <qbit86@gmail.com>
2020-06-16 21:34:47 +0300

    Set running BfsBenchmark by default

--- benchmarks/Ubiquitous.Traversal.Benchmarks/Program.cs
+++ benchmarks/Ubiquitous.Traversal.Benchmarks/Program.cs
@@ -16,7 +16,7 @@
             IConfig config = ManualConfig.Create(DefaultConfig.Instance)
                 .AddJob(job);
 
-            Summary _ = BenchmarkRunner.Run<DfsBenchmark>(config);
+            Summary _ = BenchmarkRunner.Run<BfsBenchmark>(config);
         }
     }
 }
