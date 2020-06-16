0b84a51b2457154a3cec912d20e0234187f5e05f
Viktor T <qbit86@gmail.com>
2020-06-16 22:23:39 +0300

    Add bit-twiddling

--- src/Ubiquitous.Traversal/IndexedSetPolicy.cs
+++ src/Ubiquitous.Traversal/IndexedSetPolicy.cs
@@ -1,16 +1,32 @@
 ﻿namespace Ubiquitous.Traversal
 {
     using System;
+    using System.Runtime.CompilerServices;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly struct IndexedSetPolicy : ISetPolicy<byte[], int>
     {
+        private const int BitShiftPerByte = 3;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetByteCount(int count)
+        {
+            if (count <= 0)
+                return 0;
+
+            uint temp = (uint)(count - 1 + (1 << BitShiftPerByte));
+            return (int)(temp >> BitShiftPerByte);
+        }
+
         public bool Contains(byte[] items, int item)
         {
             if (items == null || (uint)item >= (uint)items.Length)
                 return false;
 
-            return items[item] != 0;
+            int byteIndex = Div8Rem(item, out int bitIndex);
+            byte bitMask = (byte)(1 << bitIndex);
+
+            return (items[byteIndex] & bitMask) != 0;
         }
 
         public void Add(byte[] items, int item)
@@ -18,7 +34,10 @@
             if (items == null || (uint)item >= (uint)items.Length)
                 return;
 
-            items[item] = 0x01;
+            int byteIndex = Div8Rem(item, out int bitIndex);
+            byte bitMask = (byte)(1 << bitIndex);
+
+            items[byteIndex] |= bitMask;
         }
 
         public void Clear(byte[] items)
@@ -27,6 +46,13 @@
                 return;
 
             Array.Clear(items, 0, items.Length);
+        }
+
+        private static int Div8Rem(int number, out int remainder)
+        {
+            uint quotient = (uint)number >> 3;
+            remainder = number & 0b111;
+            return (int)quotient;
         }
     }
 #pragma warning restore CA1815 // Override equals and operator equals on value types
