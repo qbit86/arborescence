9a30c753757d9d43124cd66198c22609c07c860f
Viktor T <qbit86@gmail.com>
2020-06-19 00:26:23 +0300

    Umplement EnumerateVertices() overloads

---
+++ src/Ubiquitous.Traversal/GenericSearch/GenericSearch.EnumerateVertices.SingleSource.cs
@@ -0,0 +1,19 @@
+﻿namespace Ubiquitous.Traversal
+{
+    using System.Collections.Generic;
+
+    // ReSharper disable UnusedTypeParameter
+    public readonly partial struct GenericSearch<TGraph, TVertex, TEdge, TEdgeEnumerator, TVertexContainer,
+        TExploredSet, TGraphPolicy, TVertexContainerPolicy, TExploredSetPolicy>
+    {
+        public IEnumerator<TVertex> EnumerateVertices(
+            TGraph graph, TVertex source, TVertexContainer vertexContainer, TExploredSet exploredSet)
+        {
+            ExploredSetPolicy.Add(exploredSet, source);
+            VertexContainerPolicy.Add(vertexContainer, source);
+
+            return EnumerateVerticesCore(graph, vertexContainer, exploredSet);
+        }
+    }
+    // ReSharper restore UnusedTypeParameter
+}
