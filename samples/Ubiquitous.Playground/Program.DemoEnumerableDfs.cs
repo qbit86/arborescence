127bbdfca5fd237565ec428a540ee61b1ea3b4d9
Viktor T <qbit86@gmail.com>
2020-06-20 20:22:35 +0300

    Add call to EnumerateEdges()

--- samples/Ubiquitous.Playground/Program.DemoEnumerableDfs.cs
+++ samples/Ubiquitous.Playground/Program.DemoEnumerableDfs.cs
@@ -1,10 +1,14 @@
 namespace Ubiquitous
 {
     using System;
+    using System.Buffers;
     using System.Collections.Generic;
     using System.IO;
     using Models;
+    using Traversal;
     using Workbench;
+    using IndexedAdjacencyListGraphPolicy =
+        Models.IndexedIncidenceGraphPolicy<Models.AdjacencyListIncidenceGraph, ArraySegmentEnumerator<int>>;
 
     internal static partial class Program
     {
@@ -36,6 +40,16 @@ namespace Ubiquitous
             }
 
             w.WriteLine();
+
+            EnumerableDfs<AdjacencyListIncidenceGraph, int, int, ArraySegmentEnumerator<int>, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedSetPolicy> dfs = default;
+            var sources = new IndexEnumerator(2);
+            byte[] enumerableExploredSet = ArrayPool<byte>.Shared.Rent(
+                IndexedSetPolicy.GetByteCount(graph.VertexCount));
+            Array.Clear(enumerableExploredSet, 0, enumerableExploredSet.Length);
+            using IEnumerator<int> steps = dfs.EnumerateEdges(graph, sources, enumerableExploredSet);
+
+            ArrayPool<byte>.Shared.Return(enumerableExploredSet);
 
             w.WriteLine("}");
         }
