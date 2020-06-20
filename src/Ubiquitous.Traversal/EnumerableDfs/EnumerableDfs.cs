4c8412169015c9d14ae6ffb6f6d4bab3745a8f33
Viktor T <qbit86@gmail.com>
2020-06-20 16:51:36 +0300

    Add EnumerableDfs<> Stub

--- src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.cs
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.cs
@@ -5,6 +5,30 @@ namespace Ubiquitous.Traversal
     using System.Runtime.CompilerServices;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
+    public readonly partial struct EnumerableDfs<
+        TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy, TExploredSetPolicy>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>, IHeadPolicy<TGraph, TVertex, TEdge>
+        where TExploredSetPolicy : ISetPolicy<TExploredSet, TVertex>
+    {
+        private TGraphPolicy GraphPolicy { get; }
+        private TExploredSetPolicy ExploredSetPolicy { get; }
+
+        public EnumerableDfs(TGraphPolicy graphPolicy, TExploredSetPolicy exploredSetPolicy)
+        {
+            if (graphPolicy == null)
+                throw new ArgumentNullException(nameof(graphPolicy));
+
+            if (exploredSetPolicy == null)
+                throw new ArgumentNullException(nameof(exploredSetPolicy));
+
+            GraphPolicy = graphPolicy;
+            ExploredSetPolicy = exploredSetPolicy;
+        }
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly partial struct LegacyDfs<
         TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
         where TEdgeEnumerator : IEnumerator<TEdge>
