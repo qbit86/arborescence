997bcd88be52a6496e35eca89e6743137edab4f7
Viktor T <qbit86@gmail.com>
2020-06-20 17:24:57 +0300

    Implement EnumerateVertices() for single and multiple sources

--- src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateVertices.cs
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateVertices.cs
@@ -6,6 +6,30 @@ namespace Ubiquitous.Traversal
     public readonly partial struct EnumerableDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy,
         TExploredSetPolicy>
     {
+        public IEnumerator<TVertex> EnumerateVertices(TGraph graph, TVertex source, TExploredSet exploredSet)
+        {
+            var stack = new Internal.Stack<TVertex>();
+
+            stack.Add(source);
+
+            return EnumerateVerticesCore(graph, stack, exploredSet);
+        }
+
+        public IEnumerator<TVertex> EnumerateVertices<TVertexEnumerator>(
+            TGraph graph, TVertexEnumerator sources, TExploredSet exploredSet)
+            where TVertexEnumerator : IEnumerator<TVertex>
+        {
+            var stack = new Internal.Stack<TVertex>();
+
+            while (sources.MoveNext())
+            {
+                TVertex s = sources.Current;
+                stack.Add(s);
+            }
+
+            return EnumerateVerticesCore(graph, stack, exploredSet);
+        }
+
         private IEnumerator<TVertex> EnumerateVerticesCore(
             TGraph graph, Internal.Stack<TVertex> stack, TExploredSet exploredSet)
         {
