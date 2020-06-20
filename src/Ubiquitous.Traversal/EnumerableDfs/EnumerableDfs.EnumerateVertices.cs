58fecc857beea71a0c26cbf9653619f8631f1b10
Viktor T <qbit86@gmail.com>
2020-06-20 17:19:41 +0300

    Implement EnumerateVerticesCore() for EnumerableDfs

---
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateVertices.cs
@@ -0,0 +1,39 @@
+namespace Ubiquitous.Traversal
+{
+    using System.Collections.Generic;
+
+    // ReSharper disable UnusedTypeParameter
+    public readonly partial struct EnumerableDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy,
+        TExploredSetPolicy>
+    {
+        private IEnumerator<TVertex> EnumerateVerticesCore(
+            TGraph graph, Internal.Stack<TVertex> stack, TExploredSet exploredSet)
+        {
+            try
+            {
+                while (stack.TryTake(out TVertex u))
+                {
+                    if (ExploredSetPolicy.Contains(exploredSet, u))
+                        continue;
+
+                    ExploredSetPolicy.Add(exploredSet, u);
+                    TEdgeEnumerator outEdges = GraphPolicy.EnumerateOutEdges(graph, u);
+                    while (outEdges.MoveNext())
+                    {
+                        TEdge e = outEdges.Current;
+                        if (!GraphPolicy.TryGetHead(graph, e, out TVertex v))
+                            continue;
+
+                        yield return v;
+                        stack.Add(v);
+                    }
+                }
+            }
+            finally
+            {
+                stack.Dispose();
+            }
+        }
+    }
+    // ReSharper restore UnusedTypeParameter
+}
