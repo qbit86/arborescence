37ccab83bb312ef848e005469c3bb9aacd7eb042
Viktor T <qbit86@gmail.com>
2020-06-20 20:12:27 +0300

    Add DemoEnumerableDfs()

---
+++ samples/Ubiquitous.Playground/Program.DemoEnumerableDfs.cs
@@ -0,0 +1,43 @@
+namespace Ubiquitous
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Models;
+    using Workbench;
+
+    internal static partial class Program
+    {
+        private static void DemoEnumerableDfs()
+        {
+            var builder = new AdjacencyListIncidenceGraphBuilder(0, 31);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader("09"))
+            {
+                IEnumerable<Endpoints<int>> edges = IndexedEdgeListParser.ParseEdges(textReader);
+                foreach (Endpoints<int> edge in edges)
+                    builder.TryAdd(edge.Tail, edge.Head, out _);
+            }
+
+            AdjacencyListIncidenceGraph graph = builder.ToGraph();
+            Console.Write($"{nameof(graph.VertexCount)}: {graph.VertexCount.ToString(F)}");
+            Console.WriteLine($", {nameof(graph.EdgeCount)}: {graph.EdgeCount.ToString(F)}");
+
+            TextWriter w = Console.Out;
+
+            w.WriteLine("digraph \"DFS forest\" {");
+            w.WriteLine("  node [shape=circle style=dashed fontname=\"Times-Italic\"]");
+
+            // Enumerate vertices.
+            for (int v = 0; v < graph.VertexCount; ++v)
+            {
+                w.Write(v == 0 ? "  " : " ");
+                w.Write(V(v));
+            }
+
+            w.WriteLine();
+
+            w.WriteLine("}");
+        }
+    }
+}
