ed6eff474d337862317a1bc69cdd73414800322d
Viktor T <qbit86@gmail.com>
2020-06-21 22:13:14 +0300

    Remove LegacyDfs<>

--- src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.cs
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.cs
@@ -2,7 +2,6 @@ namespace Ubiquitous.Traversal
 {
     using System;
     using System.Collections.Generic;
-    using System.Runtime.CompilerServices;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
     public readonly partial struct EnumerableDfs<
@@ -25,34 +24,6 @@ namespace Ubiquitous.Traversal
             GraphPolicy = graphPolicy;
             ExploredSetPolicy = exploredSetPolicy;
         }
-    }
-#pragma warning restore CA1815 // Override equals and operator equals on value types
-
-#pragma warning disable CA1815 // Override equals and operator equals on value types
-    public readonly partial struct LegacyDfs<
-        TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
-        where TEdgeEnumerator : IEnumerator<TEdge>
-        where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>, IHeadPolicy<TGraph, TVertex, TEdge>
-        where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
-    {
-        private TGraphPolicy GraphPolicy { get; }
-        private TColorMapPolicy ColorMapPolicy { get; }
-
-        public LegacyDfs(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
-        {
-            if (graphPolicy == null)
-                throw new ArgumentNullException(nameof(graphPolicy));
-
-            if (colorMapPolicy == null)
-                throw new ArgumentNullException(nameof(colorMapPolicy));
-
-            GraphPolicy = graphPolicy;
-            ColorMapPolicy = colorMapPolicy;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private Color GetColorOrDefault(TColorMap colorMap, TVertex vertex) =>
-            ColorMapPolicy.TryGetValue(colorMap, vertex, out Color result) ? result : Color.None;
     }
 #pragma warning restore CA1815 // Override equals and operator equals on value types
 }
