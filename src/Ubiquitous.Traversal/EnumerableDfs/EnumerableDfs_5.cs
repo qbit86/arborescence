a2a922260ae3155b7d0cb977a521eff2709665a7
Viktor T <qbit86@gmail.com>
2020-06-21 22:04:15 +0300

    Remove LegacyDfs<> factory

--- src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs_5.cs
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs_5.cs
@@ -18,19 +18,4 @@
         }
 #pragma warning restore CA1000 // Do not declare static members on generic types
     }
-
-    public static class LegacyDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap>
-        where TEdgeEnumerator : IEnumerator<TEdge>
-    {
-#pragma warning disable CA1000 // Do not declare static members on generic types
-        public static LegacyDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>
-            Create<TGraphPolicy, TColorMapPolicy>(TGraphPolicy graphPolicy, TColorMapPolicy colorMapPolicy)
-            where TGraphPolicy : IOutEdgesPolicy<TGraph, TVertex, TEdgeEnumerator>, IHeadPolicy<TGraph, TVertex, TEdge>
-            where TColorMapPolicy : IMapPolicy<TColorMap, TVertex, Color>
-        {
-            return new LegacyDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap, TGraphPolicy, TColorMapPolicy>(
-                graphPolicy, colorMapPolicy);
-        }
-#pragma warning restore CA1000 // Do not declare static members on generic types
-    }
 }
