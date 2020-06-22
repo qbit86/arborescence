deb59734d21050a311307b491feb172ff22a94fe
Viktor T <qbit86@gmail.com>
2020-06-23 00:04:34 +0300

    Refactor EnumerableDfs<>.EnumerateVertices() overloads

--- src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateVertices.MultipleSource.cs
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateVertices.MultipleSource.cs
@@ -11,14 +11,39 @@ namespace Ubiquitous.Traversal
             where TVertexEnumerator : IEnumerator<TVertex>
         {
             var stack = new Internal.Stack<VertexInfo>();
-
-            while (sources.MoveNext())
+            try
             {
-                TVertex source = sources.Current;
-                stack.Add(new VertexInfo(source, false));
-            }
+                while (sources.MoveNext())
+                {
+                    TVertex source = sources.Current;
+                    stack.Add(new VertexInfo(source, false));
+                }
 
-            return EnumerateVerticesCore(graph, stack, exploredSet);
+                while (stack.TryTake(out VertexInfo stackFrame))
+                {
+                    TVertex u = stackFrame.ExploredVertex;
+                    if (ExploredSetPolicy.Contains(exploredSet, u))
+                        continue;
+
+                    ExploredSetPolicy.Add(exploredSet, u);
+                    if (stackFrame.HasInEdge)
+                        yield return u;
+
+                    TEdgeEnumerator outEdges = GraphPolicy.EnumerateOutEdges(graph, u);
+                    while (outEdges.MoveNext())
+                    {
+                        TEdge e = outEdges.Current;
+                        if (!GraphPolicy.TryGetHead(graph, e, out TVertex v))
+                            continue;
+
+                        stack.Add(new VertexInfo(v, true));
+                    }
+                }
+            }
+            finally
+            {
+                stack.Dispose();
+            }
         }
     }
     // ReSharper restore UnusedTypeParameter
