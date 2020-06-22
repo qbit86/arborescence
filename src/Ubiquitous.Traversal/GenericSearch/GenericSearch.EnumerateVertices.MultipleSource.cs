87457fd552e1af06fd0b624000385bdb5b6e1c21
Viktor T <qbit86@gmail.com>
2020-06-23 00:35:19 +0300

    Emit vertex when it's discovered in GenericSearch

--- src/Ubiquitous.Traversal/GenericSearch/GenericSearch.EnumerateVertices.MultipleSource.cs
+++ src/Ubiquitous.Traversal/GenericSearch/GenericSearch.EnumerateVertices.MultipleSource.cs
@@ -14,10 +14,28 @@
             {
                 TVertex source = sources.Current;
                 ExploredSetPolicy.Add(exploredSet, source);
+                yield return source;
                 VertexContainerPolicy.Add(vertexContainer, source);
             }
 
-            return EnumerateVerticesCore(graph, vertexContainer, exploredSet);
+            while (VertexContainerPolicy.TryTake(vertexContainer, out TVertex u))
+            {
+                TEdgeEnumerator outEdges = GraphPolicy.EnumerateOutEdges(graph, u);
+                while (outEdges.MoveNext())
+                {
+                    TEdge e = outEdges.Current;
+                    if (!GraphPolicy.TryGetHead(graph, e, out TVertex v))
+                        continue;
+
+                    bool vExplored = ExploredSetPolicy.Contains(exploredSet, v);
+                    if (!vExplored)
+                    {
+                        ExploredSetPolicy.Add(exploredSet, v);
+                        yield return v;
+                        VertexContainerPolicy.Add(vertexContainer, v);
+                    }
+                }
+            }
         }
     }
     // ReSharper restore UnusedTypeParameter
