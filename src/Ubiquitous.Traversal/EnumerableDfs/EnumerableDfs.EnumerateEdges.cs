5f7a9159d8fff84aca3af37e73e42b250132e66c
Viktor T <qbit86@gmail.com>
2020-06-22 23:49:58 +0300

    Refactor EnumerableDfs<>.EnumerateEdges() overloads

--- src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateEdges.cs
+++ src/Ubiquitous.Traversal/EnumerableDfs/EnumerableDfs.EnumerateEdges.cs
@@ -1,43 +1,9 @@
 namespace Ubiquitous.Traversal
 {
-    using System.Collections.Generic;
-
     // ReSharper disable UnusedTypeParameter
     public readonly partial struct EnumerableDfs<
         TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy, TExploredSetPolicy>
     {
-        private IEnumerator<TEdge> EnumerateEdgesCore(
-            TGraph graph, Internal.Stack<EdgeInfo> stack, TExploredSet exploredSet)
-        {
-            try
-            {
-                while (stack.TryTake(out EdgeInfo stackFrame))
-                {
-                    TVertex u = stackFrame.ExploredVertex;
-                    if (ExploredSetPolicy.Contains(exploredSet, u))
-                        continue;
-
-                    if (stackFrame.TryGetInEdge(out TEdge inEdge))
-                        yield return inEdge;
-                    ExploredSetPolicy.Add(exploredSet, u);
-
-                    TEdgeEnumerator outEdges = GraphPolicy.EnumerateOutEdges(graph, u);
-                    while (outEdges.MoveNext())
-                    {
-                        TEdge e = outEdges.Current;
-                        if (!GraphPolicy.TryGetHead(graph, e, out TVertex v))
-                            continue;
-
-                        stack.Add(new EdgeInfo(v, e));
-                    }
-                }
-            }
-            finally
-            {
-                stack.Dispose();
-            }
-        }
-
         private readonly struct EdgeInfo
         {
             private readonly TVertex _exploredVertex;
