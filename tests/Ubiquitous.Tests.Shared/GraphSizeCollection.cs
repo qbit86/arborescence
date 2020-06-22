e6ece069cc77fd993f06f9122d7d5dd0644c95c5
Viktor T <qbit86@gmail.com>
2020-06-23 01:44:31 +0300

    Add GraphSizeCollection

---
+++ tests/Ubiquitous.Tests.Shared/GraphSizeCollection.cs
@@ -0,0 +1,26 @@
+﻿namespace Ubiquitous
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    internal sealed class GraphSizeCollection : IEnumerable<object[]>
+    {
+        private static readonly double[] s_densityPowers = { 1.0, Math.Sqrt(2.0), 0.5 * (1.0 + Math.Sqrt(5.0)), 2.0 };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1, 1.0 };
+
+            for (int power = 1; power < 7; ++power)
+            {
+                double exp = Math.Pow(10.0, 0.5 * power);
+                int vertexCount = (int)Math.Ceiling(exp);
+                foreach (double densityPower in s_densityPowers)
+                    yield return new object[] { vertexCount, densityPower };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
