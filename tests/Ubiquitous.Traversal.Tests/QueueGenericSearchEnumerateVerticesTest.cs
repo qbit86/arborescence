d5e07c1390995e3b3256014f5d1d4250381a9643
Viktor T <qbit86@gmail.com>
2020-06-23 03:10:03 +0300

    Add QueueGenericSearchEnumerateVerticesTest stub

---
+++ tests/Ubiquitous.Traversal.Tests/QueueGenericSearchEnumerateVerticesTest.cs
@@ -0,0 +1,122 @@
+﻿namespace Ubiquitous
+{
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using Misnomer;
+    using Models;
+    using Traversal;
+    using Workbench;
+    using Xunit;
+    using EdgeEnumerator = ArraySegmentEnumerator<int>;
+    using IndexedAdjacencyListGraphPolicy =
+        Models.IndexedIncidenceGraphPolicy<Models.AdjacencyListIncidenceGraph, ArraySegmentEnumerator<int>>;
+
+    public class QueueGenericSearchEnumerateVerticesTest
+    {
+        public QueueGenericSearchEnumerateVerticesTest()
+        {
+            InstantBfs = default;
+            GenericSearch = default;
+        }
+
+        private InstantBfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy>
+            InstantBfs { get; }
+
+        private GenericSearch<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, Queue<int>, byte[],
+                IndexedAdjacencyListGraphPolicy, QueuePolicy, IndexedSetPolicy>
+            GenericSearch { get; }
+
+        private void EnumerateVerticesCore(AdjacencyListIncidenceGraph graph, bool multipleSource)
+        {
+            Debug.Assert(graph != null, "graph != null");
+
+            // Arrange
+
+            Debug.Assert(graph.VertexCount > 0, "graph.VertexCount > 0");
+
+            byte[] instantColorMap = ArrayPool<byte>.Shared.Rent(graph.VertexCount);
+            Array.Clear(instantColorMap, 0, instantColorMap.Length);
+            var container = new Queue<int>();
+            byte[] enumerableExploredSet = ArrayPool<byte>.Shared.Rent(
+                IndexedSetPolicy.GetByteCount(graph.VertexCount));
+            Array.Clear(enumerableExploredSet, 0, enumerableExploredSet.Length);
+
+            var instantSteps = new Rist<int>(graph.VertexCount);
+            var enumerableSteps = new Rist<int>(graph.VertexCount);
+            BfsHandler<AdjacencyListIncidenceGraph, int, int> bfsHandler = CreateBfsHandler(instantSteps);
+
+            throw new NotImplementedException();
+        }
+
+        private static BfsHandler<AdjacencyListIncidenceGraph, int, int> CreateBfsHandler(IList<int> discoveredVertices)
+        {
+            Debug.Assert(discoveredVertices != null, "discoveredVertices != null");
+
+            var result = new BfsHandler<AdjacencyListIncidenceGraph, int, int>();
+            result.DiscoverVertex += (g, v) => discoveredVertices.Add(v);
+            return result;
+        }
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+        [Theory]
+        [ClassData(typeof(GraphSizeCollection))]
+        public void EnumerateVertices_SingleSource_ByGraphSize(int vertexCount, double densityPower)
+        {
+            AdjacencyListIncidenceGraph graph = GraphHelper.GenerateAdjacencyListIncidenceGraph(
+                vertexCount, densityPower);
+            EnumerateVerticesCore(graph, false);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestCaseCollection))]
+        public void EnumerateVertices_SingleSource_ByTestCase(string testCase)
+        {
+            Assert.NotNull(testCase);
+            var builder = new AdjacencyListIncidenceGraphBuilder(10);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader(testCase))
+            {
+                Assert.NotEqual(TextReader.Null, textReader);
+                IEnumerable<Endpoints<int>> edges = IndexedEdgeListParser.ParseEdges(textReader);
+                foreach (Endpoints<int> edge in edges)
+                    builder.TryAdd(edge.Tail, edge.Head, out _);
+            }
+
+            AdjacencyListIncidenceGraph graph = builder.ToGraph();
+            EnumerateVerticesCore(graph, false);
+        }
+
+        [Theory]
+        [ClassData(typeof(GraphSizeCollection))]
+        public void EnumerateVertices_MultipleSource_ByGraphSize(int vertexCount, double densityPower)
+        {
+            AdjacencyListIncidenceGraph graph = GraphHelper.GenerateAdjacencyListIncidenceGraph(
+                vertexCount, densityPower);
+            EnumerateVerticesCore(graph, true);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestCaseCollection))]
+        public void EnumerateVertices_MultipleSource_ByTestCase(string testCase)
+        {
+            Assert.NotNull(testCase);
+            var builder = new AdjacencyListIncidenceGraphBuilder(10);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader(testCase))
+            {
+                Assert.NotEqual(TextReader.Null, textReader);
+                IEnumerable<Endpoints<int>> edges = IndexedEdgeListParser.ParseEdges(textReader);
+                foreach (Endpoints<int> edge in edges)
+                    builder.TryAdd(edge.Tail, edge.Head, out _);
+            }
+
+            AdjacencyListIncidenceGraph graph = builder.ToGraph();
+            EnumerateVerticesCore(graph, true);
+        }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+    }
+}
