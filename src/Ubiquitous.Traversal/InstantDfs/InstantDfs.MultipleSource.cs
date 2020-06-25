5f7690e8b81c0bfa3aba63f7b34bb66c79f77079
Viktor T <qbit86@gmail.com>
2020-06-25 09:41:49 +0300

    Rename files

---
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.MultipleSource.cs
@@ -0,0 +1,62 @@
+namespace Ubiquitous.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    // ReSharper disable UnusedTypeParameter
+    public readonly partial struct InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+    {
+        public void Traverse<TVertexEnumerator, THandler>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, THandler handler)
+            where TVertexEnumerator : IEnumerator<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                Color color = GetColorOrDefault(colorMap, u);
+                if (color != Color.None && color != Color.White)
+                    continue;
+
+                handler.OnStartVertex(graph, u);
+                TraverseCore(graph, u, colorMap, handler, s_false);
+            }
+        }
+
+        public void Traverse<TVertexEnumerator, THandler>(
+            TGraph graph, TVertexEnumerator vertices, TColorMap colorMap, THandler handler, TVertex startVertex)
+            where TVertexEnumerator : IEnumerator<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            handler.OnStartVertex(graph, startVertex);
+            TraverseCore(graph, startVertex, colorMap, handler, s_false);
+
+            while (vertices.MoveNext())
+            {
+                TVertex u = vertices.Current;
+                Color color = GetColorOrDefault(colorMap, u);
+                if (color != Color.None && color != Color.White)
+                    continue;
+
+                handler.OnStartVertex(graph, u);
+                TraverseCore(graph, u, colorMap, handler, s_false);
+            }
+        }
+    }
+    // ReSharper restore UnusedTypeParameter
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
---
+++ src/Ubiquitous.Traversal/InstantDfs/InstantDfs.SingleSource.cs
@@ -0,0 +1,35 @@
+namespace Ubiquitous.Traversal
+{
+    using System;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    // ReSharper disable UnusedTypeParameter
+    public readonly partial struct InstantDfs<TGraph, TVertex, TEdge, TEdgeEnumerator, TColorMap,
+        TGraphPolicy, TColorMapPolicy>
+    {
+        private static readonly Func<TGraph, TVertex, bool> s_false = (g, v) => false;
+
+        public void Traverse<THandler>(TGraph graph, TVertex startVertex, TColorMap colorMap, THandler handler)
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            handler.OnStartVertex(graph, startVertex);
+            TraverseCore(graph, startVertex, colorMap, handler, s_false);
+        }
+
+        public void Traverse<THandler>(TGraph graph, TVertex startVertex, TColorMap colorMap, THandler handler,
+            Func<TGraph, TVertex, bool> terminationCondition)
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            handler.OnStartVertex(graph, startVertex);
+            TraverseCore(graph, startVertex, colorMap, handler, terminationCondition ?? s_false);
+        }
+    }
+    // ReSharper restore UnusedTypeParameter
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
