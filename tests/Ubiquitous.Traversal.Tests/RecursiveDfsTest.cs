75269fe4ba7bae2f5ad3ceba40a973e9ba9ab000
Viktor T <qbit86@gmail.com>
2020-07-02 00:41:01 +0300

    Add RecursiveDfs test

---
+++ tests/Ubiquitous.Traversal.Tests/RecursiveDfsTest.cs
@@ -0,0 +1,169 @@
+﻿namespace Ubiquitous
+{
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using Misnomer;
+    using Models;
+    using Traversal;
+    using Workbench;
+    using Xunit;
+    using EdgeEnumerator = ArraySegmentEnumerator<int>;
+    using IndexedAdjacencyListGraphPolicy =
+        Models.IndexedIncidenceGraphPolicy<Models.AdjacencyListIncidenceGraph, ArraySegmentEnumerator<int>>;
+
+    public sealed class RecursiveDfsTest
+    {
+        public RecursiveDfsTest()
+        {
+            InstantDfs = default;
+            RecursiveDfs = default;
+        }
+
+        private InstantDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy>
+            InstantDfs { get; }
+
+        private RecursiveDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedColorMapPolicy>
+            RecursiveDfs { get; }
+
+        private void TraverseCore(AdjacencyListIncidenceGraph graph, bool multipleSource)
+        {
+            Debug.Assert(graph != null, "graph != null");
+
+            // Arrange
+
+            Debug.Assert(graph.VertexCount > 0, "graph.VertexCount > 0");
+
+            byte[] instantColorMap = ArrayPool<byte>.Shared.Rent(graph.VertexCount);
+            Array.Clear(instantColorMap, 0, instantColorMap.Length);
+            using var instantSteps = new Rist<(string, int)>(graph.VertexCount);
+            DfsHandler<AdjacencyListIncidenceGraph, int, int> instantHandler = CreateDfsHandler(instantSteps);
+
+            byte[] recursiveColorMap = ArrayPool<byte>.Shared.Rent(graph.VertexCount);
+            Array.Clear(recursiveColorMap, 0, recursiveColorMap.Length);
+            using var recursiveSteps = new Rist<(string, int)>(graph.VertexCount);
+            DfsHandler<AdjacencyListIncidenceGraph, int, int> recursiveHandler = CreateDfsHandler(recursiveSteps);
+
+            // Act
+
+            if (multipleSource)
+            {
+                if (graph.VertexCount < 3)
+                    return;
+
+                int sourceCount = graph.VertexCount / 3;
+                var sources = new IndexEnumerator(sourceCount);
+
+                InstantDfs.Traverse(graph, sources, instantColorMap, instantHandler);
+                RecursiveDfs.Traverse(graph, sources, recursiveColorMap, recursiveHandler);
+            }
+            else
+            {
+                int source = graph.VertexCount >> 1;
+                InstantDfs.Traverse(graph, source, instantColorMap, instantHandler);
+                RecursiveDfs.Traverse(graph, source, instantColorMap, recursiveHandler);
+            }
+
+            // Assert
+
+            int instantStepCount = instantSteps.Count;
+            int recursiveStepCount = recursiveSteps.Count;
+            Assert.Equal(instantStepCount, recursiveStepCount);
+
+            int count = instantStepCount;
+            for (int i = 0; i < count; ++i)
+            {
+                (string, int) instantStep = instantSteps[i];
+                (string, int) recursiveStep = recursiveSteps[i];
+
+                if (instantStep == recursiveStep)
+                    continue;
+
+                Assert.Equal(instantStep, recursiveStep);
+            }
+
+            // Cleanup
+
+            ArrayPool<byte>.Shared.Return(instantColorMap);
+            ArrayPool<byte>.Shared.Return(recursiveColorMap);
+        }
+
+        private static DfsHandler<AdjacencyListIncidenceGraph, int, int> CreateDfsHandler(IList<(string, int)> steps)
+        {
+            Debug.Assert(steps != null, "steps != null");
+
+            var result = new DfsHandler<AdjacencyListIncidenceGraph, int, int>();
+            result.StartVertex += (g, v) => steps.Add((nameof(result.OnStartVertex), v));
+            result.DiscoverVertex += (g, v) => steps.Add((nameof(result.DiscoverVertex), v));
+            result.FinishVertex += (g, v) => steps.Add((nameof(result.FinishVertex), v));
+            result.TreeEdge += (g, e) => steps.Add((nameof(result.TreeEdge), e));
+            result.BackEdge += (g, e) => steps.Add((nameof(result.BackEdge), e));
+            result.ExamineEdge += (g, e) => steps.Add((nameof(result.ExamineEdge), e));
+            result.ForwardOrCrossEdge += (g, e) => steps.Add((nameof(result.ForwardOrCrossEdge), e));
+            result.FinishEdge += (g, e) => steps.Add((nameof(result.FinishEdge), e));
+            return result;
+        }
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+        [Theory]
+        [ClassData(typeof(GraphSizeCollection))]
+        public void Traverse_SingleSource_ByGraphSize(int vertexCount, double densityPower)
+        {
+            AdjacencyListIncidenceGraph graph = GraphHelper.GenerateAdjacencyListIncidenceGraph(
+                vertexCount, densityPower);
+            TraverseCore(graph, false);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestCaseCollection))]
+        public void Traverse_SingleSource_ByTestCase(string testCase)
+        {
+            Assert.NotNull(testCase);
+            var builder = new AdjacencyListIncidenceGraphBuilder(10);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader(testCase))
+            {
+                Assert.NotEqual(TextReader.Null, textReader);
+                IEnumerable<Endpoints<int>> edges = IndexedEdgeListParser.ParseEdges(textReader);
+                foreach (Endpoints<int> edge in edges)
+                    builder.TryAdd(edge.Tail, edge.Head, out _);
+            }
+
+            AdjacencyListIncidenceGraph graph = builder.ToGraph();
+            TraverseCore(graph, false);
+        }
+
+        [Theory]
+        [ClassData(typeof(GraphSizeCollection))]
+        public void Traverse_MultipleSource_ByGraphSize(int vertexCount, double densityPower)
+        {
+            AdjacencyListIncidenceGraph graph = GraphHelper.GenerateAdjacencyListIncidenceGraph(
+                vertexCount, densityPower);
+            TraverseCore(graph, true);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestCaseCollection))]
+        public void Traverse_MultipleSource_ByTestCase(string testCase)
+        {
+            Assert.NotNull(testCase);
+            var builder = new AdjacencyListIncidenceGraphBuilder(10);
+
+            using (TextReader textReader = IndexedGraphs.GetTextReader(testCase))
+            {
+                Assert.NotEqual(TextReader.Null, textReader);
+                IEnumerable<Endpoints<int>> edges = IndexedEdgeListParser.ParseEdges(textReader);
+                foreach (Endpoints<int> edge in edges)
+                    builder.TryAdd(edge.Tail, edge.Head, out _);
+            }
+
+            AdjacencyListIncidenceGraph graph = builder.ToGraph();
+            TraverseCore(graph, true);
+        }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+    }
+}
