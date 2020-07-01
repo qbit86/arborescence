3a66f29efcd7808ac7db0f749014b6f248d7a17b
Viktor T <qbit86@gmail.com>
2020-07-02 01:20:07 +0300

    Move GraphHelper to shared project

---
+++ tests/Ubiquitous.Tests.Shared/GraphHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace Ubiquitous
+{
+    using System;
+    using Models;
+
+    internal static class GraphHelper
+    {
+        internal static AdjacencyListIncidenceGraph GenerateAdjacencyListIncidenceGraph(
+            int vertexCount, double densityPower)
+        {
+            int edgeCount = (int)Math.Ceiling(Math.Pow(vertexCount, densityPower));
+
+            var builder = new AdjacencyListIncidenceGraphBuilder(vertexCount);
+            var prng = new Random(1729);
+
+            for (int e = 0; e < edgeCount; ++e)
+            {
+                int tail = prng.Next(vertexCount);
+                int head = prng.Next(vertexCount);
+                builder.TryAdd(tail, head, out _);
+            }
+
+            AdjacencyListIncidenceGraph result = builder.ToGraph();
+            return result;
+        }
+    }
+}
