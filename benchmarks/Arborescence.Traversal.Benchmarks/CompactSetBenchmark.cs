64283569fc3d6fd888cb3c8627d3b82370ca377a
Viktor Tselov <lightmap.tselov@gmail.com>
2020-07-03 21:32:18 +0300

    Add CompactSetBenchmark

---
+++ benchmarks/Arborescence.Traversal.Benchmarks/CompactSetBenchmark.cs
@@ -0,0 +1,82 @@
+﻿namespace Arborescence
+{
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using BenchmarkDotNet.Attributes;
+    using Models;
+    using Traversal;
+    using EdgeEnumerator = ArraySegmentEnumerator<int>;
+    using IndexedAdjacencyListGraphPolicy =
+        Models.IndexedIncidenceGraphPolicy<Models.AdjacencyListIncidenceGraph, ArraySegmentEnumerator<int>>;
+
+    [MemoryDiagnoser]
+    public abstract class CompactSetBenchmark
+    {
+        private byte[] _compactExploredSet = Array.Empty<byte>();
+        private byte[] _fastExploredSet = Array.Empty<byte>();
+
+        protected CompactSetBenchmark()
+        {
+            CompactDfs = default;
+            FastDfs = default;
+        }
+
+        [Params(10, 100, 1000, 10000)]
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public int VertexCount { get; set; }
+
+        private EnumerableDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, CompactSetPolicy>
+            CompactDfs { get; }
+
+        private EnumerableDfs<AdjacencyListIncidenceGraph, int, int, EdgeEnumerator, byte[],
+                IndexedAdjacencyListGraphPolicy, IndexedSetPolicy>
+            FastDfs { get; }
+
+        private AdjacencyListIncidenceGraph Graph { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            Graph = GraphHelper.Default.GetGraph(VertexCount);
+
+            _compactExploredSet = ArrayPool<byte>.Shared.Rent(CompactSetPolicy.GetByteCount(Graph.VertexCount));
+            _fastExploredSet = ArrayPool<byte>.Shared.Rent(Graph.VertexCount);
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            ArrayPool<byte>.Shared.Return(_compactExploredSet, true);
+            _compactExploredSet = Array.Empty<byte>();
+            ArrayPool<byte>.Shared.Return(_fastExploredSet, true);
+            _fastExploredSet = Array.Empty<byte>();
+        }
+
+        [Benchmark(Baseline = true)]
+        public int Fast()
+        {
+            Array.Clear(_fastExploredSet, 0, _fastExploredSet.Length);
+            using IEnumerator<int> steps = FastDfs.EnumerateEdges(Graph, 0, _fastExploredSet);
+            int count = 0;
+            while (steps.MoveNext())
+                ++count;
+
+            return count;
+        }
+
+        [Benchmark]
+        public int Compact()
+        {
+            Array.Clear(_compactExploredSet, 0, _compactExploredSet.Length);
+            using IEnumerator<int> steps = CompactDfs.EnumerateEdges(Graph, 0, _compactExploredSet);
+            int count = 0;
+            while (steps.MoveNext())
+                ++count;
+
+            return count;
+        }
+    }
+}
