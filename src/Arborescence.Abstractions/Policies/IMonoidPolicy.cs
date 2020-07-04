6a1fbb53c59321aad69842eae2e211377b2a25d5
Viktor T <qbit86@gmail.com>
2020-07-05 02:30:45 +0300

    Add doc comments for the methods of monoid policy

--- src/Arborescence.Abstractions/Policies/IMonoidPolicy.cs
+++ src/Arborescence.Abstractions/Policies/IMonoidPolicy.cs
@@ -6,7 +6,17 @@ namespace Arborescence
     /// <typeparam name="T">The type of the elements in the monoid.</typeparam>
     public interface IMonoidPolicy<T>
     {
+        /// <summary>
+        /// Gets the identity element of the monoid.
+        /// </summary>
         T Identity { get; }
+
+        /// <summary>
+        /// Combines to elements of the monoid into one element of the same monoid.
+        /// </summary>
+        /// <param name="left">The left element to combine.</param>
+        /// <param name="right">The right element to combine.</param>
+        /// <returns>The result of combining the elements.</returns>
         T Combine(T left, T right);
     }
 }
