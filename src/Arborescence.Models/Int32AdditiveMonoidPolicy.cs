5000adfa5ecf8ac7c744e50dc07f63d97180246b
Viktor T <viktor.t***@gmail.com>
2020-07-10 02:14:55 +0300

    Add doc comment for the Int32AdditiveMonoidPolicy

--- src/Arborescence.Models/Int32AdditiveMonoidPolicy.cs
+++ src/Arborescence.Models/Int32AdditiveMonoidPolicy.cs
@@ -1,6 +1,10 @@
 namespace Arborescence.Models
 {
 #pragma warning disable CA1815 // Override equals and operator equals on value types
+    /// <summary>
+    /// Represents a monoid for the elements of the type <see cref="int"/> with 0 as the identity element
+    /// and addition as the binary operation.
+    /// </summary>
     public readonly struct Int32AdditiveMonoidPolicy : IMonoidPolicy<int>
     {
         /// <inheritdoc/>
