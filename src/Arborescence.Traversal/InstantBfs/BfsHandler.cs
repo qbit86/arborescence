5af6f7e6c905fcf916f65b450f25332acd332615
Viktor T <viktor.t***@gmail.com>
2020-07-11 22:51:17 +0300

    Add <inheritdoc> to BfsHandler<> and DfsHandler<>

--- src/Arborescence.Traversal/InstantBfs/BfsHandler.cs
+++ src/Arborescence.Traversal/InstantBfs/BfsHandler.cs
@@ -1,19 +1,27 @@
 namespace Arborescence.Traversal
 {
+    /// <inheritdoc/>
     public sealed class BfsHandler<TGraph, TVertex, TEdge> : IBfsHandler<TGraph, TVertex, TEdge>
     {
+        /// <inheritdoc/>
         public void OnDiscoverVertex(TGraph g, TVertex v) => DiscoverVertex?.Invoke(g, v);
 
+        /// <inheritdoc/>
         public void OnExamineVertex(TGraph g, TVertex v) => ExamineVertex?.Invoke(g, v);
 
+        /// <inheritdoc/>
         public void OnFinishVertex(TGraph g, TVertex v) => FinishVertex?.Invoke(g, v);
 
+        /// <inheritdoc/>
         public void OnExamineEdge(TGraph g, TEdge e) => ExamineEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnTreeEdge(TGraph g, TEdge e) => TreeEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnNonTreeGrayHeadEdge(TGraph g, TEdge e) => NonTreeGrayHeadEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnNonTreeBlackHeadEdge(TGraph g, TEdge e) => NonTreeBlackHeadEdge?.Invoke(g, e);
 
         public event VertexEventHandler<TGraph, TVertex> DiscoverVertex;
--- src/Arborescence.Traversal/InstantDfs/DfsHandler.cs
+++ src/Arborescence.Traversal/InstantDfs/DfsHandler.cs
@@ -1,21 +1,30 @@
 ﻿namespace Arborescence.Traversal
 {
+    /// <inheritdoc/>
     public sealed class DfsHandler<TGraph, TVertex, TEdge> : IDfsHandler<TGraph, TVertex, TEdge>
     {
+        /// <inheritdoc/>
         public void OnStartVertex(TGraph g, TVertex v) => StartVertex?.Invoke(g, v);
 
+        /// <inheritdoc/>
         public void OnDiscoverVertex(TGraph g, TVertex v) => DiscoverVertex?.Invoke(g, v);
 
+        /// <inheritdoc/>
         public void OnFinishVertex(TGraph g, TVertex v) => FinishVertex?.Invoke(g, v);
 
+        /// <inheritdoc/>
         public void OnExamineEdge(TGraph g, TEdge e) => ExamineEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnTreeEdge(TGraph g, TEdge e) => TreeEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnBackEdge(TGraph g, TEdge e) => BackEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnForwardOrCrossEdge(TGraph g, TEdge e) => ForwardOrCrossEdge?.Invoke(g, e);
 
+        /// <inheritdoc/>
         public void OnFinishEdge(TGraph g, TEdge e) => FinishEdge?.Invoke(g, e);
 
         public event VertexEventHandler<TGraph, TVertex> StartVertex;
