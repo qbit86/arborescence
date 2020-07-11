bd36b8fe131dc88a10d57703316ee9a1a8a2c65d
Viktor T <viktor.t***@gmail.com>
2020-07-11 03:22:37 +0300

    Add doc comment to Color

--- src/Arborescence.Traversal/Color.cs
+++ src/Arborescence.Traversal/Color.cs
@@ -1,6 +1,10 @@
 namespace Arborescence.Traversal
 {
 #pragma warning disable CA1028 // Enum Storage should be Int32
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <summary>
+    /// Specifies labels used to mark vertices while traversing a graph.
+    /// </summary>
     public enum Color : byte
     {
         None = 0,
@@ -8,5 +12,6 @@ namespace Arborescence.Traversal
         Gray,
         Black
     }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning restore CA1028 // Enum Storage should be Int32
 }
