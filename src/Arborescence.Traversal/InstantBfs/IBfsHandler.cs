17e185fa7c9ef8d9aaafdd93e8c6c283e9e26e4d
Viktor T <viktor.t***@gmail.com>
2020-07-11 17:44:16 +0300

    Add doc comments on vertex events

--- src/Arborescence.Traversal/InstantBfs/IBfsHandler.cs
+++ src/Arborescence.Traversal/InstantBfs/IBfsHandler.cs
@@ -10,9 +10,31 @@
     /// <typeparam name="TEdge">The type of the edge.</typeparam>
     public interface IBfsHandler<in TGraph, in TVertex, in TEdge>
     {
+        /// <summary>
+        /// This is invoked when a vertex is encountered for the first time.
+        /// </summary>
+        /// <param name="g">The graph.</param>
+        /// <param name="v">The vertex.</param>
         void OnDiscoverVertex(TGraph g, TVertex v);
+
+        /// <summary>
+        /// This is invoked on a vertex as it is taken from the fringe.
+        /// This happens immediately before <see cref="OnExamineEdge"/> is invoked
+        /// on each of the out-edges of vertex <paramref name="v"/>.
+        /// </summary>
+        /// <param name="g">The graph.</param>
+        /// <param name="v">The vertex.</param>
         void OnExamineVertex(TGraph g, TVertex v);
+
+        /// <summary>
+        /// This is invoked on a vertex after all of its out-edges have been added to the search tree
+        /// and all of the adjacent vertices have been discovered
+        /// (but before the out-edges of the adjacent vertices have been examined).
+        /// </summary>
+        /// <param name="g">The graph.</param>
+        /// <param name="v">The vertex.</param>
         void OnFinishVertex(TGraph g, TVertex v);
+
         void OnExamineEdge(TGraph g, TEdge e);
         void OnTreeEdge(TGraph g, TEdge e);
         void OnNonTreeGrayHeadEdge(TGraph g, TEdge e);
--- src/Arborescence.Traversal/InstantDfs/IDfsHandler.cs
+++ src/Arborescence.Traversal/InstantDfs/IDfsHandler.cs
@@ -10,9 +10,31 @@ namespace Arborescence.Traversal
     /// <typeparam name="TEdge">The type of the edge.</typeparam>
     public interface IDfsHandler<in TGraph, in TVertex, in TEdge>
     {
+        /// <summary>
+        /// This is invoked on the source vertex once before the start of the search.
+        /// </summary>
+        /// <param name="g">The graph.</param>
+        /// <param name="v">The vertex.</param>
         void OnStartVertex(TGraph g, TVertex v);
+
+        /// <summary>
+        /// This is invoked when a vertex is encountered for the first time.
+        /// </summary>
+        /// <param name="g">The graph.</param>
+        /// <param name="v">The vertex.</param>
         void OnDiscoverVertex(TGraph g, TVertex v);
+
+        /// <summary>
+        /// This is invoked on vertex <paramref name="v"/> after <see cref="OnFinishVertex"/> has been called
+        /// for all the vertices in the DFS-tree rooted at vertex <paramref name="v"/>.
+        /// If vertex <paramref name="v"/> is a leaf in the DFS-tree,
+        /// then the <see cref="OnFinishVertex"/> function is called on <paramref name="v"/>
+        /// after all the out-edges of <paramref name="v"/> have been examined.
+        /// </summary>
+        /// <param name="g">The graph.</param>
+        /// <param name="v">The vertex.</param>
         void OnFinishVertex(TGraph g, TVertex v);
+
         void OnExamineEdge(TGraph g, TEdge e);
         void OnTreeEdge(TGraph g, TEdge e);
         void OnBackEdge(TGraph g, TEdge e);
