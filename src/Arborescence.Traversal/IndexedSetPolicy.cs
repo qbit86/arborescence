9e6d6e00029d1e389ae035fcfd93db011209da0e
Viktor T <viktor.t***@gmail.com>
2020-07-11 13:58:56 +0300

    Add doc comments to policies for map and set

--- src/Arborescence.Traversal/IndexedSetPolicy.cs
+++ src/Arborescence.Traversal/IndexedSetPolicy.cs
@@ -1,8 +1,12 @@
 ﻿namespace Arborescence.Traversal
 {
 #pragma warning disable CA1815 // Override equals and operator equals on value types
+    /// <summary>
+    /// Defines methods to support adding and checking items for the set represented as a byte array.
+    /// </summary>
     public readonly struct IndexedSetPolicy : ISetPolicy<byte[], int>
     {
+        /// <inheritdoc/>
         public bool Contains(byte[] items, int item)
         {
             if (items == null || (uint)item >= (uint)items.Length)
@@ -11,6 +15,7 @@
             return items[item] != 0;
         }
 
+        /// <inheritdoc/>
         public void Add(byte[] items, int item)
         {
             if (items == null || (uint)item >= (uint)items.Length)
