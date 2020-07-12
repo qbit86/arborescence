c65cbc594af0e84a4fadb93f43c9d240b9ff67f6
Viktor T <viktor.t***@gmail.com>
2020-07-12 13:33:05 +0300

    Add doc comments for events in DfsHandler<>

--- src/Arborescence.Traversal/InstantDfs/DfsHandler.cs
+++ src/Arborescence.Traversal/InstantDfs/DfsHandler.cs
@@ -27,13 +27,44 @@
         /// <inheritdoc/>
         public void OnFinishEdge(TGraph g, TEdge e) => FinishEdge?.Invoke(g, e);
 
+        /// <summary>
+        /// Raised for the source vertex once before the start of the search.
+        /// </summary>
         public event VertexEventHandler<TGraph, TVertex> StartVertex;
+
+        /// <summary>
+        /// Raised when a vertex is encountered for the first time.
+        /// </summary>
         public event VertexEventHandler<TGraph, TVertex> DiscoverVertex;
+
+        /// <summary>
+        /// Raised for a vertex after finished processing all of its descendants.
+        /// </summary>
         public event VertexEventHandler<TGraph, TVertex> FinishVertex;
+
+        /// <summary>
+        /// Raised for every out-edge of each vertex after it is discovered.
+        /// </summary>
         public event EdgeEventHandler<TGraph, TEdge> ExamineEdge;
+
+        /// <summary>
+        /// Raised for each edge as it becomes a member of the search tree.
+        /// </summary>
         public event EdgeEventHandler<TGraph, TEdge> TreeEdge;
+
+        /// <summary>
+        /// Raised for an edge as it is classified as a back edge in the graph.
+        /// </summary>
         public event EdgeEventHandler<TGraph, TEdge> BackEdge;
+
+        /// <summary>
+        /// Raised for an edge as it is classified as a forward or cross edge in the graph.
+        /// </summary>
         public event EdgeEventHandler<TGraph, TEdge> ForwardOrCrossEdge;
+
+        /// <summary>
+        /// Raised for each edge when processing its head is finished.
+        /// </summary>
         public event EdgeEventHandler<TGraph, TEdge> FinishEdge;
     }
 }
