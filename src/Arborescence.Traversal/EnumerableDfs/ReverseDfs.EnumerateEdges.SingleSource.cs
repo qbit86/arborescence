ce8f168bbe26b7d70fa328bee3afcaf1aea2bc29
Viktor T <viktor.t***@gmail.com>
2020-07-13 23:19:05 +0300

    Add xml-doc comments for methods of ReverseDfs<>

--- src/Arborescence.Traversal/EnumerableDfs/ReverseDfs.EnumerateEdges.SingleSource.cs
+++ src/Arborescence.Traversal/EnumerableDfs/ReverseDfs.EnumerateEdges.SingleSource.cs
@@ -6,6 +6,13 @@ namespace Arborescence.Traversal
     public readonly partial struct ReverseDfs<
         TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy, TExploredSetPolicy>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="exploredSet">The set of explored vertices.</param>
+        /// <returns>An enumerator to enumerate the edges of the the graph.</returns>
         public IEnumerator<TEdge> EnumerateEdges(TGraph graph, TVertex source, TExploredSet exploredSet)
         {
             var stack = new Internal.Stack<EdgeInfo<TVertex, TEdge>>();
--- src/Arborescence.Traversal/EnumerableDfs/ReverseDfs.EnumerateVertices.SingleSource.cs
+++ src/Arborescence.Traversal/EnumerableDfs/ReverseDfs.EnumerateVertices.SingleSource.cs
@@ -7,6 +7,13 @@ namespace Arborescence.Traversal
     public readonly partial struct ReverseDfs<
         TGraph, TVertex, TEdge, TEdgeEnumerator, TExploredSet, TGraphPolicy, TExploredSetPolicy>
     {
+        /// <summary>
+        /// Enumerates vertices of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="exploredSet">The set of explored vertices.</param>
+        /// <returns>An enumerator to enumerate the vertices of the the graph.</returns>
         public IEnumerator<TVertex> EnumerateVertices(TGraph graph, TVertex source, TExploredSet exploredSet)
         {
             var stack = new Internal.Stack<TVertex>();
