e17e73444903c655d16cd5fa4908d4b350ec47a2
Viktor T <qbit86@gmail.com>
2020-07-16 00:50:10 +0300

    Add GraphDefinition struct

---
+++ tests/Arborescence.Tests.Shared/GraphDefinition.cs
@@ -0,0 +1,23 @@
+﻿namespace Arborescence
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal readonly struct GraphDefinition
+    {
+        private readonly IReadOnlyList<Endpoints<int>> _edges;
+
+        internal GraphDefinition(int vertexCount, IReadOnlyList<Endpoints<int>> edges)
+        {
+            if (vertexCount < 0)
+                throw new ArgumentOutOfRangeException(nameof(vertexCount));
+
+            VertexCount = vertexCount;
+            _edges = edges ?? Array.Empty<Endpoints<int>>();
+        }
+
+        internal int VertexCount { get; }
+
+        internal IReadOnlyList<Endpoints<int>> Edges => _edges ?? Array.Empty<Endpoints<int>>();
+    }
+}
