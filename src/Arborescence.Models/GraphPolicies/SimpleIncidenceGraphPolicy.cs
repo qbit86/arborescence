6872716f8e010ada82d5e68ece7bfc42c3bb20bd
Viktor T <qbit86@gmail.com>
2020-07-17 02:38:33 +0300

    Add non-generic SimpleIncidenceGraphPolicy struct

--- src/Arborescence.Models/GraphPolicies/SimpleIncidenceGraphPolicy.cs
+++ src/Arborescence.Models/GraphPolicies/SimpleIncidenceGraphPolicy.cs
@@ -5,22 +5,20 @@
     /// Provides access to the endpoints of each edge, and the out-edges of each vertex
     /// in the simple incidence graph.
     /// </summary>
-    /// <typeparam name="TGraph">The type of the graph.</typeparam>
-    /// <typeparam name="TEdges">The type of the edges enumerator.</typeparam>
-    public readonly struct SimpleIncidenceGraphPolicy<TGraph, TEdges> :
-        ITailPolicy<TGraph, int, uint>,
-        IHeadPolicy<TGraph, int, uint>,
-        IOutEdgesPolicy<TGraph, int, TEdges>
-        where TGraph : IIncidenceGraph<int, uint, TEdges>
+    public readonly struct SimpleIncidenceGraphPolicy :
+        ITailPolicy<SimpleIncidenceGraph, int, uint>,
+        IHeadPolicy<SimpleIncidenceGraph, int, uint>,
+        IOutEdgesPolicy<SimpleIncidenceGraph, int, ArraySegmentEnumerator<uint>>
     {
         /// <inheritdoc/>
-        public bool TryGetTail(TGraph graph, uint edge, out int tail) => graph.TryGetTail(edge, out tail);
+        public bool TryGetTail(SimpleIncidenceGraph graph, uint edge, out int tail) => graph.TryGetTail(edge, out tail);
 
         /// <inheritdoc/>
-        public bool TryGetHead(TGraph graph, uint edge, out int head) => graph.TryGetHead(edge, out head);
+        public bool TryGetHead(SimpleIncidenceGraph graph, uint edge, out int head) => graph.TryGetHead(edge, out head);
 
         /// <inheritdoc/>
-        public TEdges EnumerateOutEdges(TGraph graph, int vertex) => graph.EnumerateOutEdges(vertex);
+        public ArraySegmentEnumerator<uint> EnumerateOutEdges(SimpleIncidenceGraph graph, int vertex) =>
+            graph.EnumerateOutEdges(vertex);
     }
 #pragma warning restore CA1815 // Override equals and operator equals on value types
 }
---
+++ src/Arborescence.Models/GraphPolicies/SimpleIncidenceGraphPolicy_2.cs
@@ -0,0 +1,26 @@
+﻿namespace Arborescence.Models
+{
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    /// <summary>
+    /// Provides access to the endpoints of each edge, and the out-edges of each vertex
+    /// in the simple incidence graph.
+    /// </summary>
+    /// <typeparam name="TGraph">The type of the graph.</typeparam>
+    /// <typeparam name="TEdges">The type of the edges enumerator.</typeparam>
+    public readonly struct SimpleIncidenceGraphPolicy<TGraph, TEdges> :
+        ITailPolicy<TGraph, int, uint>,
+        IHeadPolicy<TGraph, int, uint>,
+        IOutEdgesPolicy<TGraph, int, TEdges>
+        where TGraph : IIncidenceGraph<int, uint, TEdges>
+    {
+        /// <inheritdoc/>
+        public bool TryGetTail(TGraph graph, uint edge, out int tail) => graph.TryGetTail(edge, out tail);
+
+        /// <inheritdoc/>
+        public bool TryGetHead(TGraph graph, uint edge, out int head) => graph.TryGetHead(edge, out head);
+
+        /// <inheritdoc/>
+        public TEdges EnumerateOutEdges(TGraph graph, int vertex) => graph.EnumerateOutEdges(vertex);
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
