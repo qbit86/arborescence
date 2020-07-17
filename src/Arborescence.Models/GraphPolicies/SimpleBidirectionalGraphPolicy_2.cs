2c29779e5c9d3108e5e3f54d2a1209dfcf745c97
Viktor T <qbit86@gmail.com>
2020-07-18 02:45:35 +0300

    Add SimpleBidirectionalGraphPolicy<> struct

---
+++ src/Arborescence.Models/GraphPolicies/SimpleBidirectionalGraphPolicy_2.cs
@@ -0,0 +1,30 @@
+namespace Arborescence.Models
+{
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    /// <summary>
+    /// Provides access to the endpoints of each edge, and the in- and out-edges of each vertex
+    /// in the simple bidirectional graph.
+    /// </summary>
+    /// <typeparam name="TGraph">The type of the graph.</typeparam>
+    /// <typeparam name="TEdges">The type of the edges enumerator.</typeparam>
+    public readonly struct SimpleBidirectionalGraphPolicy<TGraph, TEdges> :
+        ITailPolicy<TGraph, int, uint>,
+        IHeadPolicy<TGraph, int, uint>,
+        IOutEdgesPolicy<TGraph, int, TEdges>,
+        IInEdgesPolicy<TGraph, int, TEdges>
+        where TGraph : IBidirectionalGraph<int, uint, TEdges>
+    {
+        /// <inheritdoc/>
+        public bool TryGetTail(TGraph graph, uint edge, out int tail) => graph.TryGetTail(edge, out tail);
+
+        /// <inheritdoc/>
+        public bool TryGetHead(TGraph graph, uint edge, out int head) => graph.TryGetHead(edge, out head);
+
+        /// <inheritdoc/>
+        public TEdges EnumerateOutEdges(TGraph graph, int vertex) => graph.EnumerateOutEdges(vertex);
+
+        /// <inheritdoc/>
+        public TEdges EnumerateInEdges(TGraph graph, int vertex) => graph.EnumerateInEdges(vertex);
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
