acdeac46199603649c9ad3b0f7e7a49ed9a357ca
Viktor T <qbit86@gmail.com>
2020-07-18 02:27:15 +0300

    Add IndexedIncidenceGraphPolicy with no generic parameters

---
+++ src/Arborescence.Models/GraphPolicies/IndexedIncidenceGraphPolicy.cs
@@ -0,0 +1,26 @@
+namespace Arborescence.Models
+{
+    using IndexedIncidenceGraph = AdjacencyListIncidenceGraph;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+    /// <summary>
+    /// Provides access to the endpoints of each edge, and the out-edges of each vertex
+    /// in the indexed incidence graph.
+    /// </summary>
+    public struct IndexedIncidenceGraphPolicy :
+        ITailPolicy<IndexedIncidenceGraph, int, int>,
+        IHeadPolicy<IndexedIncidenceGraph, int, int>,
+        IOutEdgesPolicy<IndexedIncidenceGraph, int, ArraySegmentEnumerator<int>>
+    {
+        /// <inheritdoc/>
+        public bool TryGetTail(IndexedIncidenceGraph graph, int edge, out int tail) => graph.TryGetTail(edge, out tail);
+
+        /// <inheritdoc/>
+        public bool TryGetHead(IndexedIncidenceGraph graph, int edge, out int head) => graph.TryGetHead(edge, out head);
+
+        /// <inheritdoc/>
+        public ArraySegmentEnumerator<int> EnumerateOutEdges(IndexedIncidenceGraph graph, int vertex) =>
+            graph.EnumerateOutEdges(vertex);
+    }
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+}
