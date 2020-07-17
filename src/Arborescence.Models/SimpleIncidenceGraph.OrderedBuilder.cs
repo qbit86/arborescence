4db9e97494d5857955eb8c2bbfc25847910e0901
Viktor T <qbit86@gmail.com>
2020-07-17 03:15:43 +0300

    Add OrderedBuilder ctor

--- src/Arborescence.Models/SimpleIncidenceGraph.OrderedBuilder.cs
+++ src/Arborescence.Models/SimpleIncidenceGraph.OrderedBuilder.cs
@@ -7,11 +7,63 @@
         /// <inheritdoc/>
         public sealed class OrderedBuilder : IGraphBuilder<SimpleIncidenceGraph, int, uint>
         {
+            private int _currentVertexCount;
+            private ArrayBuilder<uint> _edges;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="OrderedBuilder"/> class.
+            /// </summary>
+            /// <param name="initialVertexCount">The initial number of vertices.</param>
+            /// <param name="edgeCapacity">The initial capacity of the internal backing storage for the edges.</param>
+            /// <exception cref="ArgumentOutOfRangeException">
+            /// <paramref name="initialVertexCount"/> is less than zero, or <paramref name="edgeCapacity"/> is less than zero.
+            /// </exception>
+            public OrderedBuilder(int initialVertexCount, int edgeCapacity = 0)
+            {
+                if (initialVertexCount < 0)
+                    throw new ArgumentOutOfRangeException(nameof(initialVertexCount));
+
+                if (edgeCapacity < 0)
+                    throw new ArgumentOutOfRangeException(nameof(edgeCapacity));
+
+                _currentVertexCount = initialVertexCount;
+                _edges = new ArrayBuilder<uint>(edgeCapacity);
+            }
+
             /// <inheritdoc/>
-            public bool TryAdd(int tail, int head, out uint edge) => throw new NotImplementedException();
+            public bool TryAdd(int tail, int head, out uint edge)
+            {
+                if (unchecked((uint)tail) > ushort.MaxValue)
+                {
+                    edge = default;
+                    return false;
+                }
+
+                if (unchecked((uint)head) > ushort.MaxValue)
+                {
+                    edge = default;
+                    return false;
+                }
+
+                int max = Math.Max(tail, head);
+                EnsureVertexCount(max + 1);
+
+                edge = unchecked(((uint)tail << 16) | (uint)head);
+                _edges.Add(edge);
+
+                // TODO: Update bound.
+
+                return true;
+            }
 
             /// <inheritdoc/>
             public SimpleIncidenceGraph ToGraph() => throw new NotImplementedException();
+
+            /// <summary>
+            /// Ensures that the builder can hold the specified number of vertices without growing.
+            /// </summary>
+            /// <param name="vertexCount">The number of vertices.</param>
+            public void EnsureVertexCount(int vertexCount) => throw new NotImplementedException();
         }
     }
 }
