5a1b953bc65b65f2a7dfcf88b93be488f3324e47
Viktor T <qbit86@gmail.com>
2020-07-19 13:30:48 +0300

    Add SimpleIncidenceGraphTest

---
+++ tests/Arborescence.Models.Tests/SimpleIncidenceGraphTest.cs
@@ -0,0 +1,75 @@
+namespace Arborescence
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+    using Xunit;
+
+    public sealed class SimpleIncidenceGraphTest
+    {
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+        [Theory]
+        [ClassData(typeof(GraphDefinitionCollection))]
+        internal void SimpleIncidenceGraph_ShouldContainSameSetOfEdges(GraphDefinitionParameter p)
+        {
+            // Arrange
+            var builder = new SimpleIncidenceGraph.Builder(p.VertexCount, p.Edges.Count);
+            foreach (Endpoints<int> endpoints in p.Edges)
+            {
+                bool wasAdded = builder.TryAdd(endpoints.Tail, endpoints.Head, out _);
+                if (!wasAdded)
+                    Assert.True(wasAdded);
+            }
+
+            SimpleIncidenceGraph graph = builder.ToGraph();
+            HashSet<Endpoints<int>> expectedEdgeSet = p.Edges.ToHashSet();
+
+            // Act
+            var actualEdgeSet = new HashSet<Endpoints<int>>();
+            for (int vertex = 0; vertex < graph.VertexCount; ++vertex)
+            {
+                ArraySegmentEnumerator<uint> outEdges = graph.EnumerateOutEdges(vertex);
+                while (outEdges.MoveNext())
+                {
+                    uint edge = outEdges.Current;
+                    bool hasEndpoints = TryGetEndpoints(graph, edge, out Endpoints<int> endpoints);
+                    if (!hasEndpoints)
+                        Assert.True(hasEndpoints);
+
+                    actualEdgeSet.Add(endpoints);
+                }
+            }
+
+            // Assert
+            Assert.Equal(expectedEdgeSet, actualEdgeSet, HashSetEqualityComparer.Instance);
+        }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+
+        private static bool TryGetEndpoints(SimpleIncidenceGraph graph, uint edge, out Endpoints<int> endpoints)
+        {
+            if (!graph.TryGetTail(edge, out int tail))
+            {
+                endpoints = default;
+                return false;
+            }
+
+            if (!graph.TryGetHead(edge, out int head))
+            {
+                endpoints = default;
+                return false;
+            }
+
+            endpoints = Endpoints.Create(tail, head);
+            return true;
+        }
+
+        internal sealed class HashSetEqualityComparer : IEqualityComparer<HashSet<Endpoints<int>>>
+        {
+            public static HashSetEqualityComparer Instance { get; } = new HashSetEqualityComparer();
+
+            public bool Equals(HashSet<Endpoints<int>> x, HashSet<Endpoints<int>> y) => x.SetEquals(y);
+
+            public int GetHashCode(HashSet<Endpoints<int>> obj) => obj.GetHashCode();
+        }
+    }
+}
