8ace3f90a9cb478b1cdbad8e19a987214b033367
Viktor T <qbit86@gmail.com>
2020-07-26 21:56:39 +0300

    Add MutableIndexedIncidenceGraphTest stub

---
+++ tests/Arborescence.Models.Tests/MutableIndexedIncidenceGraphTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Arborescence
+{
+    using System.Collections.Generic;
+    using Graph = Models.MutableIndexedIncidenceGraph;
+    using EdgeEnumerator = ArrayPrefixEnumerator<int>;
+
+    public sealed class MutableIndexedIncidenceGraphTest
+    {
+        private static IEqualityComparer<HashSet<Endpoints>> HashSetEqualityComparer { get; } =
+            HashSet<Endpoints>.CreateSetComparer();
+    }
+}
