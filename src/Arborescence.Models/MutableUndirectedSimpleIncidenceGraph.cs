ecc399a4985ea0a3b969336fe5dd4baf56b45362
Viktor T <qbit86@gmail.com>
2020-08-01 17:28:08 +0300

    Implement most of the methods of MutableUndirectedSimpleIncidenceGraph

--- src/Arborescence.Models/MutableUndirectedSimpleIncidenceGraph.cs
+++ src/Arborescence.Models/MutableUndirectedSimpleIncidenceGraph.cs
@@ -13,15 +13,104 @@
         private int _initialOutDegree = DefaultInitialOutDegree;
         private ArrayPrefix<ArrayPrefix<Endpoints>> _outEdgesByVertex;
 
-        public void Dispose() => throw new NotImplementedException();
+        public MutableUndirectedSimpleIncidenceGraph(int initialVertexCount)
+        {
+            if (initialVertexCount < 0)
+                throw new ArgumentOutOfRangeException(nameof(initialVertexCount));
 
-        public bool TryAdd(int tail, int head, out Endpoints edge) => throw new NotImplementedException();
+            _outEdgesByVertex = ArrayPrefixBuilder.Resize(_outEdgesByVertex, initialVertexCount, true);
+        }
 
+        /// <summary>
+        /// Gets the number of vertices.
+        /// </summary>
+        public int VertexCount => _outEdgesByVertex.Count;
+
+        /// <summary>
+        /// Gets the number of edges.
+        /// </summary>
+        public int EdgeCount => _edgeCount;
+
+        /// <summary>
+        /// Gets the initial number of out-edges for each vertex.
+        /// </summary>
+        public int InitialOutDegree
+        {
+            get => _initialOutDegree <= 0 ? DefaultInitialOutDegree : _initialOutDegree;
+            set => _initialOutDegree = value;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            _edgeCount = 0;
+            for (int vertex = 0; vertex < _outEdgesByVertex.Count; ++vertex)
+                _outEdgesByVertex[vertex] = ArrayPrefixBuilder.Release(_outEdgesByVertex[vertex], false);
+            _outEdgesByVertex = ArrayPrefixBuilder.Release(_outEdgesByVertex, true);
+        }
+
+        /// <inheritdoc/>
+        public bool TryAdd(int tail, int head, out Endpoints edge)
+        {
+            edge = new Endpoints(tail, head);
+            if (tail < 0 || head < 0)
+                return false;
+
+            int newVertexCountCandidate = Math.Max(tail, head) + 1;
+            EnsureVertexCount(newVertexCountCandidate);
+
+            if (_outEdgesByVertex[tail].Array is null)
+                _outEdgesByVertex[tail] = ArrayPrefixBuilder.Create<Endpoints>(InitialOutDegree);
+            _outEdgesByVertex[tail] = ArrayPrefixBuilder.Add(_outEdgesByVertex[tail], edge, false);
+            ++_edgeCount;
+
+            if (tail != head)
+            {
+                if (_outEdgesByVertex[head].Array is null)
+                    _outEdgesByVertex[head] = ArrayPrefixBuilder.Create<Endpoints>(InitialOutDegree);
+
+                var invertedEdge = new Endpoints(head, tail);
+                _outEdgesByVertex[tail] = ArrayPrefixBuilder.Add(_outEdgesByVertex[head], invertedEdge, false);
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc/>
         public SimpleIncidenceGraph ToGraph() => throw new NotImplementedException();
-        public bool TryGetHead(Endpoints edge, out int head) => throw new NotImplementedException();
 
-        public bool TryGetTail(Endpoints edge, out int tail) => throw new NotImplementedException();
+        /// <inheritdoc/>
+        public bool TryGetHead(Endpoints edge, out int head)
+        {
+            head = edge.Head;
+            return unchecked((uint)head < (uint)VertexCount);
+        }
 
-        public ArrayPrefixEnumerator<Endpoints> EnumerateOutEdges(int vertex) => throw new NotImplementedException();
+        /// <inheritdoc/>
+        public bool TryGetTail(Endpoints edge, out int tail)
+        {
+            tail = edge.Tail;
+            return unchecked((uint)tail < (uint)VertexCount);
+        }
+
+        /// <inheritdoc/>
+        public ArrayPrefixEnumerator<Endpoints> EnumerateOutEdges(int vertex)
+        {
+            if (unchecked((uint)vertex >= (uint)_outEdgesByVertex.Count))
+                return ArrayPrefixEnumerator<Endpoints>.Empty;
+
+            ArrayPrefix<Endpoints> outEdges = _outEdgesByVertex[vertex];
+            return new ArrayPrefixEnumerator<Endpoints>(outEdges.Array ?? Array.Empty<Endpoints>(), outEdges.Count);
+        }
+
+        /// <summary>
+        /// Ensures that the graph can hold the specified number of vertices without growing.
+        /// </summary>
+        /// <param name="vertexCount">The number of vertices.</param>
+        public void EnsureVertexCount(int vertexCount)
+        {
+            if (vertexCount > VertexCount)
+                _outEdgesByVertex = ArrayPrefixBuilder.Resize(_outEdgesByVertex, vertexCount, true);
+        }
     }
 }
