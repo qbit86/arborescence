c788a32ddb54d950fbfbb4ef5ef7fa3b5ad2e1a8
Viktor T <qbit86@gmail.com>
2020-09-06 01:43:58 +0300

    Implement EnumerateVertices() for specialized BFS

--- src/Arborescence.Traversal.Specialized/EnumerableBfs/EnumerableBfs.EnumerateVertices.cs
+++ src/Arborescence.Traversal.Specialized/EnumerableBfs/EnumerableBfs.EnumerateVertices.cs
@@ -1,6 +1,7 @@
 namespace Arborescence.Traversal.Specialized
 {
     using System;
+    using System.Buffers;
     using System.Collections.Generic;
 
     public readonly partial struct EnumerableBfs<TGraph, TEdge, TEdgeEnumerator>
@@ -13,7 +14,39 @@ namespace Arborescence.Traversal.Specialized
             if ((uint)source >= (uint)vertexCount)
                 throw new ArgumentOutOfRangeException(nameof(source));
 
-            throw new NotImplementedException();
+            byte[] exploredSet = ArrayPool<byte>.Shared.Rent(vertexCount);
+            Array.Clear(exploredSet, 0, exploredSet.Length);
+            var queue = new Internal.Queue<int>();
+            try
+            {
+                SetHelpers.Add(exploredSet, source);
+                yield return source;
+                queue.Add(source);
+
+                while (queue.TryTake(out int u))
+                {
+                    TEdgeEnumerator outEdges = graph.EnumerateOutEdges(u);
+                    while (outEdges.MoveNext())
+                    {
+                        TEdge e = outEdges.Current;
+                        if (!graph.TryGetHead(e, out int v))
+                            continue;
+
+                        if (SetHelpers.Contains(exploredSet, v))
+                            continue;
+
+                        SetHelpers.Add(exploredSet, v);
+                        yield return v;
+                        queue.Add(v);
+                    }
+                }
+            }
+            finally
+            {
+                // The Dispose call will happen on the original value of the local if it is the argument to a using statement.
+                queue.Dispose();
+                ArrayPool<byte>.Shared.Return(exploredSet);
+            }
         }
     }
 }
