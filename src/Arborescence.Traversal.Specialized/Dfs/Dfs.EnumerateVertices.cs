fe7d73fa5e43ef81de0bc207c8323cbd7991b198
Viktor T <qbit86@gmail.com>
2020-09-06 18:45:56 +0300

    Add EnumerateEdges() and EnumerateVertices() stub

---
+++ src/Arborescence.Traversal.Specialized/Dfs/Dfs.EnumerateVertices.cs
@@ -0,0 +1,19 @@
+namespace Arborescence.Traversal.Dfs
+{
+    using System;
+    using System.Collections.Generic;
+
+    public readonly partial struct Dfs<TGraph, TEdge, TEdgeEnumerator>
+    {
+        public IEnumerator<TEdge> EnumerateVertices(TGraph graph, int vertexCount, int source)
+        {
+            if (vertexCount < 0)
+                throw new ArgumentOutOfRangeException(nameof(vertexCount));
+
+            if ((uint)source >= (uint)vertexCount)
+                throw new ArgumentOutOfRangeException(nameof(source));
+
+            throw new NotImplementedException();
+        }
+    }
+}
