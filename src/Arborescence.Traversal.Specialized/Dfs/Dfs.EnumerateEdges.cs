bb94c3d1af2bbd6c9a332335e7994c5f29754e88
Viktor T <qbit86@gmail.com>
2020-09-06 19:11:47 +0300

    Add XML-doc comments

--- src/Arborescence.Traversal.Specialized/Dfs/Dfs.EnumerateEdges.cs
+++ src/Arborescence.Traversal.Specialized/Dfs/Dfs.EnumerateEdges.cs
@@ -6,6 +6,17 @@ namespace Arborescence.Traversal.Dfs
 
     public readonly partial struct Dfs<TGraph, TEdge, TEdgeEnumerator>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="vertexCount">The number of vertices.</param>
+        /// <param name="source">The source.</param>
+        /// <returns>An enumerator to enumerate the edges of the the graph.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="vertexCount"/> is less than zero,
+        /// or <paramref name="source"/> is greater than or equal to <paramref name="vertexCount"/>.
+        /// </exception>
         public IEnumerator<TEdge> EnumerateEdges(TGraph graph, int vertexCount, int source)
         {
             if (vertexCount < 0)
--- src/Arborescence.Traversal.Specialized/Dfs/Dfs.cs
+++ src/Arborescence.Traversal.Specialized/Dfs/Dfs.cs
@@ -3,6 +3,16 @@ namespace Arborescence.Traversal.Dfs
     using System.Collections.Generic;
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
+    // ReSharper disable once UnusedTypeParameter
+    /// <summary>
+    /// Represents the DFS algorithm — depth-first traversal of the graph — implemented as enumerator.
+    /// </summary>
+    /// <remarks>
+    /// This implementation assumes vertices to be indices in range [0, vertexCount).
+    /// </remarks>
+    /// <typeparam name="TGraph">The type of the graph.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
     public readonly partial struct Dfs<TGraph, TEdge, TEdgeEnumerator>
         where TGraph : IOutEdgesConcept<int, TEdgeEnumerator>, IHeadConcept<int, TEdge>
         where TEdgeEnumerator : IEnumerator<TEdge> { }
