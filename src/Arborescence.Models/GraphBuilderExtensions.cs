1f4d7cbf06ed348a93932004a4f0c661b48b65f2
Viktor Tselov <lightmap.tselov@gmail.com>
2020-10-13 00:33:23 +0300

    Add GraphBuilderExtensions

---
+++ src/Arborescence.Models/GraphBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Arborescence.Models
+{
+    using System;
+
+    public static class GraphBuilderExtensions
+    {
+        public static bool TryAdd<TGraph, TVertex, TEdge>(
+            this IGraphBuilder<TGraph, TVertex, TEdge> graphBuilder, TVertex tail, TVertex head)
+        {
+            if (graphBuilder is null)
+                throw new ArgumentNullException(nameof(graphBuilder));
+
+            return graphBuilder.TryAdd(tail, head, out TEdge _);
+        }
+    }
+}
