556da43ca6e2a206947298567557df8e5f6cdf44
Victor Pti <qbit86@gmail.com>
2020-10-21 21:50:19 +0300

    Add Arborescence.BasicUsage

---
+++ samples/Arborescence.BasicUsage/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arborescence
+{
+    using System;
+    using Models;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var builder = new SimpleIncidenceGraph.Builder();
+            builder.TryAdd(0, 2);
+            builder.TryAdd(0, 3);
+            builder.TryAdd(2, 3);
+            builder.TryAdd(3, 2);
+            SimpleIncidenceGraph graph = builder.ToGraph();
+            Console.WriteLine($"VertexCount: {graph.VertexCount}");
+        }
+    }
+}
