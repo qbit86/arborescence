0fe633904fbd105bdde0d55cec44d040313be8aa
Victor Pti <qbit86@gmail.com>
2020-10-22 00:11:07 +0300

    Add algorithm example

--- samples/Arborescence.BasicUsage/Program.cs
+++ samples/Arborescence.BasicUsage/Program.cs
@@ -1,19 +1,26 @@
 ﻿namespace Arborescence
 {
     using System;
+    using System.Collections.Generic;
     using Models;
+    using Traversal;
 
     internal static class Program
     {
         private static void Main()
         {
             var builder = new SimpleIncidenceGraph.Builder();
-            builder.TryAdd(0, 2);
-            builder.TryAdd(0, 3);
-            builder.TryAdd(2, 3);
+            builder.TryAdd(2, 0);
             builder.TryAdd(3, 2);
+            builder.TryAdd(0, 3);
+            builder.TryAdd(3, 0);
             SimpleIncidenceGraph graph = builder.ToGraph();
-            Console.WriteLine($"VertexCount: {graph.VertexCount}");
+
+            Bfs<SimpleIncidenceGraph, Endpoints, ArraySegment<Endpoints>.Enumerator> bfs;
+
+            IEnumerator<Endpoints> edges = bfs.EnumerateEdges(graph, graph.VertexCount, 2);
+            while (edges.MoveNext())
+                Console.WriteLine(edges.Current);
         }
     }
 }
