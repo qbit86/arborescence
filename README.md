223d65f36f5f67f8b6cc271a2ec63263e3f52b99
Victor Pti <qbit86@gmail.com>
2020-10-22 03:06:17 +0300

    Improve example

--- README.md
+++ README.md
@@ -19,20 +19,24 @@ API structure is inspired by [Concept C#] and [Boost Graph Concepts].
 ```cs
 var builder = new SimpleIncidenceGraph.Builder();
 builder.TryAdd(2, 0);
+builder.TryAdd(4, 3);
+builder.TryAdd(0, 4);
 builder.TryAdd(3, 2);
-builder.TryAdd(0, 3);
-builder.TryAdd(3, 0);
+builder.TryAdd(4, 4);
+builder.TryAdd(0, 2);
+builder.TryAdd(2, 4);
 SimpleIncidenceGraph graph = builder.ToGraph();
 
 Bfs<SimpleIncidenceGraph, Endpoints, ArraySegment<Endpoints>.Enumerator> bfs;
 
-IEnumerator<Endpoints> edges = bfs.EnumerateEdges(graph, graph.VertexCount, 2);
+IEnumerator<Endpoints> edges = bfs.EnumerateEdges(graph, graph.VertexCount, 3);
 while (edges.MoveNext())
     Console.WriteLine(edges.Current);
 ```
 ```
+[3, 2]
 [2, 0]
-[0, 3]
+[2, 4]
 ```
 
 ## License
--- samples/Arborescence.BasicUsage/Program.cs
+++ samples/Arborescence.BasicUsage/Program.cs
@@ -11,14 +11,17 @@
         {
             var builder = new SimpleIncidenceGraph.Builder();
             builder.TryAdd(2, 0);
+            builder.TryAdd(4, 3);
+            builder.TryAdd(0, 4);
             builder.TryAdd(3, 2);
-            builder.TryAdd(0, 3);
-            builder.TryAdd(3, 0);
+            builder.TryAdd(4, 4);
+            builder.TryAdd(0, 2);
+            builder.TryAdd(2, 4);
             SimpleIncidenceGraph graph = builder.ToGraph();
 
             Bfs<SimpleIncidenceGraph, Endpoints, ArraySegment<Endpoints>.Enumerator> bfs;
 
-            IEnumerator<Endpoints> edges = bfs.EnumerateEdges(graph, graph.VertexCount, 2);
+            IEnumerator<Endpoints> edges = bfs.EnumerateEdges(graph, graph.VertexCount, 3);
             while (edges.MoveNext())
                 Console.WriteLine(edges.Current);
         }
