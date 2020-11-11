dbc7dbc0f0e465b964a96d7e1c1b3dd4a9ced6b2
Victor Pti <qbit86@gmail.com>
2020-11-11 21:21:04 +0300

    Add all required fields

--- src/Arborescence.Search/Internal/MinHeap.cs
+++ src/Arborescence.Search/Internal/MinHeap.cs
@@ -3,6 +3,7 @@ namespace Arborescence.Internal
     using System;
     using System.Buffers;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Runtime.CompilerServices;
 
     internal struct MinHeap<
@@ -15,16 +16,37 @@ namespace Arborescence.Internal
         private const int DefaultCapacity = 4;
 
         private TElement[] _arrayFromPool;
+        private readonly TPriorityMap _priorityByElement;
+        private readonly TIndexMap _indexByElement;
         private readonly TPriorityComparer _priorityComparer;
+        private readonly TPriorityMapPolicy _priorityMapPolicy;
+        private readonly TIndexMapPolicy _indexMapPolicy;
         private int _count;
 
-        public MinHeap(TPriorityComparer priorityComparer)
+        internal MinHeap(TPriorityMap priorityByElement, TIndexMap indexByElement,
+            TPriorityComparer priorityComparer, TPriorityMapPolicy priorityMapPolicy, TIndexMapPolicy indexMapPolicy)
         {
+            if (priorityByElement is null)
+                throw new ArgumentNullException(nameof(priorityByElement));
+
+            if (indexByElement is null)
+                throw new ArgumentNullException(nameof(indexByElement));
+
             if (priorityComparer is null)
                 throw new ArgumentNullException(nameof(priorityComparer));
 
+            if (priorityMapPolicy is null)
+                throw new ArgumentNullException(nameof(priorityMapPolicy));
+
+            if (indexMapPolicy is null)
+                throw new ArgumentNullException(nameof(indexMapPolicy));
+
             _arrayFromPool = Array.Empty<TElement>();
+            _priorityByElement = priorityByElement;
+            _indexByElement = indexByElement;
             _priorityComparer = priorityComparer;
+            _priorityMapPolicy = priorityMapPolicy;
+            _indexMapPolicy = indexMapPolicy;
             _count = 0;
         }
 
@@ -34,10 +56,82 @@ namespace Arborescence.Internal
         {
             TElement[] arrayFromPool = _arrayFromPool;
             this = default;
-            if (arrayFromPool is null)
-                return;
+            if (arrayFromPool != null)
+                Pool.Return(arrayFromPool, ShouldClear());
+        }
 
+        internal void Add(TElement element)
+        {
+            int count = _count;
+            TElement[] array = _arrayFromPool;
+
+            if ((uint)count < (uint)array.Length)
+            {
+                UncheckedAdd(element);
+            }
+            else
+            {
+                UncheckedGrow();
+                UncheckedAdd(element);
+            }
+
+            VerifyHeap();
+        }
+
+        internal void AddOrUpdate(TElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool TryPeek(out TElement element)
+        {
+            if (_count == 0)
+            {
+                element = default;
+                return false;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        internal bool TryTake()
+        {
+            int count = _count;
+
+            if (count == 0)
+                return false;
+
+            throw new NotImplementedException();
+        }
+
+        private void UncheckedAdd(TElement element)
+        {
+            int count = _count;
+            TElement[] array = _arrayFromPool;
+            Debug.Assert((uint)count < (uint)array.Length, "(uint)count < (uint)array.Length");
+
+            throw new NotImplementedException();
+        }
+
+        private void UncheckedGrow()
+        {
+            int count = _count;
+            TElement[] arrayFromPool = _arrayFromPool;
+            Debug.Assert((uint)count == (uint)arrayFromPool.Length, "(uint)count == (uint)arrayFromPool.Length");
+
+            int newCapacity = count > 0 ? count << 1 : DefaultCapacity;
+            TElement[] newArrayFromPool = Pool.Rent(newCapacity);
+            if (count > 0)
+                Array.Copy(arrayFromPool, newArrayFromPool, count);
+
+            _arrayFromPool = newArrayFromPool;
             Pool.Return(arrayFromPool, ShouldClear());
+        }
+
+        [Conditional("DEBUG")]
+        private void VerifyHeap()
+        {
+            throw new NotImplementedException();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
