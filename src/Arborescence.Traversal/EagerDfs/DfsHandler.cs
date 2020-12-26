91866c4d38444a34c85d58abc9eff2c1769bbad2
Viktor Pti <qbit86@gmail.com>
2020-12-26 12:51:58 +0300

    Rename event handlers

--- src/Arborescence.Traversal/EagerDfs/DfsHandler.cs
+++ src/Arborescence.Traversal/EagerDfs/DfsHandler.cs
@@ -30,41 +30,41 @@
         /// <summary>
         /// Raised for the source vertex once before the start of the search.
         /// </summary>
-        public event VertexEventHandler<TGraph, TVertex> StartVertex;
+        public event VertexHandler<TGraph, TVertex> StartVertex;
 
         /// <summary>
         /// Raised when a vertex is encountered for the first time.
         /// </summary>
-        public event VertexEventHandler<TGraph, TVertex> DiscoverVertex;
+        public event VertexHandler<TGraph, TVertex> DiscoverVertex;
 
         /// <summary>
         /// Raised for a vertex after finished processing all of its descendants.
         /// </summary>
-        public event VertexEventHandler<TGraph, TVertex> FinishVertex;
+        public event VertexHandler<TGraph, TVertex> FinishVertex;
 
         /// <summary>
         /// Raised for every out-edge of each vertex after it is discovered.
         /// </summary>
-        public event EdgeEventHandler<TGraph, TEdge> ExamineEdge;
+        public event EdgeHandler<TGraph, TEdge> ExamineEdge;
 
         /// <summary>
         /// Raised for each edge as it becomes a member of the search tree.
         /// </summary>
-        public event EdgeEventHandler<TGraph, TEdge> TreeEdge;
+        public event EdgeHandler<TGraph, TEdge> TreeEdge;
 
         /// <summary>
         /// Raised for an edge as it is classified as a back edge in the graph.
         /// </summary>
-        public event EdgeEventHandler<TGraph, TEdge> BackEdge;
+        public event EdgeHandler<TGraph, TEdge> BackEdge;
 
         /// <summary>
         /// Raised for an edge as it is classified as a forward or cross edge in the graph.
         /// </summary>
-        public event EdgeEventHandler<TGraph, TEdge> ForwardOrCrossEdge;
+        public event EdgeHandler<TGraph, TEdge> ForwardOrCrossEdge;
 
         /// <summary>
         /// Raised for each edge when processing its head is finished.
         /// </summary>
-        public event EdgeEventHandler<TGraph, TEdge> FinishEdge;
+        public event EdgeHandler<TGraph, TEdge> FinishEdge;
     }
 }
