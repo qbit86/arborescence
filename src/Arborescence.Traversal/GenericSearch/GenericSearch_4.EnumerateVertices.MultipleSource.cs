4d1b751f870039a5b550d99a5dd1e96e64a8a90f
Viktor Pti <qbit86@gmail.com>
2021-01-03 20:43:31 +0300

    Add EnumerateVertices() for multiple sources

---
+++ src/Arborescence.Traversal/GenericSearch/GenericSearch_4.EnumerateVertices.MultipleSource.cs
@@ -0,0 +1,67 @@
+namespace Arborescence.Traversal
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    public readonly partial struct GenericSearch<TGraph, TVertex, TEdge, TEdgeEnumerator>
+    {
+        /// <summary>
+        /// Enumerates vertices of the graph in an order specified by the fringe starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="fringe">The collection of discovered vertices which are not finished yet.</param>
+        /// <param name="exploredSet">The set of explored vertices.</param>
+        /// <typeparam name="TVertexEnumerator">The type of the vertex enumerator.</typeparam>
+        /// <returns>An enumerator to enumerate the vertices of the the graph.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>.
+        /// </exception>
+        public IEnumerator<TVertex> EnumerateVertices<TVertexEnumerator, TFringe, TExploredSet>(
+            TGraph graph, TVertexEnumerator sources, TFringe fringe, TExploredSet exploredSet)
+            where TVertexEnumerator : IEnumerator<TVertex>
+            where TFringe : IProducerConsumerCollection<TVertex>
+            where TExploredSet : ISet<TVertex>
+        {
+            if (graph == null)
+                throw new ArgumentNullException(nameof(graph));
+
+            if (sources == null)
+                throw new ArgumentNullException(nameof(sources));
+
+            while (sources.MoveNext())
+            {
+                TVertex source = sources.Current;
+                exploredSet.Add(source);
+                yield return source;
+                if (!fringe.TryAdd(source))
+                    throw new InvalidOperationException(nameof(fringe.TryAdd));
+            }
+
+            while (fringe.TryTake(out TVertex u))
+            {
+#if DEBUG
+                Debug.Assert(exploredSet.Contains(u));
+#endif
+                TEdgeEnumerator outEdges = graph.EnumerateOutEdges(u);
+                while (outEdges.MoveNext())
+                {
+                    TEdge e = outEdges.Current;
+                    if (!graph.TryGetHead(e, out TVertex v))
+                        continue;
+
+                    if (exploredSet.Contains(v))
+                        continue;
+
+                    exploredSet.Add(v);
+                    yield return v;
+                    if (!fringe.TryAdd(v))
+                        throw new InvalidOperationException(nameof(fringe.TryAdd));
+                }
+            }
+        }
+    }
+}
