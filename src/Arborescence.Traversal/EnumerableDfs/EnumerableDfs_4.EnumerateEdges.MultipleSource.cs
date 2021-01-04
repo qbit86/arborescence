b0e215ac28f3c74edaab09f3a8c28cee015071ad
Viktor Pti <qbit86@gmail.com>
2021-01-04 11:11:03 +0300

    Add EnumerableDfs`4.EnumerateEdges<>() for multiple sources

---
+++ src/Arborescence.Traversal/EnumerableDfs/EnumerableDfs_4.EnumerateEdges.MultipleSource.cs
@@ -0,0 +1,57 @@
+namespace Arborescence.Traversal
+{
+    using System;
+    using System.Collections.Generic;
+
+    public readonly partial struct EnumerableDfs<TGraph, TVertex, TEdge, TEdgeEnumerator>
+    {
+        public IEnumerator<TEdge> EnumerateEdges<TVertexEnumerator, TExploredSet>(
+            TGraph graph, TVertexEnumerator sources, TExploredSet exploredSet)
+            where TVertexEnumerator : IEnumerator<TVertex>
+            where TExploredSet : ISet<TVertex>
+        {
+            if (graph == null)
+                throw new ArgumentNullException(nameof(graph));
+
+            if (sources == null)
+                throw new ArgumentNullException(nameof(sources));
+
+            var stack = new Internal.Stack<TEdgeEnumerator>();
+            try
+            {
+                while (sources.MoveNext())
+                {
+                    TVertex source = sources.Current;
+                    if (exploredSet.Contains(source))
+                        continue;
+
+                    exploredSet.Add(source);
+                    stack.Add(graph.EnumerateOutEdges(source));
+
+                    while (stack.TryTake(out TEdgeEnumerator outEdges))
+                    {
+                        if (!outEdges.MoveNext())
+                            continue;
+
+                        stack.Add(outEdges);
+
+                        TEdge e = outEdges.Current;
+                        if (!graph.TryGetHead(e, out TVertex v))
+                            continue;
+
+                        if (exploredSet.Contains(v))
+                            continue;
+
+                        yield return e;
+                        exploredSet.Add(v);
+                        stack.Add(graph.EnumerateOutEdges(v));
+                    }
+                }
+            }
+            finally
+            {
+                stack.Dispose();
+            }
+        }
+    }
+}
