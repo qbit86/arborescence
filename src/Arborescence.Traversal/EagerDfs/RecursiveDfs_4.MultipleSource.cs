d77020d3734364cd496ee09047ec341b5a4fb8f5
Viktor Pti <qbit86@gmail.com>
2021-01-08 02:44:12 +0300

    Add XML-doc comments for RecursiveDfs`4

--- src/Arborescence.Traversal/EagerDfs/RecursiveDfs_4.MultipleSource.cs
+++ src/Arborescence.Traversal/EagerDfs/RecursiveDfs_4.MultipleSource.cs
@@ -5,6 +5,24 @@ namespace Arborescence.Traversal
 
     public readonly partial struct RecursiveDfs<TGraph, TVertex, TEdge, TEdgeEnumerator>
     {
+        /// <summary>
+        /// Traverses the graph in a DFS manner starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="colorMap">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <typeparam name="TVertexEnumerator">The type of the vertex enumerator.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
         public void Traverse<TVertexEnumerator, TColorMap, THandler>(
             TGraph graph, TVertexEnumerator sources, TColorMap colorMap, THandler handler)
             where TVertexEnumerator : IEnumerator<TVertex>
@@ -32,6 +50,25 @@ namespace Arborescence.Traversal
             }
         }
 
+        /// <summary>
+        /// Traverses the graph in a DFS manner starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="colorMap">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="startVertex">The source to start with.</param>
+        /// <typeparam name="TVertexEnumerator">The type of the vertex enumerator.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
         public void Traverse<TVertexEnumerator, TColorMap, THandler>(
             TGraph graph, TVertexEnumerator sources, TColorMap colorMap, THandler handler, TVertex startVertex)
             where TVertexEnumerator : IEnumerator<TVertex>
