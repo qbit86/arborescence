f09db2624cd0800215f194870f11370ed0eefa76
Viktor Pti <qbit86@gmail.com>
2021-01-19 09:57:49 +0300

    Fix conditional compilation

--- src/Arborescence.Search/Internal/MinHeap.cs
+++ src/Arborescence.Search/Internal/MinHeap.cs
@@ -351,7 +351,7 @@ namespace Arborescence.Internal
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool ShouldClear()
         {
-#if NETSTANDARD2_1 || NETCOREAPP2_0
+#if NETSTANDARD2_1 || NETCOREAPP2_1
             return RuntimeHelpers.IsReferenceOrContainsReferences<TElement>();
 #else
             return true;
