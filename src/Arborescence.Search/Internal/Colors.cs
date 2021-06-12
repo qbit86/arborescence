f29528e78b60238f6eaa9e51564f30251ccce9f4
Viktor T <qbit86@gmail.com>
2021-06-12 20:31:42 +0300

    Remove unused internal static class Colors

--- src/Arborescence.Search/Internal/Colors.cs
+++
@@ -1,9 +0,0 @@
-namespace Arborescence.Internal
-{
-    internal static class Colors
-    {
-        internal const byte White = 0;
-        internal const byte Gray = 1;
-        internal const byte Black = 2;
-    }
-}
