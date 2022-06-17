fcfc072976ceb44a3050f697738b31b9fd750812
Viktor Pti <qbit86@gmail.com>
2022-06-18 00:04:59 +0300

    Add XML-doc comments for `AdditiveMonoid<T>`

--- src/Arborescence.Models/AdditiveMonoid.cs
+++ src/Arborescence.Models/AdditiveMonoid.cs
@@ -4,11 +4,16 @@ using System.Numerics;
 
 namespace Arborescence.Models
 {
+    /// <summary>
+    /// Represents a monoid for the elements of generic type <typeparamref name="T"/> with addition as the binary operation.
+    /// </summary>
     public readonly struct AdditiveMonoid<T> : IMonoid<T>
         where T: IAdditionOperators<T, T, T>, IAdditiveIdentity<T, T>
     {
+        /// <inheritdoc />
         public T Identity => T.AdditiveIdentity;
 
+        /// <inheritdoc />
         public T Combine(T left, T right) => left + right;
     }
 }
