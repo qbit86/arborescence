4e91c1fe4d8acbcc006d3a7bc6e2d731d1c58818
Viktor Ptitselov <qbit86@gmail.com>
2023-01-06 23:51:37 +0200

    [Models] Add shared project Arborescence.TryHelpers

---
+++ src/Arborescence.Shared/Arborescence.TryHelpers/TryHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Arborescence
+{
+    internal static class TryHelpers
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static bool Some<T>(T valueToReturn, out T value)
+        {
+            value = valueToReturn;
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static bool None<T>([MaybeNullWhen(false)] out T value)
+        {
+            value = default;
+            return false;
+        }
+    }
+}
