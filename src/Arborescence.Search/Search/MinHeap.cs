4c7105568fea99ff26f5aa072944fa313297c153
Viktor Ptitselov <qbit86@gmail.com>
2023-01-09 17:38:13 +0200

    [Search] Simplify conditional compilation

--- src/Arborescence.Search/Search/MinHeap.cs
+++ src/Arborescence.Search/Search/MinHeap.cs
@@ -334,7 +334,7 @@ namespace Arborescence.Search
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool ShouldClear()
         {
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER || NET5_0_OR_GREATER
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER
             return RuntimeHelpers.IsReferenceOrContainsReferences<TElement>();
 #else
             return true;
