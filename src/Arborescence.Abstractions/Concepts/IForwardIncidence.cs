2d6c67821e0e4c789db2b44dd1e4bbcdbd1559b4
Viktor Ptitselov <qbit86@gmail.com>
2023-01-11 20:57:18 +0200

    [Abstractions] Add IForwardIncidence<TVertex, TEdge, TEdges>. Deprecate ITraversable<TVertex, TEdge, TEdges>.

--- src/Arborescence.Abstractions/Concepts/IForwardIncidence.cs
+++ src/Arborescence.Abstractions/Concepts/IForwardIncidence.cs
@@ -1,7 +1,5 @@
 namespace Arborescence
 {
-    using System;
-
     /// <summary>
     /// Represents the concept of traversing along the edges of a graph —
     /// an access to the head of each edge and to the outgoing edges of each vertex.
@@ -9,8 +7,6 @@ namespace Arborescence
     /// <typeparam name="TVertex">The type of the vertex.</typeparam>
     /// <typeparam name="TEdge">The type of the edge.</typeparam>
     /// <typeparam name="TEdges">The type of the edges enumerator.</typeparam>
-    [Obsolete(
-        "ITraversable<TVertex, TEdge, TEdges> has been deprecated. Use IForwardIncidence<TVertex, TEdge, TEdges> instead.")]
-    public interface ITraversable<TVertex, in TEdge, out TEdges> :
+    public interface IForwardIncidence<TVertex, in TEdge, out TEdges> :
         IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdges> { }
 }
