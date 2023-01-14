e0cbd9d81c302e937e60c63c0efd6dc753129925
Viktor Ptitselov <qbit86@gmail.com>
2023-01-14 16:20:49 +0200

    [Traversal.Adjacency] Add EnumerateEdgesIterator<> for single source

---
+++ src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_2.EnumerateEdges.SingleSource.cs
@@ -0,0 +1,47 @@
+#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP1_0_OR_GREATER || NET47_OR_GREATER
+namespace Arborescence.Traversal.Adjacency
+{
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    public static partial class EnumerableGenericSearch<TVertex, TNeighborEnumerator>
+    {
+        internal static IEnumerator<(TVertex Tail, TVertex Head)> EnumerateEdgesIterator<
+            TGraph, TFrontier, TExploredSet>(
+            TGraph graph, TVertex source, TFrontier frontier, TExploredSet exploredSet)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator>
+            where TFrontier : IProducerConsumerCollection<TVertex>
+            where TExploredSet : ISet<TVertex>
+        {
+            exploredSet.Add(source);
+            frontier.AddOrThrow(source);
+
+            while (frontier.TryTake(out TVertex? current))
+            {
+#if DEBUG
+                Debug.Assert(exploredSet.Contains(current));
+#endif
+                TNeighborEnumerator neighbors = graph.EnumerateAdjacentVertices(current);
+                try
+                {
+                    while (neighbors.MoveNext())
+                    {
+                        TVertex neighbor = neighbors.Current;
+                        if (exploredSet.Contains(neighbor))
+                            continue;
+
+                        yield return (current, neighbor);
+                        exploredSet.Add(neighbor);
+                        frontier.AddOrThrow(neighbor);
+                    }
+                }
+                finally
+                {
+                    neighbors.Dispose();
+                }
+            }
+        }
+    }
+}
+#endif
