515b71fe839e94d7eee12e1906872e8742a36f69
Viktor Ptitselov <qbit86@gmail.com>
2023-01-15 01:23:47 +0200

    [Traversal.Adjacency] Add XML-doc comments for EnumerableGenericSearch_2.EnumerateEdges

--- src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_2.EnumerateEdges.MultipleSource.cs.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_2.EnumerateEdges.MultipleSource.cs.cs
@@ -8,6 +8,25 @@ namespace Arborescence.Traversal.Adjacency
 
     public static partial class EnumerableGenericSearch<TVertex, TNeighborEnumerator>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in an order specified by the frontier starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="frontier">The collection of discovered vertices which are not finished yet.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TSourceEnumerator">The type of the source enumerator.</typeparam>
+        /// <typeparam name="TFrontier">The type of the generic queue.</typeparam>
+        /// <returns>An enumerator to enumerate the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="frontier"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="System.InvalidOperationException">
+        /// <see cref="IProducerConsumerCollection{TVertex}.TryAdd"/> for <paramref name="frontier"/>
+        /// returns <see langword="false"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<(TVertex Tail, TVertex Head)> EnumerateEdges<TGraph, TSourceEnumerator, TFrontier>(
             TGraph graph, TSourceEnumerator sources, TFrontier frontier)
@@ -16,6 +35,26 @@ namespace Arborescence.Traversal.Adjacency
             where TFrontier : IProducerConsumerCollection<TVertex> =>
             EnumerateEdgesChecked(graph, sources, frontier);
 
+        /// <summary>
+        /// Enumerates edges of the graph in an order specified by the frontier starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="frontier">The collection of discovered vertices which are not finished yet.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TSourceEnumerator">The type of the source enumerator.</typeparam>
+        /// <typeparam name="TFrontier">The type of the generic queue.</typeparam>
+        /// <returns>An enumerator to enumerate the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="frontier"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="System.InvalidOperationException">
+        /// <see cref="IProducerConsumerCollection{TVertex}.TryAdd"/> for <paramref name="frontier"/>
+        /// returns <see langword="false"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<(TVertex Tail, TVertex Head)> EnumerateEdges<TGraph, TSourceEnumerator, TFrontier>(
             TGraph graph, TSourceEnumerator sources, TFrontier frontier, IEqualityComparer<TVertex> comparer)
@@ -24,6 +63,28 @@ namespace Arborescence.Traversal.Adjacency
             where TFrontier : IProducerConsumerCollection<TVertex> =>
             EnumerateEdgesChecked(graph, sources, frontier, comparer);
 
+        /// <summary>
+        /// Enumerates edges of the graph in an order specified by the frontier starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="frontier">The collection of discovered vertices which are not finished yet.</param>
+        /// <param name="exploredSet">The set of explored vertices.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TSourceEnumerator">The type of the source enumerator.</typeparam>
+        /// <typeparam name="TFrontier">The type of the generic queue.</typeparam>
+        /// <typeparam name="TExploredSet">The type of the set of explored vertices.</typeparam>
+        /// <returns>An enumerator to enumerate the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="frontier"/> is <see langword="null"/>,
+        /// or <paramref name="exploredSet"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="System.InvalidOperationException">
+        /// <see cref="IProducerConsumerCollection{TVertex}.TryAdd"/> for <paramref name="frontier"/>
+        /// returns <see langword="false"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<(TVertex Tail, TVertex Head)> EnumerateEdges<
             TGraph, TSourceEnumerator, TFrontier, TExploredSet>(
