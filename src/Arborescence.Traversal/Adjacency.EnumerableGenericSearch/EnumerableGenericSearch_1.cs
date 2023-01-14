af0092babdea1e85176b58038ed1b72a969c4c92
Viktor Ptitselov <qbit86@gmail.com>
2023-01-15 00:27:05 +0200

    [Traversal.Adjacency] Add XML-doc comments for EnumerableGenericSearch<>

--- src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_1.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_1.cs
@@ -1,4 +1,9 @@
 namespace Arborescence.Traversal.Adjacency
 {
+    /// <summary>
+    /// Represents the generic search algorithm — traversal of the graph
+    /// where the order of exploring vertices is determined by the frontier implementation.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
     public static partial class EnumerableGenericSearch<TVertex> { }
 }
