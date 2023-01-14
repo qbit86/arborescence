fb5a3cbebd3325f622b15862358a7870d6455adb
Viktor Ptitselov <qbit86@gmail.com>
2023-01-14 23:42:12 +0200

    [Traversal.Adjacency] Add [MethodImpl(MethodImplOptions.AggressiveInlining)] attributes

--- src/Arborescence.Traversal/Adjacency.EnumerableBfs/EnumerableBfs_1.EnumerateVertices.MultipleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableBfs/EnumerableBfs_1.EnumerateVertices.MultipleSource.cs
@@ -1,21 +1,25 @@
 namespace Arborescence.Traversal.Adjacency
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public static partial class EnumerableBfs<TVertex>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<TVertex> EnumerateVertices<TGraph, TSourceEnumerator>(
             TGraph graph, TSourceEnumerator sources)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
             where TSourceEnumerator : IEnumerator<TVertex> =>
             EnumerableBfs<TVertex, IEnumerator<TVertex>>.EnumerateVerticesChecked(graph, sources);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<TVertex> EnumerateVertices<TGraph, TSourceEnumerator>(
             TGraph graph, TSourceEnumerator sources, IEqualityComparer<TVertex> comparer)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
             where TSourceEnumerator : IEnumerator<TVertex> =>
             EnumerableBfs<TVertex, IEnumerator<TVertex>>.EnumerateVerticesChecked(graph, sources, comparer);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<TVertex> EnumerateVertices<TGraph, TSourceEnumerator, TExploredSet>(
             TGraph graph, TSourceEnumerator sources, TExploredSet exploredSet)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
--- src/Arborescence.Traversal/Adjacency.EnumerableBfs/EnumerableBfs_1.EnumerateVertices.SingleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableBfs/EnumerableBfs_1.EnumerateVertices.SingleSource.cs
@@ -1,18 +1,22 @@
 namespace Arborescence.Traversal.Adjacency
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public static partial class EnumerableBfs<TVertex>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<TVertex> EnumerateVertices<TGraph>(TGraph graph, TVertex source)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>> =>
             EnumerableBfs<TVertex, IEnumerator<TVertex>>.EnumerateVerticesChecked(graph, source);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<TVertex> EnumerateVertices<TGraph>(
             TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>> =>
             EnumerableBfs<TVertex, IEnumerator<TVertex>>.EnumerateVerticesChecked(graph, source, comparer);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerator<TVertex> EnumerateVertices<TGraph, TExploredSet>(
             TGraph graph, TVertex source, TExploredSet exploredSet)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
