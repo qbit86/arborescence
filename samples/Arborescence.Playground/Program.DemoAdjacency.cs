77692c1c1698ac5b210ab4a2c6fb04d82ed6f07f
Viktor Ptitselov <qbit86@gmail.com>
2023-01-15 13:15:55 +0200

    [Samples] Fix DemoAdjacency()

--- samples/Arborescence.Playground/Program.DemoAdjacency.cs
+++ samples/Arborescence.Playground/Program.DemoAdjacency.cs
@@ -1,36 +1,35 @@
-namespace Arborescence
+namespace Arborescence;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Models;
+using Traversal.Adjacency;
+using Workbench;
+using EdgeEnumerator = System.ArraySegment<Endpoints>.Enumerator;
+
+internal static partial class Program
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using Models;
-    using Traversal.Adjacency;
-    using Workbench;
-    using EdgeEnumerator = System.ArraySegment<Endpoints>.Enumerator;
-
-    internal static partial class Program
+    private static void DemoAdjacency()
     {
-        private static void DemoAdjacency()
+        SimpleIncidenceGraph.Builder builder = new(18);
+        using (TextReader textReader = IndexedGraphs.GetTextReader("09"))
         {
-            SimpleIncidenceGraph.Builder builder = new(18);
-            using (TextReader textReader = IndexedGraphs.GetTextReader("05"))
-            {
-                IEnumerable<Endpoints> edges = IndexedEdgeListParser.ParseEdges(textReader);
-                foreach (Endpoints edge in edges)
-                    builder.Add(edge.Tail, edge.Head);
-            }
+            IEnumerable<Endpoints> edges = IndexedEdgeListParser.ParseEdges(textReader);
+            foreach (Endpoints edge in edges)
+                builder.Add(edge.Tail, edge.Head);
+        }
 
-            SimpleIncidenceGraph incidenceGraph = builder.ToGraph();
-            var adjacencyGraph = IncidenceAdjacencyAdapter<int, Endpoints, EdgeEnumerator>.Create(incidenceGraph);
-            IEnumerable<int> sources = Enumerable.Range(0, incidenceGraph.VertexCount);
-            IEnumerator<(int Tail, int Head)> edgeEnumerator =
-                EnumerableBfs<int>.EnumerateEdges(adjacencyGraph, sources.GetEnumerator());
-            while (edgeEnumerator.MoveNext())
-            {
-                (int tail, int head) = edgeEnumerator.Current;
-                Console.WriteLine($"{V(tail)} -> {V(head)}");
-            }
+        SimpleIncidenceGraph incidenceGraph = builder.ToGraph();
+        var adjacencyGraph = IncidenceAdjacencyAdapter<int, Endpoints, EdgeEnumerator>.Create(incidenceGraph);
+        IEnumerable<int> sources = "abcd".Select(it => Base32.Parse(it.ToString()));
+        IEnumerator<(int Tail, int Head)> edgeEnumerator =
+            EnumerableBfs<int>.EnumerateEdges(adjacencyGraph, sources.GetEnumerator());
+        while (edgeEnumerator.MoveNext())
+        {
+            (int tail, int head) = edgeEnumerator.Current;
+            Console.WriteLine($"{V(tail)} -> {V(head)}");
         }
     }
 }
