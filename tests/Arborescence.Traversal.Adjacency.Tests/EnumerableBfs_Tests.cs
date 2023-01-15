6987cb2621d45b44f71cfc4091f484cbcf17f42e
Viktor Ptitselov <qbit86@gmail.com>
2023-01-16 00:22:16 +0200

    [Tests] Add Arborescence.Traversal.Adjacency.Tests

---
+++ tests/Arborescence.Traversal.Adjacency.Tests/EnumerableBfs_Tests.cs
@@ -0,0 +1,62 @@
+namespace Arborescence.Traversal.Adjacency.Tests;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Models;
+using Workbench;
+using Xunit;
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER
+using Graph = Models.SimpleIncidenceGraph;
+using EdgeEnumerator = System.ArraySegment<Endpoints>.Enumerator;
+#else
+using Graph = Models.Compatibility.SimpleIncidenceGraph;
+using EdgeEnumerator = System.Collections.Generic.IEnumerator<Endpoints>;
+#endif
+
+public sealed class EnumerableBfs_Tests
+{
+    [Fact]
+    public void EnumerateEdges_SingleSource_ReturnsCorrectEdgesInOrder()
+    {
+        // Arrange
+        Graph.Builder builder = new(0, 31);
+        using (TextReader textReader = IndexedGraphs.GetTextReader("08"))
+        {
+            IEnumerable<Endpoints> edges = IndexedEdgeListParser.ParseEdges(textReader);
+            foreach (Endpoints edge in edges)
+                builder.Add(edge.Tail, edge.Head);
+        }
+
+        Graph incidenceGraph = builder.ToGraph();
+        var adjacencyGraph = IncidenceAdjacencyAdapter<int, Endpoints, EdgeEnumerator>.Create(incidenceGraph);
+
+        List<(string Tail, string Head)> expectedBase32 = new(12)
+        {
+            ("d", "g"),
+            ("d", "k"),
+            ("g", "a"),
+            ("a", "f"),
+            ("a", "j"),
+            ("a", "n"),
+            ("f", "p"),
+            ("j", "r"),
+            ("p", "m"),
+            ("p", "q"),
+            ("r", "e"),
+            ("m", "i")
+        };
+        var expected = expectedBase32.Select(it => (V(it.Tail), V(it.Head))).ToList();
+
+        // Act
+        List<(int Tail, int Head)> actual = new(expected.Count);
+        IEnumerator<(int Tail, int Head)> edgeEnumerator = EnumerableBfs<int>.EnumerateEdges(adjacencyGraph, V("d"));
+        while (edgeEnumerator.MoveNext())
+            actual.Add(edgeEnumerator.Current);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    private static int V(string idBase32) => Base32.Parse(idBase32);
+}
