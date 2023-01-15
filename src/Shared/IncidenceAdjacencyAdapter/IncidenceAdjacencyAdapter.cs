9c17284adac70b99464199ed645f5c5db743af2f
Viktor Ptitselov <qbit86@gmail.com>
2023-01-15 12:07:50 +0200

    [Shared] Add IncidenceAdjacencyAdapter<TVertex, TEdge, TEdgeEnumerator>

---
+++ src/Shared/IncidenceAdjacencyAdapter/IncidenceAdjacencyAdapter.cs
@@ -0,0 +1,48 @@
+﻿namespace Arborescence.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    internal static class IncidenceAdjacencyAdapter<TVertex, TEdge, TEdgeEnumerator>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+    {
+        internal static IncidenceAdjacencyAdapter<TVertex, TEdge, TEdgeEnumerator, TGraph> Create<TGraph>(TGraph graph)
+            where TGraph : ITailIncidence<TVertex, TEdge>, IHeadIncidence<TVertex, TEdge>,
+            IForwardIncidence<TVertex, TEdge, TEdgeEnumerator> =>
+            new(graph);
+    }
+
+    internal sealed class IncidenceAdjacencyAdapter<TVertex, TEdge, TEdgeEnumerator, TGraph> :
+        IAdjacency<TVertex, IEnumerator<TVertex>>, ITailIncidence<TVertex, TEdge>, IHeadIncidence<TVertex, TEdge>,
+        IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TGraph : ITailIncidence<TVertex, TEdge>, IHeadIncidence<TVertex, TEdge>,
+        IForwardIncidence<TVertex, TEdge, TEdgeEnumerator>
+    {
+        private readonly TGraph _graph;
+
+        public IncidenceAdjacencyAdapter(TGraph graph) =>
+            _graph = graph ?? throw new ArgumentNullException(nameof(graph));
+
+        public IEnumerator<TVertex> EnumerateNeighbors(TVertex vertex) => EnumerateNeighborsIterator(vertex);
+
+        public bool TryGetHead(TEdge edge, [MaybeNullWhen(false)] out TVertex head) =>
+            _graph.TryGetHead(edge, out head);
+
+        public TEdgeEnumerator EnumerateOutEdges(TVertex vertex) => _graph.EnumerateOutEdges(vertex);
+
+        public bool TryGetTail(TEdge edge, [MaybeNullWhen(false)] out TVertex tail) =>
+            _graph.TryGetTail(edge, out tail);
+
+        private IEnumerator<TVertex> EnumerateNeighborsIterator(TVertex vertex)
+        {
+            TEdgeEnumerator edgeEnumerator = _graph.EnumerateOutEdges(vertex);
+            while (edgeEnumerator.MoveNext())
+            {
+                if (_graph.TryGetHead(edgeEnumerator.Current, out TVertex? neighbor))
+                    yield return neighbor;
+            }
+        }
+    }
+}
