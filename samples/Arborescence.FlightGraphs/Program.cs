db6b15282fa88196bcbf42857c4cb57ae437ab7e
Viktor Ptitselov <qbit86@gmail.com>
2023-01-16 21:29:39 +0200

    [Samples] Add usage example for FlightIncidenceGraph

--- samples/Arborescence.FlightGraphs/Program.cs
+++ samples/Arborescence.FlightGraphs/Program.cs
@@ -6,3 +6,17 @@ ArraySegment<string>.Enumerator istanbulNeighborEnumerator =
     adjacencyGraph.EnumerateNeighbors("IST");
 while (istanbulNeighborEnumerator.MoveNext())
     Console.WriteLine(istanbulNeighborEnumerator.Current);
+
+Console.WriteLine();
+
+var incidenceGraph = FlightIncidenceGraph.Create();
+ArraySegment<int>.Enumerator istanbulFlightEnumerator = incidenceGraph.EnumerateOutEdges("IST");
+while (istanbulFlightEnumerator.MoveNext())
+    Console.WriteLine(istanbulFlightEnumerator.Current);
+
+Console.WriteLine();
+
+if (incidenceGraph.TryGetTail(676, out string? flight676Origin))
+    Console.WriteLine(flight676Origin);
+if (incidenceGraph.TryGetHead(676, out string? flight676Destination))
+    Console.WriteLine(flight676Destination);
