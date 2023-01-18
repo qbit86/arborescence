134049c10a511050077d581ce1f7aeb7051e5543
Viktor Ptitselov <qbit86@gmail.com>
2023-01-18 23:00:52 +0200

    [Traversal.Adjacency] Add EnumerableDfs<TVertex, TNeighborEnumerator>.EnumerateEdges<>() for single source

--- src/Arborescence.Traversal/Adjacency.EnumerableDfs/EnumerableDfs_2.EnumerateEdges.SingleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableDfs/EnumerableDfs_2.EnumerateEdges.SingleSource.cs
@@ -1,9 +1,99 @@
 namespace Arborescence.Traversal.Adjacency
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public static partial class EnumerableDfs<TVertex, TNeighborEnumerator>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <returns>An enumerator to enumerate the endpoints of a depth-first search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerator<Endpoints<TVertex>> EnumerateEdges<TGraph>(
+            TGraph graph, TVertex source)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator> =>
+            EnumerateEdgesChecked(graph, source);
+
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <returns>An enumerator to enumerate the endpoints of a depth-first search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerator<Endpoints<TVertex>> EnumerateEdges<TGraph>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator> =>
+            EnumerateEdgesChecked(graph, source, comparer);
+
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="exploredSet">The set of explored vertices.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TExploredSet">The type of the set of explored vertices.</typeparam>
+        /// <returns>An enumerator to enumerate the endpoints of a depth-first search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="exploredSet"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerator<Endpoints<TVertex>> EnumerateEdges<TGraph, TExploredSet>(
+            TGraph graph, TVertex source, TExploredSet exploredSet)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator>
+            where TExploredSet : ISet<TVertex> =>
+            EnumerateEdgesChecked(graph, source, exploredSet);
+
+        internal static IEnumerator<Endpoints<TVertex>> EnumerateEdgesChecked<TGraph>(
+            TGraph graph, TVertex source)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            HashSet<TVertex> exploredSet = new();
+            return EnumerateEdgesIterator(graph, source, exploredSet);
+        }
+
+        internal static IEnumerator<Endpoints<TVertex>> EnumerateEdgesChecked<TGraph>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            HashSet<TVertex> exploredSet = new(comparer);
+            return EnumerateEdgesIterator(graph, source, exploredSet);
+        }
+
+        internal static IEnumerator<Endpoints<TVertex>> EnumerateEdgesChecked<TGraph, TExploredSet>(
+            TGraph graph, TVertex source, TExploredSet exploredSet)
+            where TGraph : IAdjacency<TVertex, TNeighborEnumerator>
+            where TExploredSet : ISet<TVertex>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (exploredSet is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(exploredSet));
+
+            return EnumerateEdgesIterator(graph, source, exploredSet);
+        }
+
         private static IEnumerator<Endpoints<TVertex>> EnumerateEdgesIterator<TGraph, TExploredSet>(
             TGraph graph, TVertex source, TExploredSet exploredSet)
             where TGraph : IAdjacency<TVertex, TNeighborEnumerator>
--- src/Arborescence.Traversal/Adjacency.EnumerableDfs/EnumerableDfs_2.EnumerateVertices.SingleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableDfs/EnumerableDfs_2.EnumerateVertices.SingleSource.cs
@@ -11,7 +11,7 @@ namespace Arborescence.Traversal.Adjacency
         /// <param name="graph">The graph.</param>
         /// <param name="source">The source.</param>
         /// <typeparam name="TGraph">The type of the graph.</typeparam>
-        /// <returns>An enumerator to enumerate the vertices of a search tree.</returns>
+        /// <returns>An enumerator to enumerate the vertices of a depth-first search tree.</returns>
         /// <exception cref="System.ArgumentNullException">
         /// <paramref name="graph"/> is <see langword="null"/>.
         /// </exception>
@@ -27,7 +27,7 @@ namespace Arborescence.Traversal.Adjacency
         /// <param name="source">The source.</param>
         /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
         /// <typeparam name="TGraph">The type of the graph.</typeparam>
-        /// <returns>An enumerator to enumerate the vertices of a search tree.</returns>
+        /// <returns>An enumerator to enumerate the vertices of a depth-first search tree.</returns>
         /// <exception cref="System.ArgumentNullException">
         /// <paramref name="graph"/> is <see langword="null"/>.
         /// </exception>
@@ -45,7 +45,7 @@ namespace Arborescence.Traversal.Adjacency
         /// <param name="exploredSet">The set of explored vertices.</param>
         /// <typeparam name="TGraph">The type of the graph.</typeparam>
         /// <typeparam name="TExploredSet">The type of the set of explored vertices.</typeparam>
-        /// <returns>An enumerator to enumerate the vertices of a search tree.</returns>
+        /// <returns>An enumerator to enumerate the vertices of a depth-first search tree.</returns>
         /// <exception cref="System.ArgumentNullException">
         /// <paramref name="graph"/> is <see langword="null"/>,
         /// or <paramref name="exploredSet"/> is <see langword="null"/>.
