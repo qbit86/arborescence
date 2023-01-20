a17700c070f2d4d6a80c5707dd0d3b99c18463f6
Viktor Ptitselov <qbit86@gmail.com>
2023-01-21 01:20:59 +0200

    [Traversal.Incidence] Add EnumerableGenericSearch<TVertex, TEdge>.EnumerateVertices<>() for single source

---
+++ src/Arborescence.Traversal/Incidence.EnumerableGenericSearch/EnumerableGenericSearch_2.cs
@@ -0,0 +1,10 @@
+namespace Arborescence.Traversal.Incidence
+{
+    /// <summary>
+    /// Represents the generic search algorithm — traversal of the graph
+    /// where the order of exploring vertices is determined by the frontier implementation.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    public static partial class EnumerableGenericSearch<TVertex, TEdge> { }
+}
