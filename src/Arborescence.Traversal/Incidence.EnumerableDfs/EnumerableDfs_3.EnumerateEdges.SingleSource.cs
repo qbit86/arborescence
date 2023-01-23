863ed272048e89183e261e173fbf99699680b74e
Viktor Ptitselov <qbit86@gmail.com>
2023-01-23 23:33:22 +0200

    [Traversal.Incidence] Implement EnumerableDfs<TVertex, TEdge, TEdgeEnumerator>.EnumerateEdges<>() for single source

--- src/Arborescence.Traversal/Incidence.EnumerableDfs/EnumerableDfs_3.EnumerateEdges.SingleSource.cs
+++ src/Arborescence.Traversal/Incidence.EnumerableDfs/EnumerableDfs_3.EnumerateEdges.SingleSource.cs
@@ -1,9 +1,99 @@
 namespace Arborescence.Traversal.Incidence
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public static partial class EnumerableDfs<TVertex, TEdge, TEdgeEnumerator>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<TEdge> EnumerateEdges<TGraph>(
+            TGraph graph, TVertex source)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator> =>
+            EnumerateEdgesChecked(graph, source);
+
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<TEdge> EnumerateEdges<TGraph>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator> =>
+            EnumerateEdgesChecked(graph, source, comparer);
+
+        /// <summary>
+        /// Enumerates edges of the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="exploredSet">The set of explored vertices.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TExploredSet">The type of the set of explored vertices.</typeparam>
+        /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="exploredSet"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<TEdge> EnumerateEdges<TGraph, TExploredSet>(
+            TGraph graph, TVertex source, TExploredSet exploredSet)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TExploredSet : ISet<TVertex> =>
+            EnumerateEdgesChecked(graph, source, exploredSet);
+
+        internal static IEnumerable<TEdge> EnumerateEdgesChecked<TGraph>(
+            TGraph graph, TVertex source)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            HashSet<TVertex> exploredSet = new();
+            return EnumerateEdgesIterator(graph, source, exploredSet);
+        }
+
+        internal static IEnumerable<TEdge> EnumerateEdgesChecked<TGraph>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            HashSet<TVertex> exploredSet = new(comparer);
+            return EnumerateEdgesIterator(graph, source, exploredSet);
+        }
+
+        internal static IEnumerable<TEdge> EnumerateEdgesChecked<TGraph, TExploredSet>(
+            TGraph graph, TVertex source, TExploredSet exploredSet)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TExploredSet : ISet<TVertex>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (exploredSet is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(exploredSet));
+
+            return EnumerateEdgesIterator(graph, source, exploredSet);
+        }
+
         private static IEnumerable<TEdge> EnumerateEdgesIterator<TGraph, TExploredSet>(
             TGraph graph, TVertex source, TExploredSet exploredSet)
             where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
