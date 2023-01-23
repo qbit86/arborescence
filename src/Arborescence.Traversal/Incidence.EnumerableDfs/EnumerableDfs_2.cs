451ce9f6fc9ad51bad0f831a55f7978bb8a508f2
Viktor Ptitselov <qbit86@gmail.com>
2023-01-23 23:53:40 +0200

    [Traversal.Incidence] Implement EnumerableDfs<TVertex, TEdge>.EnumerateVertices<>()

---
+++ src/Arborescence.Traversal/Incidence.EnumerableDfs/EnumerableDfs_2.cs
@@ -0,0 +1,9 @@
+namespace Arborescence.Traversal.Incidence
+{
+    /// <summary>
+    /// Represents the DFS algorithm — depth-first traversal of the graph — implemented as iterator.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    public static partial class EnumerableDfs<TVertex, TEdge> { }
+}
