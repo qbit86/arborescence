d958c049efbeab35b5227bf531214e85fad3119f
Viktor Ptitselov <qbit86@gmail.com>
2023-02-01 20:47:58 +0200

    [Traversal.Incidence] Add EagerBfs<TVertex, TEdge> partial classes

---
+++ src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_2.cs
@@ -0,0 +1,9 @@
+namespace Arborescence.Traversal.Incidence
+{
+    /// <summary>
+    /// Represents the BFS algorithm — breadth-first traversal of the graph.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    public static partial class EagerBfs<TVertex, TEdge> { }
+}
