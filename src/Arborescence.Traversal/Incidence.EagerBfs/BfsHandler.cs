4a8c1208f9b4de3ca98afdee0483d46114b0e9da
Viktor Ptitselov <qbit86@gmail.com>
2023-02-01 20:39:42 +0200

    [Traversal] Move IBfsHandler<> to Incidence.EagerBfs/ folder

--- src/Arborescence.Traversal/Incidence.EagerBfs/BfsHandler.cs
+++ src/Arborescence.Traversal/Incidence.EagerBfs/BfsHandler.cs
@@ -1,3 +1,4 @@
+// TODO: Move to Arborescence.Traversal.Incidence.
 namespace Arborescence.Traversal
 {
     /// <inheritdoc/>
--- src/Arborescence.Traversal/Incidence.EagerBfs/IBfsHandler.cs
+++ src/Arborescence.Traversal/Incidence.EagerBfs/IBfsHandler.cs
@@ -1,6 +1,7 @@
-﻿namespace Arborescence.Traversal
+﻿// TODO: Move to Arborescence.Traversal.Incidence.
+namespace Arborescence.Traversal
 {
-    // https://boost.org/doc/libs/1_76_0/libs/graph/doc/BFSVisitor.html
+    // https://www.boost.org/doc/libs/1_81_0/libs/graph/doc/BFSVisitor.html
 
     /// <summary>
     /// Defines callbacks to be invoked while traversing a graph in a BFS manner.
