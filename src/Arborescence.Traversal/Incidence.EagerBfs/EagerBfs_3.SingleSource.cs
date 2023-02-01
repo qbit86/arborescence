eaec86eb6a43007497398df18858cae28a3031b2
Viktor Ptitselov <qbit86@gmail.com>
2023-02-02 00:04:54 +0200

    [Traversal.Incidence] Add XML-doc comments

--- src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_3.SingleSource.cs
+++ src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_3.SingleSource.cs
@@ -5,6 +5,25 @@ namespace Arborescence.Traversal.Incidence
 
     public static partial class EagerBfs<TVertex, TEdge, TEdgeEnumerator>
     {
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="colorByVertex">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="colorByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Traverse<TGraph, TColorMap, THandler>(
             TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler)
