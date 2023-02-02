8a82edc19b3d6466b8ade616b56cfb4309837ec2
Viktor Ptitselov <qbit86@gmail.com>
2023-02-02 23:57:04 +0200

    [Traversal.Incidence] Add Traverse<TGraph, THandler>() overload taking comparer for single source

--- src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_2.SingleSource.cs
+++ src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_2.SingleSource.cs
@@ -10,6 +10,30 @@ namespace Arborescence.Traversal.Incidence
         /// </summary>
         /// <param name="graph">The graph.</param>
         /// <param name="source">The source.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, THandler>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer, THandler handler)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, IEnumerator<TEdge>>
+            where THandler : IBfsHandler<TGraph, TVertex, TEdge> =>
+            EagerBfs<TVertex, TEdge, IEnumerator<TEdge>>.TraverseChecked(graph, source, comparer, handler);
+
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
         /// <param name="colorByVertex">The vertex color map.</param>
         /// <param name="handler">
         /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
--- src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_3.SingleSource.cs
+++ src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_3.SingleSource.cs
@@ -10,6 +10,30 @@ namespace Arborescence.Traversal.Incidence
         /// </summary>
         /// <param name="graph">The graph.</param>
         /// <param name="source">The source.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, THandler>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer, THandler handler)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where THandler : IBfsHandler<TGraph, TVertex, TEdge> =>
+            TraverseChecked(graph, source, comparer, handler);
+
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
         /// <param name="colorByVertex">The vertex color map.</param>
         /// <param name="handler">
         /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
@@ -32,6 +56,24 @@ namespace Arborescence.Traversal.Incidence
             where THandler : IBfsHandler<TGraph, TVertex, TEdge> =>
             TraverseChecked(graph, source, colorByVertex, handler);
 
+        internal static void TraverseChecked<TGraph, THandler>(
+            TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer, THandler handler)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where THandler : IBfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (source is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(source));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            Dictionary<TVertex, Color> colorByVertex = new(comparer);
+            TraverseUnchecked(graph, source, colorByVertex, handler);
+        }
+
         internal static void TraverseChecked<TGraph, TColorMap, THandler>(
             TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler)
             where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
@@ -50,6 +92,15 @@ namespace Arborescence.Traversal.Incidence
             if (handler is null)
                 ThrowHelper.ThrowArgumentNullException(nameof(handler));
 
+            TraverseUnchecked(graph, source, colorByVertex, handler);
+        }
+
+        private static void TraverseUnchecked<TGraph, TColorMap, THandler>(
+            TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IBfsHandler<TGraph, TVertex, TEdge>
+        {
             colorByVertex[source] = Color.Gray;
             handler.OnDiscoverVertex(graph, source);
             var queue = new ValueQueue<TVertex>();
