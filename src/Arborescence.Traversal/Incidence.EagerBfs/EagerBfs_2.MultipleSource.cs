ccbb08581cc65b60c565a1c900c9a0e529ca347a
Viktor Ptitselov <qbit86@gmail.com>
2023-02-02 23:44:21 +0200

    [Traversal.Incidence] Add EagerBfs<TVertex, TEdge> "overload"

---
+++ src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_2.MultipleSource.cs
@@ -0,0 +1,37 @@
+namespace Arborescence.Traversal.Incidence
+{
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    public static partial class EagerBfs<TVertex, TEdge>
+    {
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the multiple sources.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="colorByVertex">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TSourceCollection">The type of the source collection.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="colorByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, TColorMap, THandler>(
+            TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, IEnumerator<TEdge>>
+            where TSourceCollection : IEnumerable<TVertex>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IBfsHandler<TGraph, TVertex, TEdge> =>
+            EagerBfs<TVertex, TEdge, IEnumerator<TEdge>>.TraverseChecked(graph, sources, colorByVertex, handler);
+    }
+}
---
+++ src/Arborescence.Traversal/Incidence.EagerBfs/EagerBfs_2.SingleSource.cs
@@ -0,0 +1,35 @@
+namespace Arborescence.Traversal.Incidence
+{
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    public static partial class EagerBfs<TVertex, TEdge>
+    {
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="colorByVertex">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="colorByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TColorMap, THandler>(
+            TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, IEnumerator<TEdge>>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IBfsHandler<TGraph, TVertex, TEdge> =>
+            EagerBfs<TVertex, TEdge, IEnumerator<TEdge>>.TraverseChecked(graph, source, colorByVertex, handler);
+    }
+}
