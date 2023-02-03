67f1f340c361a3936e4c9a5be43ca81f24eaff42
Viktor Ptitselov <qbit86@gmail.com>
2023-02-04 01:20:40 +0200

    [Traversal.Incidence] Add RecursiveDfs<TVertex, TEdge, TEdgeEnumerator>.Traverse<TGraph, TColorMap, THandler>() complete overload for multiple sources

--- src/Arborescence.Traversal/Incidence.RecursiveDfs/RecursiveDfs_3.MultipleSource.cs
+++ src/Arborescence.Traversal/Incidence.RecursiveDfs/RecursiveDfs_3.MultipleSource.cs
@@ -1,10 +1,65 @@
 namespace Arborescence.Traversal.Incidence
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
     using System.Threading;
 
     public static partial class RecursiveDfs<TVertex, TEdge, TEdgeEnumerator>
     {
+        /// <summary>
+        /// Traverses the graph in a depth-first order starting from the multiple sources
+        /// until the search tree is built or until the search is canceled.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources.</param>
+        /// <param name="colorByVertex">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="colorByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, TColorMap, THandler>(
+            TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge> =>
+            TraverseChecked(graph, sources, colorByVertex, handler, cancellationToken);
+
+        internal static void TraverseChecked<TGraph, TSourceCollection, TColorMap, THandler>(
+            TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (sources is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(sources));
+
+            if (colorByVertex is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(colorByVertex));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            TraverseUnchecked(graph, sources, colorByVertex, handler, cancellationToken);
+        }
+
         private static void TraverseUnchecked<TGraph, TSourceCollection, TColorMap, THandler>(
             TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
             CancellationToken cancellationToken)
