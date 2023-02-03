704552cb9421656376b96d8a3324bc7e2d4da933
Viktor Ptitselov <qbit86@gmail.com>
2023-02-03 23:50:57 +0200

    [Traversal.Incidence] Add RecursiveDfs<TVertex, TEdge, TEdgeEnumerator> boilerplate

---
+++ src/Arborescence.Traversal/Incidence.RecursiveDfs/RecursiveDfs_3.cs
@@ -0,0 +1,10 @@
+namespace Arborescence.Traversal.Incidence
+{
+    /// <summary>
+    /// Represents the DFS algorithm — depth-first traversal of the graph in a recursive manner using the program stack.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
+    public static partial class RecursiveDfs<TVertex, TEdge, TEdgeEnumerator> { }
+}
