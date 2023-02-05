ec657a5f4f2f30bb24a8857ba3da5fc5280baadd
Viktor Ptitselov <qbit86@gmail.com>
2023-02-05 10:40:52 +0200

    [Traversal.Incidence] Add EagerDfs<TVertex, TEdge, TEdgeEnumerator>.StackFrame

--- src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.cs
+++ src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.cs
@@ -2,7 +2,45 @@ namespace Arborescence.Traversal.Incidence
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Represents the DFS algorithm — depth-first traversal of the graph in a non-recursive manner.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
     public static partial class EagerDfs<TVertex, TEdge, TEdgeEnumerator>
         where TVertex : notnull
-        where TEdgeEnumerator : IEnumerator<TEdge> { }
+        where TEdgeEnumerator : IEnumerator<TEdge>
+    {
+        private readonly struct StackFrame
+        {
+            private readonly TEdge _edge;
+            private readonly bool _hasEdge;
+
+            internal StackFrame(TVertex vertex, TEdgeEnumerator edgeEnumerator)
+            {
+                _hasEdge = false;
+                _edge = default!;
+                Vertex = vertex;
+                EdgeEnumerator = edgeEnumerator;
+            }
+
+            internal StackFrame(TVertex vertex, TEdge edge, TEdgeEnumerator edgeEnumerator)
+            {
+                _hasEdge = true;
+                _edge = edge;
+                Vertex = vertex;
+                EdgeEnumerator = edgeEnumerator;
+            }
+
+            internal TVertex Vertex { get; }
+            internal TEdgeEnumerator EdgeEnumerator { get; }
+
+            internal bool TryGetEdge(out TEdge edge)
+            {
+                edge = _edge;
+                return _hasEdge;
+            }
+        }
+    }
 }
