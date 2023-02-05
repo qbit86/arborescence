675e83cf5a205c165f6a308b0c8f9c3f53204db8
Viktor Ptitselov <qbit86@gmail.com>
2023-02-05 15:27:45 +0200

    [Traversal.Incidence] Add EagerDfs<TVertex, TEdge, TEdgeEnumerator>.Traverse<TGraph, TColorMap, THandler>() for single source

--- src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.SingleSource.cs
+++ src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.SingleSource.cs
@@ -1,4 +1,71 @@
 namespace Arborescence.Traversal.Incidence
 {
-    public static partial class EagerDfs<TVertex, TEdge, TEdgeEnumerator> { }
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+
+    public static partial class EagerDfs<TVertex, TEdge, TEdgeEnumerator>
+    {
+        /// <summary>
+        /// Traverses the graph in a depth-first order starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="colorByVertex">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="colorByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TColorMap, THandler>(
+            TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge> =>
+            TraverseChecked(graph, source, colorByVertex, handler, cancellationToken);
+
+        internal static void TraverseChecked<TGraph, TColorMap, THandler>(
+            TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (source is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(source));
+
+            if (colorByVertex is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(colorByVertex));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            TraverseUnchecked(graph, source, colorByVertex, handler, cancellationToken);
+        }
+
+        private static void TraverseUnchecked<TGraph, TColorMap, THandler>(
+            TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            handler.OnStartVertex(graph, source);
+            TraverseCore(graph, source, colorByVertex, handler, cancellationToken);
+        }
+    }
 }
