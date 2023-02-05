476e018b64b1dcdeb4f5bb320050431522483e08
Viktor Ptitselov <qbit86@gmail.com>
2023-02-05 16:16:44 +0200

    [Traversal.Incidence] Add EagerDfs<TVertex, TEdge, TEdgeEnumerator>.Traverse<TGraph, TSourceCollection, THandler>() overloads for multiple sources

--- src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.MultipleSource.cs
+++ src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.MultipleSource.cs
@@ -12,6 +12,63 @@ namespace Arborescence.Traversal.Incidence
         /// </summary>
         /// <param name="graph">The graph.</param>
         /// <param name="sources">The sources.</param>
+        /// <param name="handler">
+        /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TSourceCollection">The type of the source collection.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge> =>
+            TraverseChecked(graph, sources, handler, cancellationToken);
+
+        /// <summary>
+        /// Traverses the graph in a depth-first order starting from the multiple sources
+        /// until the search tree is built or until the search is canceled.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <param name="handler">
+        /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TSourceCollection">The type of the source collection.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="sources"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, IEqualityComparer<TVertex> comparer, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge> =>
+            TraverseChecked(graph, sources, comparer, handler, cancellationToken);
+
+        /// <summary>
+        /// Traverses the graph in a depth-first order starting from the multiple sources
+        /// until the search tree is built or until the search is canceled.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="sources">The sources.</param>
         /// <param name="colorByVertex">The vertex color map.</param>
         /// <param name="handler">
         /// The <see cref="IDfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
@@ -37,6 +94,46 @@ namespace Arborescence.Traversal.Incidence
             where TColorMap : IDictionary<TVertex, Color>
             where THandler : IDfsHandler<TGraph, TVertex, TEdge> =>
             TraverseChecked(graph, sources, colorByVertex, handler, cancellationToken);
+
+        internal static void TraverseChecked<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (sources is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(sources));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            Dictionary<TVertex, Color> colorByVertex = new();
+            TraverseUnchecked(graph, sources, colorByVertex, handler, cancellationToken);
+        }
+
+        internal static void TraverseChecked<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, IEqualityComparer<TVertex> comparer, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (sources is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(sources));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            Dictionary<TVertex, Color> colorByVertex = new(comparer);
+            TraverseUnchecked(graph, sources, colorByVertex, handler, cancellationToken);
+        }
 
         internal static void TraverseChecked<TGraph, TSourceCollection, TColorMap, THandler>(
             TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
