8096ee940b0d0e84b4b7fd13a28afc103415721b
Viktor Ptitselov <qbit86@gmail.com>
2023-02-05 11:39:57 +0200

    [Traversal.Incidence] Add EagerDfs<TVertex, TEdge, TEdgeEnumerator>.TraverseCore<TGraph, TColorMap, THandler>()

--- src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.cs
+++ src/Arborescence.Traversal/Incidence.EagerDfs/EagerDfs_3.cs
@@ -1,6 +1,8 @@
 namespace Arborescence.Traversal.Incidence
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
 
     /// <summary>
     /// Represents the DFS algorithm — depth-first traversal of the graph in a non-recursive manner.
@@ -12,6 +14,95 @@ namespace Arborescence.Traversal.Incidence
         where TVertex : notnull
         where TEdgeEnumerator : IEnumerator<TEdge>
     {
+        private static void TraverseCore<TGraph, TColorMap, THandler>(
+            TGraph graph, TVertex vertex, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IHeadIncidence<TVertex, TEdge>, IOutEdgesIncidence<TVertex, TEdgeEnumerator>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IDfsHandler<TGraph, TVertex, TEdge>
+        {
+            colorByVertex[vertex] = Color.Gray;
+            handler.OnDiscoverVertex(graph, vertex);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                colorByVertex[vertex] = Color.Black;
+                handler.OnFinishVertex(graph, vertex);
+                return;
+            }
+
+            var stack = new ValueStack<StackFrame>();
+            try
+            {
+                TEdgeEnumerator outEdges = graph.EnumerateOutEdges(vertex);
+                stack.Add(new(vertex, outEdges));
+
+                while (stack.TryTake(out StackFrame stackFrame))
+                {
+                    vertex = stackFrame.Vertex;
+                    if (stackFrame.TryGetEdge(out TEdge inEdge))
+                        handler.OnFinishEdge(graph, inEdge);
+
+                    TEdgeEnumerator edges = stackFrame.EdgeEnumerator;
+                    while (true)
+                    {
+                        if (!edges.MoveNext())
+                        {
+                            edges.Dispose();
+                            break;
+                        }
+
+                        TEdge edge = edges.Current;
+                        if (!graph.TryGetHead(edge, out TVertex? neighbor))
+                            continue;
+
+                        handler.OnExamineEdge(graph, edge);
+                        Color color = GetColorOrDefault(colorByVertex, neighbor);
+                        if (color is Color.None or Color.White)
+                        {
+                            handler.OnTreeEdge(graph, edge);
+                            stack.Add(new(vertex, edge, edges));
+                            vertex = neighbor;
+                            colorByVertex[vertex] = Color.Gray;
+                            handler.OnDiscoverVertex(graph, vertex);
+                            edges = graph.EnumerateOutEdges(vertex);
+                            if (cancellationToken.IsCancellationRequested)
+                            {
+                                colorByVertex[vertex] = Color.Black;
+                                handler.OnFinishVertex(graph, vertex);
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            if (color is Color.Gray)
+                                handler.OnBackEdge(graph, edge);
+                            else
+                                handler.OnForwardOrCrossEdge(graph, edge);
+                            handler.OnFinishEdge(graph, edge);
+                        }
+                    }
+
+                    colorByVertex[vertex] = Color.Black;
+                    handler.OnFinishVertex(graph, vertex);
+                }
+            }
+            catch
+            {
+                while (stack.TryTake(out StackFrame stackFrame))
+                    stackFrame.EdgeEnumerator.Dispose();
+                throw;
+            }
+            finally
+            {
+                stack.Dispose();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Color GetColorOrDefault<TColorMap>(TColorMap colorByVertex, TVertex vertex)
+            where TColorMap : IDictionary<TVertex, Color> =>
+            colorByVertex.TryGetValue(vertex, out Color result) ? result : Color.None;
+
         private readonly struct StackFrame
         {
             private readonly TEdge _edge;
