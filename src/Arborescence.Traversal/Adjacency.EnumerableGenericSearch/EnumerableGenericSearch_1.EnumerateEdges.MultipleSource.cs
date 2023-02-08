d3bf9f1c71030d7c0eefdf7a2e5cab2837d0990b
Viktor Ptitselov <qbit86@gmail.com>
2023-02-08 17:33:38 +0200

    [Traversal.Adjacency] Refactor sources in EnumerableGenericSearch<> and EnumerableBfs<> for edges

--- src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_1.EnumerateEdges.MultipleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EnumerableGenericSearch/EnumerableGenericSearch_1.EnumerateEdges.MultipleSource.cs
@@ -10,10 +10,10 @@ namespace Arborescence.Traversal.Adjacency
         /// Enumerates edges of the graph in an order specified by the frontier starting from the multiple sources.
         /// </summary>
         /// <param name="graph">The graph.</param>
-        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="sources">The sources.</param>
         /// <param name="frontier">The collection of discovered vertices which are not finished yet.</param>
         /// <typeparam name="TGraph">The type of the graph.</typeparam>
-        /// <typeparam name="TSourceEnumerator">The type of the source enumerator.</typeparam>
+        /// <typeparam name="TSourceCollection">The type of the source collection.</typeparam>
         /// <typeparam name="TFrontier">The type of the generic queue.</typeparam>
         /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
         /// <exception cref="System.ArgumentNullException">
@@ -27,10 +27,10 @@ namespace Arborescence.Traversal.Adjacency
         /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Endpoints<TVertex>> EnumerateEdges<
-            TGraph, TSourceEnumerator, TFrontier>(
-            TGraph graph, TSourceEnumerator sources, TFrontier frontier)
+            TGraph, TSourceCollection, TFrontier>(
+            TGraph graph, TSourceCollection sources, TFrontier frontier)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
-            where TSourceEnumerator : IEnumerator<TVertex>
+            where TSourceCollection : IEnumerable<TVertex>
             where TFrontier : IProducerConsumerCollection<TVertex> =>
             EnumerableGenericSearch<TVertex, IEnumerator<TVertex>>.EnumerateEdgesChecked(graph, sources, frontier);
 
@@ -38,11 +38,11 @@ namespace Arborescence.Traversal.Adjacency
         /// Enumerates edges of the graph in an order specified by the frontier starting from the multiple sources.
         /// </summary>
         /// <param name="graph">The graph.</param>
-        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="sources">The sources.</param>
         /// <param name="frontier">The collection of discovered vertices which are not finished yet.</param>
         /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
         /// <typeparam name="TGraph">The type of the graph.</typeparam>
-        /// <typeparam name="TSourceEnumerator">The type of the source enumerator.</typeparam>
+        /// <typeparam name="TSourceCollection">The type of the source collection.</typeparam>
         /// <typeparam name="TFrontier">The type of the generic queue.</typeparam>
         /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
         /// <exception cref="System.ArgumentNullException">
@@ -56,10 +56,10 @@ namespace Arborescence.Traversal.Adjacency
         /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Endpoints<TVertex>> EnumerateEdges<
-            TGraph, TSourceEnumerator, TFrontier>(
-            TGraph graph, TSourceEnumerator sources, TFrontier frontier, IEqualityComparer<TVertex> comparer)
+            TGraph, TSourceCollection, TFrontier>(
+            TGraph graph, TSourceCollection sources, TFrontier frontier, IEqualityComparer<TVertex> comparer)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
-            where TSourceEnumerator : IEnumerator<TVertex>
+            where TSourceCollection : IEnumerable<TVertex>
             where TFrontier : IProducerConsumerCollection<TVertex> =>
             EnumerableGenericSearch<TVertex, IEnumerator<TVertex>>.EnumerateEdgesChecked(
                 graph, sources, frontier, comparer);
@@ -68,11 +68,11 @@ namespace Arborescence.Traversal.Adjacency
         /// Enumerates edges of the graph in an order specified by the frontier starting from the multiple sources.
         /// </summary>
         /// <param name="graph">The graph.</param>
-        /// <param name="sources">The sources enumerator.</param>
+        /// <param name="sources">The sources.</param>
         /// <param name="frontier">The collection of discovered vertices which are not finished yet.</param>
         /// <param name="exploredSet">The set of explored vertices.</param>
         /// <typeparam name="TGraph">The type of the graph.</typeparam>
-        /// <typeparam name="TSourceEnumerator">The type of the source enumerator.</typeparam>
+        /// <typeparam name="TSourceCollection">The type of the source collection.</typeparam>
         /// <typeparam name="TFrontier">The type of the generic queue.</typeparam>
         /// <typeparam name="TExploredSet">The type of the set of explored vertices.</typeparam>
         /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
@@ -88,10 +88,10 @@ namespace Arborescence.Traversal.Adjacency
         /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Endpoints<TVertex>> EnumerateEdges<
-            TGraph, TSourceEnumerator, TFrontier, TExploredSet>(
-            TGraph graph, TSourceEnumerator sources, TFrontier frontier, TExploredSet exploredSet)
+            TGraph, TSourceCollection, TFrontier, TExploredSet>(
+            TGraph graph, TSourceCollection sources, TFrontier frontier, TExploredSet exploredSet)
             where TGraph : IAdjacency<TVertex, IEnumerator<TVertex>>
-            where TSourceEnumerator : IEnumerator<TVertex>
+            where TSourceCollection : IEnumerable<TVertex>
             where TFrontier : IProducerConsumerCollection<TVertex>
             where TExploredSet : ISet<TVertex> =>
             EnumerableGenericSearch<TVertex, IEnumerator<TVertex>>.EnumerateEdgesChecked(
