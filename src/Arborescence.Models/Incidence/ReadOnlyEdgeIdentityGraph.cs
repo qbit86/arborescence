f28a2f224ee091186c10fabad2886732dfe886c2
Viktor Ptitselov <qbit86@gmail.com>
2023-02-12 23:34:32 +0200

    [Models] Add ReadOnlyEdgeIdentityGraph<>

---
+++ src/Arborescence.Models/Incidence/ReadOnlyEdgeIdentityGraph.cs
@@ -0,0 +1,42 @@
+namespace Arborescence.Models.Incidence
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    public readonly struct ReadOnlyEdgeIdentityGraph<
+        TVertex, TEdge, TEndpointMap, TEdgesMap, TEdgeCollection, TEdgeEnumerator, TEdgeCollectionPolicy> :
+        ITailIncidence<TVertex, TEdge>,
+        IHeadIncidence<TVertex, TEdge>,
+        IOutEdgesIncidence<TVertex, TEdgeEnumerator>,
+        IAdjacency<TVertex, IEnumerator<TVertex>>
+        where TEndpointMap : IReadOnlyDictionary<TEdge, TVertex>
+        where TEdgesMap : IReadOnlyDictionary<TVertex, TEdgeCollection>
+        where TEdgeEnumerator : IEnumerator<TEdge>
+        where TEdgeCollectionPolicy : IEnumerablePolicy<TEdgeCollection, TEdgeEnumerator>
+    {
+        private readonly TEndpointMap _tailByEdge;
+        private readonly TEndpointMap _headByEdge;
+        private readonly TEdgesMap _outEdgesByVertex;
+        private readonly TEdgeCollectionPolicy _edgeCollectionPolicy;
+
+        public bool TryGetTail(TEdge edge, [MaybeNullWhen(false)] out TVertex tail) =>
+            _tailByEdge.TryGetValue(edge, out tail);
+
+        public bool TryGetHead(TEdge edge, [MaybeNullWhen(false)] out TVertex head) =>
+            _headByEdge.TryGetValue(edge, out head);
+
+        public TEdgeEnumerator EnumerateOutEdges(TVertex vertex) =>
+            EdgesMapHelpers<TEdgeCollection, TEdgeEnumerator>.EnumerateOutEdges(
+                _outEdgesByVertex, vertex, _edgeCollectionPolicy);
+
+        public IEnumerator<TVertex> EnumerateNeighbors(TVertex vertex)
+        {
+            TEdgeEnumerator edgeEnumerator = EnumerateOutEdges(vertex);
+            while (edgeEnumerator.MoveNext())
+            {
+                if (TryGetHead(edgeEnumerator.Current, out TVertex? neighbor))
+                    yield return neighbor;
+            }
+        }
+    }
+}
