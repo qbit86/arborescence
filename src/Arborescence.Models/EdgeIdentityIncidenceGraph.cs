34842c4c1915228f7607e00fbe275f9b4bfdc02f
Viktor Ptitselov <qbit86@gmail.com>
2023-02-12 03:00:17 +0200

    [Models] Refine EdgeIdentityIncidenceGraph<>

--- src/Arborescence.Models/EdgeIdentityIncidenceGraph.cs
+++ src/Arborescence.Models/EdgeIdentityIncidenceGraph.cs
@@ -1,19 +1,60 @@
 namespace Arborescence.Models
 {
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
 
-    public readonly struct EdgeIdentityIncidenceGraph<TVertex, TEdge, TEdgeEnumerator> :
+    public readonly struct EdgeIdentityIncidenceGraph<TVertex, TEdge, TEndpointMap, TEdgesMap> :
         ITailIncidence<TVertex, TEdge>,
         IHeadIncidence<TVertex, TEdge>,
-        IOutEdgesIncidence<TVertex, TEdgeEnumerator>,
+        IOutEdgesIncidence<TVertex, List<TEdge>.Enumerator>,
         IAdjacency<TVertex, IEnumerator<TVertex>>
+        where TEndpointMap : IReadOnlyDictionary<TEdge, TVertex>
+        where TEdgesMap : IReadOnlyDictionary<TVertex, List<TEdge>>
     {
-        public bool TryGetTail(TEdge edge, out TVertex tail) => throw new System.NotImplementedException();
+        private static readonly List<TEdge> s_empty = new();
+        private readonly TEndpointMap _tailByEdge;
+        private readonly TEndpointMap _headByEdge;
+        private readonly TEdgesMap _outEdgesByVertex;
 
-        public bool TryGetHead(TEdge edge, out TVertex head) => throw new System.NotImplementedException();
+        private EdgeIdentityIncidenceGraph(TEndpointMap tailByEdge, TEndpointMap headByEdge, TEdgesMap outEdgesByVertex)
+        {
+            _tailByEdge = tailByEdge;
+            _headByEdge = headByEdge;
+            _outEdgesByVertex = outEdgesByVertex;
+        }
 
-        public TEdgeEnumerator EnumerateOutEdges(TVertex vertex) => throw new System.NotImplementedException();
+        public static EdgeIdentityIncidenceGraph<TVertex, TEdge, TEndpointMap, TEdgesMap> CreateUnchecked(
+            TEndpointMap tailByEdge, TEndpointMap headByEdge, TEdgesMap outEdgesByVertex)
+        {
+            if (tailByEdge is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(tailByEdge));
+            if (headByEdge is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(headByEdge));
+            if (outEdgesByVertex is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(outEdgesByVertex));
 
-        public IEnumerator<TVertex> EnumerateNeighbors(TVertex vertex) => throw new System.NotImplementedException();
+            return new(tailByEdge, headByEdge, outEdgesByVertex);
+        }
+
+        public bool TryGetTail(TEdge edge, [MaybeNullWhen(false)] out TVertex tail) =>
+            _tailByEdge.TryGetValue(edge, out tail);
+
+        public bool TryGetHead(TEdge edge, [MaybeNullWhen(false)] out TVertex head) =>
+            _headByEdge.TryGetValue(edge, out head);
+
+        public List<TEdge>.Enumerator EnumerateOutEdges(TVertex vertex) =>
+            _outEdgesByVertex.TryGetValue(vertex, out List<TEdge>? outEdges)
+                ? outEdges.GetEnumerator()
+                : s_empty.GetEnumerator();
+
+        public IEnumerator<TVertex> EnumerateNeighbors(TVertex vertex)
+        {
+            List<TEdge>.Enumerator edgeEnumerator = EnumerateOutEdges(vertex);
+            while (edgeEnumerator.MoveNext())
+            {
+                if (TryGetHead(edgeEnumerator.Current, out TVertex? neighbor))
+                    yield return neighbor;
+            }
+        }
     }
 }
