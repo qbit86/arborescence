29a1fc7e07c1c4b5c39f7a5135ed22b7844d9d26
Viktor Ptitselov <qbit86@gmail.com>
2023-02-13 22:24:27 +0200

    [Models] Add IncidenceEnumerator<TVertex, TNeighborEnumerator>

---
+++ src/Arborescence.Models/IncidenceEnumerator.cs
@@ -0,0 +1,49 @@
+namespace Arborescence
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Models;
+
+    public static class IncidenceEnumerator
+    {
+        public static IncidenceEnumerator<TVertex, TNeighborEnumerator> Create<TVertex, TNeighborEnumerator>(
+            TVertex vertex, TNeighborEnumerator neighborEnumerator)
+            where TNeighborEnumerator : IEnumerator<TVertex>
+        {
+            if (neighborEnumerator is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(neighborEnumerator));
+
+            return new(vertex, neighborEnumerator);
+        }
+    }
+
+    public struct IncidenceEnumerator<TVertex, TNeighborEnumerator> :
+        IEnumerable<Endpoints<TVertex>>, IEnumerator<Endpoints<TVertex>>
+        where TNeighborEnumerator : IEnumerator<TVertex>
+    {
+        private TNeighborEnumerator _neighborEnumerator;
+        private readonly TVertex _vertex;
+
+        internal IncidenceEnumerator(TVertex vertex, TNeighborEnumerator neighborEnumerator)
+        {
+            _vertex = vertex;
+            _neighborEnumerator = neighborEnumerator;
+        }
+
+        Endpoints<TVertex> IEnumerator<Endpoints<TVertex>>.Current => new(_vertex, _neighborEnumerator.Current);
+
+        public object Current => Endpoints.Create(_vertex, _neighborEnumerator.Current);
+
+        public void Dispose() => _neighborEnumerator.Dispose();
+
+        public bool MoveNext() => _neighborEnumerator.MoveNext();
+
+        public void Reset() => _neighborEnumerator.Reset();
+
+        public IncidenceEnumerator<TVertex, TNeighborEnumerator> GetEnumerator() => this;
+
+        IEnumerator<Endpoints<TVertex>> IEnumerable<Endpoints<TVertex>>.GetEnumerator() => this;
+
+        IEnumerator IEnumerable.GetEnumerator() => this;
+    }
+}
