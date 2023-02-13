d9f4a086478224593d241b932b55fa3d0af6aa9b
Viktor Ptitselov <qbit86@gmail.com>
2023-02-13 03:45:12 +0200

    [Models] Add ReadOnlyAdjacencyGraphFactory<>

---
+++ src/Arborescence.Models/Adjacency/ReadOnlyAdjacencyGraphFactory.cs
@@ -0,0 +1,23 @@
+namespace Arborescence.Models.Adjacency
+{
+    using System.Collections.Generic;
+
+    public static class ReadOnlyAdjacencyGraphFactory<TVertex, TVertexCollection, TVertexEnumerator>
+        where TVertexEnumerator : IEnumerator<TVertex>
+    {
+        public static ReadOnlyAdjacencyGraph<
+                TVertex, TVerticesMap, TVertexCollection, TVertexEnumerator, TVertexCollectionPolicy>
+            CreateUnchecked<TVerticesMap, TVertexCollectionPolicy>(
+                TVerticesMap neighborsByVertex, TVertexCollectionPolicy vertexCollectionPolicy)
+            where TVerticesMap : IReadOnlyDictionary<TVertex, TVertexCollection>
+            where TVertexCollectionPolicy : IEnumerablePolicy<TVertexCollection, TVertexEnumerator>
+        {
+            if (neighborsByVertex is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(neighborsByVertex));
+            if (vertexCollectionPolicy is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(vertexCollectionPolicy));
+
+            return new(neighborsByVertex, vertexCollectionPolicy);
+        }
+    }
+}
