fd7faddf43d0d73e51cd082cae444fb46985974f
Viktor Ptitselov <qbit86@gmail.com>
2023-02-13 02:58:47 +0200

    [Models] Add ReadOnlyEdgeEndpointsGraphFactory<TVertex, TEdgeCollection, TEdgeEnumerator>

---
+++ src/Arborescence.Models/Incidence/ReadOnlyEdgeEndpointsGraphFactory.cs
@@ -0,0 +1,23 @@
+namespace Arborescence.Models.Incidence
+{
+    using System.Collections.Generic;
+
+    public static class ReadOnlyEdgeEndpointsGraphFactory<TVertex, TEdgeCollection, TEdgeEnumerator>
+        where TEdgeEnumerator : IEnumerator<Endpoints<TVertex>>
+    {
+        public static ReadOnlyEdgeEndpointsGraph<
+                TVertex, TEdgesMap, TEdgeCollection, TEdgeEnumerator, TEdgeCollectionPolicy>
+            CreateUnchecked<TEdgesMap, TEdgeCollectionPolicy>(
+                TEdgesMap outEdgesByVertex, TEdgeCollectionPolicy edgeCollectionPolicy)
+            where TEdgesMap : IReadOnlyDictionary<TVertex, TEdgeCollection>
+            where TEdgeCollectionPolicy : IEnumerablePolicy<TEdgeCollection, TEdgeEnumerator>
+        {
+            if (outEdgesByVertex is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(outEdgesByVertex));
+            if (edgeCollectionPolicy is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(edgeCollectionPolicy));
+
+            return new(outEdgesByVertex, edgeCollectionPolicy);
+        }
+    }
+}
