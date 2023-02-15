a6d668fe406a311026303f304e9fae3d02f990c8
Viktor Ptitselov <qbit86@gmail.com>
2023-02-15 23:39:25 +0200

    [Models] Add ReadOnlyDictionaryMultimapPolicy<> factory

--- src/Arborescence.Models/Policies/ReadOnlyDictionaryMultimapPolicy.cs
+++ src/Arborescence.Models/Policies/ReadOnlyDictionaryMultimapPolicy.cs
@@ -2,6 +2,21 @@ namespace Arborescence.Models
 {
     using System.Collections.Generic;
 
+    public static class ReadOnlyDictionaryMultimapPolicy<TKey, TValueCollection, TValueEnumerator, TMultimap>
+        where TMultimap : IReadOnlyDictionary<TKey, TValueCollection>
+    {
+        public static ReadOnlyDictionaryMultimapPolicy<
+            TKey, TValueCollection, TValueEnumerator, TMultimap, TCollectionPolicy> Create<TCollectionPolicy>(
+            TCollectionPolicy collectionPolicy)
+            where TCollectionPolicy : IEnumerablePolicy<TValueCollection, TValueEnumerator>
+        {
+            if (collectionPolicy is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(collectionPolicy));
+
+            return new(collectionPolicy);
+        }
+    }
+
     public readonly struct ReadOnlyDictionaryMultimapPolicy<
         TKey, TValueCollection, TValueEnumerator, TMultimap, TCollectionPolicy> :
         IMultimapPolicy<TKey, TMultimap, TValueEnumerator>
