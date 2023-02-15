109514ca7e27603ea57d6b50189b9d0f679a39ff
Viktor Ptitselov <qbit86@gmail.com>
2023-02-16 01:04:13 +0200

    [Models] Add ListDictionaryMultimapPolicy<TKey, TValue, TMultimap>

--- src/Arborescence.Models/Policies/ListDictionaryMultimapPolicy.cs
+++ src/Arborescence.Models/Policies/ListDictionaryMultimapPolicy.cs
@@ -13,4 +13,16 @@ namespace Arborescence.Models
                 ? s_collectionPolicy.GetEnumerator(values)
                 : s_collectionPolicy.GetEmptyEnumerator();
     }
+
+    public readonly struct ListDictionaryMultimapPolicy<TKey, TValue, TMultimap> :
+        IMultimapPolicy<TKey, TMultimap, List<TValue>.Enumerator>
+        where TMultimap : IReadOnlyDictionary<TKey, List<TValue>>
+    {
+        private static readonly ListEnumerablePolicy<TValue> s_collectionPolicy = default;
+
+        public List<TValue>.Enumerator GetEnumerator(TMultimap multimap, TKey key) =>
+            multimap.TryGetValue(key, out List<TValue>? values)
+                ? s_collectionPolicy.GetEnumerator(values)
+                : s_collectionPolicy.GetEmptyEnumerator();
+    }
 }
