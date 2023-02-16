74baaa95f55c7d06884b132c63b96bc48e2e9502
Viktor Ptitselov <qbit86@gmail.com>
2023-02-17 01:21:31 +0200

    [Models.Specialized] Implement Int32AdjacencyMultimapPolicy.GetEnumerator()

--- src/Arborescence.Models.Specialized/Adjacency/Int32AdjacencyMultimapPolicy.cs
+++ src/Arborescence.Models.Specialized/Adjacency/Int32AdjacencyMultimapPolicy.cs
@@ -2,6 +2,7 @@
 namespace Arborescence.Models.Specialized
 {
     using System;
+    using System.Diagnostics;
 
     internal readonly struct Int32AdjacencyMultimapPolicy :
         IMultimapPolicy<int, int[], ArraySegment<int>.Enumerator>
@@ -11,7 +12,20 @@ namespace Arborescence.Models.Specialized
             if (multimap is null)
                 return ArraySegment<int>.Empty.GetEnumerator();
 
-            throw new NotImplementedException();
+            Debug.Assert(multimap.Length >= 2, "multimap.Length >= 2");
+            int vertexCount = multimap[0];
+            Debug.Assert(vertexCount >= 0, "vertexCount >= 0");
+            if (unchecked((uint)key >= (uint)vertexCount))
+                return ArraySegment<int>.Empty.GetEnumerator();
+
+            int edgeCount = multimap[1];
+            Debug.Assert(edgeCount >= 0, "edgeCount >= 0");
+            ReadOnlySpan<int> upperBoundByVertex = multimap.AsSpan(2, vertexCount);
+            int upperBound = upperBoundByVertex[key];
+            int lowerBound = key == 0 ? 0 : upperBoundByVertex[key - 1];
+            int offset = 2 + vertexCount;
+            ArraySegment<int> segment = new(multimap, offset, upperBound - lowerBound);
+            return segment.GetEnumerator();
         }
     }
 }
