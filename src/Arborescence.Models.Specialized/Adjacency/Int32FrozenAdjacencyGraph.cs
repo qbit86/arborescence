d46a86fa341d502eee6bdd88cc324ebb1e6c671c
Viktor Ptitselov <qbit86@gmail.com>
2023-02-17 01:32:07 +0200

    [Models.Specialized] Refactor Int32FrozenAdjacencyGraph declaration

--- src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraph.cs
+++ src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraph.cs
@@ -2,40 +2,27 @@
 namespace Arborescence.Models.Specialized
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
+    using Edge = Endpoints<int>;
     using VertexEnumerator = System.ArraySegment<int>.Enumerator;
-
-    internal struct Int32IncidenceEnumerator : IEnumerator<Int32Endpoints>
-    {
-        public bool MoveNext() => throw new NotImplementedException();
-
-        public void Reset() => throw new NotImplementedException();
-
-        public Int32Endpoints Current => throw new NotImplementedException();
-
-        object IEnumerator.Current => Current;
-
-        public void Dispose() => throw new NotImplementedException();
-    }
+    using EdgeEnumerator = IncidenceEnumerator<int, System.ArraySegment<int>.Enumerator>;
 
     internal readonly struct Int32FrozenAdjacencyGraph :
-        IHeadIncidence<int, Int32Endpoints>,
-        ITailIncidence<int, Int32Endpoints>,
-        IOutEdgesIncidence<int, Int32IncidenceEnumerator>,
+        IHeadIncidence<int, Edge>,
+        ITailIncidence<int, Edge>,
+        IOutEdgesIncidence<int, EdgeEnumerator>,
         IAdjacency<int, VertexEnumerator>,
         IEquatable<Int32FrozenAdjacencyGraph>
     {
-        public bool TryGetHead(Int32Endpoints edge, out int head) => throw new NotImplementedException();
+        public bool TryGetHead(Edge edge, out int head) => throw new NotImplementedException();
 
-        public bool TryGetTail(Int32Endpoints edge, out int tail) => throw new NotImplementedException();
+        public bool TryGetTail(Edge edge, out int tail) => throw new NotImplementedException();
 
-        public Int32IncidenceEnumerator EnumerateOutEdges(int vertex) => throw new NotImplementedException();
-
-        public bool Equals(Int32FrozenAdjacencyGraph other) => throw new NotImplementedException();
+        public EdgeEnumerator EnumerateOutEdges(int vertex) => throw new NotImplementedException();
 
         public VertexEnumerator EnumerateOutNeighbors(int vertex) => throw new NotImplementedException();
+
+        public bool Equals(Int32FrozenAdjacencyGraph other) => throw new NotImplementedException();
 
         public override bool Equals([NotNullWhen(true)] object? obj) =>
             obj is Int32FrozenAdjacencyGraph other && Equals(other);
