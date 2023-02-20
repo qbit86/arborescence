3037f67b18664071a00f28acd20c400975d5233e
Viktor Ptitselov <qbit86@gmail.com>
2023-02-20 02:30:38 +0200

    [Models.Specialized] Add FromList<TMultimap>() to Int32FrozenAdjacencyGraphFactory<TVertexCollection>()

--- src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
+++ src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
@@ -7,12 +7,25 @@ namespace Arborescence.Models.Specialized
     public static class Int32FrozenAdjacencyGraphFactory<TVertexCollection>
         where TVertexCollection : ICollection<int>
     {
-        public static Int32FrozenAdjacencyGraph Create<TMultimap>(TMultimap neighborsByVertex)
-            where TMultimap : IReadOnlyDictionary<int, TVertexCollection>
+        public static Int32FrozenAdjacencyGraph FromList<TMultimap>(TMultimap neighborsByVertex)
+            where TMultimap : IReadOnlyList<TVertexCollection>
         {
             if (neighborsByVertex is null)
                 return default;
 
+            Int32ReadOnlyDictionary<TVertexCollection, TMultimap> dictionary =
+                Int32ReadOnlyDictionaryFactory<TVertexCollection>.Create(neighborsByVertex);
+
+            return FromDictionaryUnchecked(dictionary);
+        }
+
+        public static Int32FrozenAdjacencyGraph FromDictionary<TMultimap>(TMultimap neighborsByVertex)
+            where TMultimap : IReadOnlyDictionary<int, TVertexCollection> =>
+            neighborsByVertex is null ? default : FromDictionaryUnchecked(neighborsByVertex);
+
+        private static Int32FrozenAdjacencyGraph FromDictionaryUnchecked<TMultimap>(TMultimap neighborsByVertex)
+            where TMultimap : IReadOnlyDictionary<int, TVertexCollection>
+        {
             int vertexCount = neighborsByVertex.Count;
             ArrayPrefix<int> data = new();
             data = ArrayPrefixBuilder.Add(data, vertexCount, false);
@@ -22,8 +35,11 @@ namespace Arborescence.Models.Specialized
             int offset = 2 + vertexCount;
             for (int key = 0; key < vertexCount; ++key)
             {
-                if (!neighborsByVertex.TryGetValue(key, out TVertexCollection? neighbors))
+                if (!neighborsByVertex.TryGetValue(key, out TVertexCollection? neighbors) || neighbors is null)
+                {
+                    upperBoundByVertex[key] = offset;
                     continue;
+                }
 
                 int neighborCount = neighbors.Count;
                 if (neighborCount is 0)
