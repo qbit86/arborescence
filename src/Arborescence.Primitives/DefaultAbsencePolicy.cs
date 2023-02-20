da90b31ba8aefb193d62d0350ef79306bd15de2d
Viktor Ptitselov <qbit86@gmail.com>
2023-02-20 17:20:02 +0200

    [Primitives] Remove NullAbsencePolicy

--- src/Arborescence.Primitives/DefaultAbsencePolicy.cs
+++ src/Arborescence.Primitives/DefaultAbsencePolicy.cs
@@ -3,8 +3,8 @@ namespace Arborescence
     using System;
     using System.Collections.Generic;
 
-    public readonly struct DefaultAbsencePolicy<T> : IEquatable<T> where T : struct
+    public readonly struct DefaultAbsencePolicy<T> : IEquatable<T>
     {
-        public bool Equals(T other) => EqualityComparer<T>.Default.Equals(default, other);
+        public bool Equals(T? other) => EqualityComparer<T>.Default.Equals(other!, default!);
     }
 }
--- src/Arborescence.Primitives/NullAbsencePolicy.cs
+++
@@ -1,9 +0,0 @@
-namespace Arborescence
-{
-    using System;
-
-    public readonly struct NullAbsencePolicy<T> : IEquatable<T>
-    {
-        public bool Equals(T? other) => other is null;
-    }
-}
