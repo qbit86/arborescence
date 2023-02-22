649defda7c6580c0780b2ef99f9000aa74a6aaef
Viktor Ptitselov <qbit86@gmail.com>
2023-02-23 01:18:14 +0200

    [Primitives.Specialized] Implement TryGetValueCore() and Put() in Int32Dictionary<>

--- src/Arborescence.Primitives.Specialized/Int32Dictionary_3.cs
+++ src/Arborescence.Primitives.Specialized/Int32Dictionary_3.cs
@@ -5,6 +5,7 @@ namespace Arborescence
     using System.Diagnostics.CodeAnalysis;
     using System.Runtime.CompilerServices;
     using Primitives;
+    using static TryHelpers;
 
     public readonly partial struct Int32Dictionary<TValue, TValueList, TAbsenceComparer> :
         IReadOnlyDictionary<int, TValue>, IDictionary<int, TValue>,
@@ -32,15 +33,47 @@ namespace Arborescence
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool TryGetValue(int key, [MaybeNullWhen(false)] out TValue value) => TryGetValueCore(key, out value);
 
-        private bool TryGetValueCore(int key, [MaybeNullWhen(false)] out TValue value) =>
-            throw new NotImplementedException();
+        private bool TryGetValueCore(int key, [MaybeNullWhen(false)] out TValue value)
+        {
+            Int32Dictionary<TValue, TValueList, TAbsenceComparer> self = this;
+            if (self._items is not { } items)
+                return None(out value);
+            if (unchecked((uint)key >= (uint)items.Count))
+                return None(out value);
+
+            value = items[key];
+            return !self._absenceComparer.Equals(value, _absenceMarker!);
+        }
+
+        private void Put(int key, TValue value)
+        {
+            Int32Dictionary<TValue, TValueList, TAbsenceComparer> self = this;
+            TValueList items = _items;
+            int count = items.Count;
+            if (key < 0)
+            {
+                ThrowHelper.ThrowArgumentOutOfRangeException(nameof(key));
+                return;
+            }
+
+            if (key < count)
+            {
+                items[key] = value;
+                return;
+            }
+
+            int absentCount = key - count;
+            for (int i = 0; i < absentCount; ++i)
+                items.Add(self._absenceMarker!);
+            items.Add(value);
+        }
 
         public TValue this[int key]
         {
             get => TryGetValueCore(key, out TValue? value)
                 ? value
                 : ThrowHelper.ThrowKeyNotFoundException<TValue>(key);
-            set => throw new NotImplementedException();
+            set => Put(key, value);
         }
     }
 }
