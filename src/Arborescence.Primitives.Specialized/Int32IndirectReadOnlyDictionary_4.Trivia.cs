c542ca81520efb6a397b710955e25f86ca52a6af
Viktor Ptitselov <qbit86@gmail.com>
2023-02-25 04:13:59 +0200

    [Primitives.Specialized] Implement Int32IndirectReadOnlyDictionary<>.Values

--- src/Arborescence.Primitives.Specialized/Int32IndirectReadOnlyDictionary_4.Trivia.cs
+++ src/Arborescence.Primitives.Specialized/Int32IndirectReadOnlyDictionary_4.Trivia.cs
@@ -2,12 +2,14 @@ namespace Arborescence
 {
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
 
     partial struct Int32IndirectReadOnlyDictionary<TKey, TValue, TKeyToIndexMap, TIndexToValueMap>
     {
         public IEnumerable<TKey> Keys => throw new System.NotImplementedException();
 
-        public IEnumerable<TValue> Values => throw new System.NotImplementedException();
+        public IEnumerable<TValue> Values =>
+            _valueByIndex is { } valueByIndex ? valueByIndex.Values : Enumerable.Empty<TValue>();
 
         bool IReadOnlyDictionary<TKey, TValue>.TryGetValue(TKey key, out TValue value) =>
             TryGetValueCore(key, out value!);
