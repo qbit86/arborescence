ceac6c0dcedc5ed426fad9754a6591031cb7b4bb
Viktor Ptitselov <qbit86@gmail.com>
2023-02-27 00:02:36 +0200

    [Primitives.Specialized] Add CreateFromList<> to Int32IndirectDictionaryFactory<TKey, TValue>

--- src/Arborescence.Primitives.Specialized/Int32IndirectDictionaryFactory.cs
+++ src/Arborescence.Primitives.Specialized/Int32IndirectDictionaryFactory.cs
@@ -5,6 +5,38 @@ namespace Arborescence
 
     public static class Int32IndirectDictionaryFactory<TKey, TValue>
     {
+        public static Int32IndirectDictionary<TKey, TValue, TKeyToIndexMap, Int32Dictionary<TValue, TValueList>>
+            CreateFromList<TKeyToIndexMap, TValueList>(TKeyToIndexMap indexByKey, TValueList values)
+            where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
+            where TValueList : IList<TValue>
+        {
+            if (indexByKey is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(indexByKey));
+            if (values is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(values));
+            Int32Dictionary<TValue, TValueList> valueByIndex = new(values);
+            return new(indexByKey, valueByIndex);
+        }
+
+        public static Int32IndirectDictionary<
+                TKey, TValue, TKeyToIndexMap, Int32Dictionary<TValue, TValueList, TAbsenceComparer>>
+            CreateFromListWithAbsence<TKeyToIndexMap, TValueList, TAbsenceComparer>(
+                TKeyToIndexMap indexByKey, TValueList values, TAbsenceComparer absenceComparer, TValue? absenceMarker)
+            where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
+            where TValueList : IList<TValue>
+            where TAbsenceComparer : IEqualityComparer<TValue>
+        {
+            if (indexByKey is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(indexByKey));
+            if (values is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(values));
+            if (absenceComparer is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(absenceComparer));
+            Int32Dictionary<TValue, TValueList, TAbsenceComparer> valueByIndex =
+                new(values, absenceComparer, absenceMarker);
+            return new(indexByKey, valueByIndex);
+        }
+
         public static Int32IndirectDictionary<TKey, TValue, TKeyToIndexMap, TIndexToValueMap>
             Create<TKeyToIndexMap, TIndexToValueMap>(TKeyToIndexMap indexByKey, TIndexToValueMap valueByIndex)
             where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
