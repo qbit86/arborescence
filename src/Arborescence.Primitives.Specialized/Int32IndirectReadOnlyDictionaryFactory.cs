619ecf09a382a521004e7a53aae348573e66c5a1
Viktor Ptitselov <qbit86@gmail.com>
2023-02-26 20:52:33 +0200

    [Primitives.Specialized] Add more factory methods to Int32IndirectReadOnlyDictionaryFactory<TKey, TValue>

--- src/Arborescence.Primitives.Specialized/Int32IndirectReadOnlyDictionaryFactory.cs
+++ src/Arborescence.Primitives.Specialized/Int32IndirectReadOnlyDictionaryFactory.cs
@@ -1,10 +1,59 @@
 namespace Arborescence
 {
+    using System;
     using System.Collections.Generic;
     using Primitives;
 
     public static class Int32IndirectReadOnlyDictionaryFactory<TKey, TValue>
     {
+        public static Int32IndirectReadOnlyDictionary<
+                TKey, TValue, TKeyToIndexMap, Int32ReadOnlyDictionary<TValue, TValueList>>
+            CreateFromList<TKeyToIndexMap, TValueList>(TKeyToIndexMap indexByKey, TValueList values)
+            where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
+            where TValueList : IReadOnlyList<TValue>
+        {
+            if (indexByKey is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(indexByKey));
+            if (values is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(values));
+            Int32ReadOnlyDictionary<TValue, TValueList> valueByIndex = new(values);
+            return new(indexByKey, valueByIndex);
+        }
+
+        public static Int32IndirectReadOnlyDictionary<
+                TKey, TValue, TKeyToIndexMap, Int32ReadOnlyDictionary<TValue, TValueList, DefaultAbsencePolicy<TValue>>>
+            CreateFromListWithAbsence<TKeyToIndexMap, TValueList>(
+                TKeyToIndexMap indexByKey, TValueList values)
+            where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
+            where TValueList : IReadOnlyList<TValue>
+        {
+            if (indexByKey is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(indexByKey));
+            if (values is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(values));
+            Int32ReadOnlyDictionary<TValue, TValueList, DefaultAbsencePolicy<TValue>> valueByIndex =
+                new(values, default);
+            return new(indexByKey, valueByIndex);
+        }
+
+        public static Int32IndirectReadOnlyDictionary<
+                TKey, TValue, TKeyToIndexMap, Int32ReadOnlyDictionary<TValue, TValueList, TAbsencePolicy>>
+            CreateFromListWithAbsence<TKeyToIndexMap, TValueList, TAbsencePolicy>(
+                TKeyToIndexMap indexByKey, TValueList values, TAbsencePolicy absencePolicy)
+            where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
+            where TValueList : IReadOnlyList<TValue>
+            where TAbsencePolicy : IEquatable<TValue>
+        {
+            if (indexByKey is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(indexByKey));
+            if (values is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(values));
+            if (absencePolicy is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(absencePolicy));
+            Int32ReadOnlyDictionary<TValue, TValueList, TAbsencePolicy> valueByIndex = new(values, absencePolicy);
+            return new(indexByKey, valueByIndex);
+        }
+
         public static Int32IndirectReadOnlyDictionary<TKey, TValue, TKeyToIndexMap, TIndexToValueMap>
             Create<TKeyToIndexMap, TIndexToValueMap>(TKeyToIndexMap indexByKey, TIndexToValueMap valueByIndex)
             where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
