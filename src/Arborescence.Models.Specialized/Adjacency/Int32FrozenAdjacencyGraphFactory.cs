8ff2df7c9c1f614f272f27723fc8383d3a786b34
Viktor Ptitselov <qbit86@gmail.com>
2023-04-11 02:56:58 +0300

    [Models.Specialized] Add boilerplate for Int32FrozenAdjacencyGraphFactory<>.FromEdgeList()

--- src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
+++ src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
@@ -3,6 +3,7 @@ namespace Arborescence.Models.Specialized
 {
     using System;
     using System.Collections.Generic;
+    using Edge = Endpoints<int>;
 
     public static class Int32FrozenAdjacencyGraphFactory<TVertexCollection>
         where TVertexCollection : ICollection<int>
@@ -14,6 +15,32 @@ namespace Arborescence.Models.Specialized
         public static Int32FrozenAdjacencyGraph FromDictionary<TMultimap>(TMultimap neighborsByVertex)
             where TMultimap : IReadOnlyDictionary<int, TVertexCollection> =>
             neighborsByVertex is null ? default : FromDictionaryUnchecked(neighborsByVertex);
+
+#if NET5_0_OR_GREATER
+        public static Int32FrozenAdjacencyGraph FromEdgeList(Span<Edge> edges, int vertexCount)
+        {
+            if (vertexCount is 0)
+                return default;
+            if (vertexCount < 0)
+                ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
+
+            edges.Sort(EdgeComparer.Instance);
+            return FromSortedEdgeList(edges, vertexCount);
+        }
+#endif
+
+        public static Int32FrozenAdjacencyGraph FromEdgeList(Edge[] edges, int vertexCount)
+        {
+            if (vertexCount is 0)
+                return default;
+            if (edges is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(edges));
+            if (vertexCount < 0)
+                ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
+
+            Array.Sort(edges, EdgeComparer.Instance);
+            return FromSortedEdgeList(edges, vertexCount);
+        }
 
         private static Int32FrozenAdjacencyGraph FromListUnchecked<TMultimap>(TMultimap neighborsByVertex)
             where TMultimap : IReadOnlyList<TVertexCollection>
@@ -55,6 +82,16 @@ namespace Arborescence.Models.Specialized
             }
 
             return new(data.Array!);
+        }
+
+        private static Int32FrozenAdjacencyGraph FromSortedEdgeList(
+            ReadOnlySpan<Edge> edgesSortedByTail, int vertexCount) => throw new NotImplementedException();
+
+        private sealed class EdgeComparer : IComparer<Edge>
+        {
+            internal static EdgeComparer Instance { get; } = new();
+
+            public int Compare(Edge x, Edge y) => x.Tail.CompareTo(y.Tail);
         }
     }
 }
