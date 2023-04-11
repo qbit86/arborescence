7417c57794e1347547ec2764b112b0187ac1b9f9
Viktor Ptitselov <qbit86@gmail.com>
2023-04-11 03:03:46 +0300

    [Models.Specialized] Split static classes Int32FrozenAdjacencyGraphFactory into separate files

--- src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
+++ src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
@@ -43,59 +43,5 @@ namespace Arborescence.Models.Specialized
             public int Compare(Edge x, Edge y) => x.Tail.CompareTo(y.Tail);
         }
     }
-
-    public static class Int32FrozenAdjacencyGraphFactory<TVertexCollection>
-        where TVertexCollection : ICollection<int>
-    {
-        public static Int32FrozenAdjacencyGraph FromList<TMultimap>(TMultimap neighborsByVertex)
-            where TMultimap : IReadOnlyList<TVertexCollection> =>
-            neighborsByVertex is null ? default : FromListUnchecked(neighborsByVertex);
-
-        public static Int32FrozenAdjacencyGraph FromDictionary<TMultimap>(TMultimap neighborsByVertex)
-            where TMultimap : IReadOnlyDictionary<int, TVertexCollection> =>
-            neighborsByVertex is null ? default : FromDictionaryUnchecked(neighborsByVertex);
-
-        private static Int32FrozenAdjacencyGraph FromListUnchecked<TMultimap>(TMultimap neighborsByVertex)
-            where TMultimap : IReadOnlyList<TVertexCollection>
-        {
-            Int32ReadOnlyDictionary<TVertexCollection, TMultimap> dictionary =
-                Int32ReadOnlyDictionaryFactory<TVertexCollection>.Create(neighborsByVertex);
-            return FromDictionaryUnchecked(dictionary);
-        }
-
-        private static Int32FrozenAdjacencyGraph FromDictionaryUnchecked<TMultimap>(TMultimap neighborsByVertex)
-            where TMultimap : IReadOnlyDictionary<int, TVertexCollection>
-        {
-            int vertexCount = neighborsByVertex.Count;
-            ArrayPrefix<int> data = new();
-            data = ArrayPrefixBuilder.Add(data, vertexCount, false);
-            data = ArrayPrefixBuilder.Add(data, 0, false);
-            data = ArrayPrefixBuilder.EnsureSize(data, data.Count + vertexCount, false);
-            Span<int> upperBoundByVertex = data.Array.AsSpan(2, vertexCount);
-            int offset = 2 + vertexCount;
-            for (int key = 0; key < vertexCount; ++key)
-            {
-                if (!neighborsByVertex.TryGetValue(key, out TVertexCollection? neighbors) || neighbors is null)
-                {
-                    upperBoundByVertex[key] = offset;
-                    continue;
-                }
-
-                int neighborCount = neighbors.Count;
-                if (neighborCount is 0)
-                {
-                    upperBoundByVertex[key] = offset;
-                    continue;
-                }
-
-                data = ArrayPrefixBuilder.EnsureSize(data, data.Count + neighborCount, false);
-                neighbors.CopyTo(data.Array!, offset);
-                offset += neighborCount;
-                upperBoundByVertex[key] = offset;
-            }
-
-            return new(data.Array!);
-        }
-    }
 }
 #endif
