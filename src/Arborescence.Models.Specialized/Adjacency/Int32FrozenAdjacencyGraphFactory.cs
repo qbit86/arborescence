12ea0dbd6d986df7b3c993a260639526cb984b1a
Viktor Ptitselov <qbit86@gmail.com>
2023-04-12 03:40:31 +0300

    [Models.Specialized] Add overload of FromEdges() taking List<Edge>

--- src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
+++ src/Arborescence.Models.Specialized/Adjacency/Int32FrozenAdjacencyGraphFactory.cs
@@ -1,39 +1,29 @@
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER
 namespace Arborescence.Models.Specialized
 {
+#if NET5_0_OR_GREATER
+    using System.Runtime.InteropServices;
+#endif
     using System;
     using System.Collections.Generic;
     using Edge = Endpoints<int>;
 
     public static class Int32FrozenAdjacencyGraphFactory
     {
-#if NET5_0_OR_GREATER
-        public static Int32FrozenAdjacencyGraph FromEdgeList(Span<Edge> edges, int vertexCount)
+        public static Int32FrozenAdjacencyGraph FromEdges(Edge[] edges, int vertexCount)
         {
-            if (vertexCount is 0)
-                return default;
-            if (vertexCount < 0)
-                ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
-
-            edges.Sort(EdgeComparer.Instance);
-            return FromSortedEdgeList(edges, vertexCount);
-        }
-#endif
-
-        public static Int32FrozenAdjacencyGraph FromEdgeList(Edge[] edges, int vertexCount)
-        {
-            if (vertexCount is 0)
-                return default;
             if (edges is null)
                 ThrowHelper.ThrowArgumentNullException(nameof(edges));
+            if (vertexCount is 0)
+                return default;
             if (vertexCount < 0)
                 ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
 
             Array.Sort(edges, EdgeComparer.Instance);
-            return FromSortedEdgeList(edges, vertexCount);
+            return FromSortedEdges(edges, vertexCount);
         }
 
-        private static Int32FrozenAdjacencyGraph FromSortedEdgeList(
+        private static Int32FrozenAdjacencyGraph FromSortedEdges(
             ReadOnlySpan<Edge> edgesSortedByTail, int vertexCount) => throw new NotImplementedException();
 
         private sealed class EdgeComparer : IComparer<Edge>
@@ -42,6 +32,32 @@ namespace Arborescence.Models.Specialized
 
             public int Compare(Edge x, Edge y) => x.Tail.CompareTo(y.Tail);
         }
+
+#if NET5_0_OR_GREATER
+        public static Int32FrozenAdjacencyGraph FromEdges(Span<Edge> edges, int vertexCount)
+        {
+            if (vertexCount is 0)
+                return default;
+            if (vertexCount < 0)
+                ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
+
+            edges.Sort(EdgeComparer.Instance);
+            return FromSortedEdges(edges, vertexCount);
+        }
+
+        public static Int32FrozenAdjacencyGraph FromEdges(List<Edge> edges, int vertexCount)
+        {
+            if (edges is null)
+                throw new ArgumentNullException(nameof(edges));
+            if (vertexCount is 0)
+                return default;
+            if (vertexCount < 0)
+                ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
+
+            edges.Sort(EdgeComparer.Instance);
+            return FromSortedEdges(CollectionsMarshal.AsSpan(edges), vertexCount);
+        }
+#endif
     }
 }
 #endif
