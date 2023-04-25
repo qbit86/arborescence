5969ebf8dc293abed9ea28b994a90507ce89982d
Viktor Ptitselov <qbit86@gmail.com>
2023-04-26 02:33:19 +0300

    [Models.Specialized] Add comment about nuances of Int32FrozenIncidenceGraphFactory implementation

--- src/Arborescence.Models.Specialized/Incidence/Int32FrozenIncidenceGraphFactory.cs
+++ src/Arborescence.Models.Specialized/Incidence/Int32FrozenIncidenceGraphFactory.cs
@@ -1,10 +1,11 @@
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER
 namespace Arborescence.Models.Specialized
 {
+    using System;
 #if NET5_0_OR_GREATER
     using System.Collections.Generic;
+    using System.Runtime.InteropServices;
 #endif
-    using System;
 
     public static class Int32FrozenIncidenceGraphFactory
     {
@@ -17,8 +18,24 @@ namespace Arborescence.Models.Specialized
             if (vertexCount < 0)
                 ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
 
-            throw new NotImplementedException();
+            // We cannot reuse this overload taking span:
+            // Int32FrozenIncidenceGraphFactory.FromEdgesUnchecked(int, Span<Endpoints<int>>)
+            // Because the sorting algorithm that takes spans is only available in .NET 5 and later:
+            // MemoryExtensions.Sort<TKey,TValue,TComparer>(Span<TKey>, Span<TValue>, TComparer)
+            // https://learn.microsoft.com/en-us/dotnet/api/system.memoryextensions.sort?view=net-6.0
+
+            // We need to implement sorting independently with another overload that takes arrays:
+            // Int32FrozenIncidenceGraphFactory.FromEdgesUnchecked(int, Endpoints<int>[])
+            // Because the sorting algorithm that takes arrays is available on older versions of .NET:
+            // Array.Sort<TKey,TValue>(TKey[], TValue[], IComparer<TKey>)
+            // https://learn.microsoft.com/en-us/dotnet/api/system.array.sort?view=net-6.0
+
+            return FromEdgesUnchecked(vertexCount, endpointsByEdge);
         }
+
+        private static Int32FrozenIncidenceGraph FromEdgesUnchecked(
+            int vertexCount, Endpoints<int>[] endpointsByEdge) =>
+            throw new NotImplementedException();
 
 #if NET5_0_OR_GREATER
         public static Int32FrozenIncidenceGraph FromEdges(int vertexCount, Span<Endpoints<int>> endpointsByEdge)
@@ -28,10 +45,10 @@ namespace Arborescence.Models.Specialized
             if (vertexCount < 0)
                 ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
 
-            throw new NotImplementedException();
+            return FromEdgesUnchecked(vertexCount, endpointsByEdge);
         }
 
-        public static Int32FrozenAdjacencyGraph FromEdges(int vertexCount, List<Endpoints<int>> endpointsByEdge)
+        public static Int32FrozenIncidenceGraph FromEdges(int vertexCount, List<Endpoints<int>> endpointsByEdge)
         {
             if (endpointsByEdge is null)
                 throw new ArgumentNullException(nameof(endpointsByEdge));
@@ -40,8 +57,12 @@ namespace Arborescence.Models.Specialized
             if (vertexCount < 0)
                 ThrowHelper.ThrowArgumentOutOfRangeException(nameof(vertexCount));
 
-            throw new NotImplementedException();
+            return FromEdgesUnchecked(vertexCount, CollectionsMarshal.AsSpan(endpointsByEdge));
         }
+
+        private static Int32FrozenIncidenceGraph FromEdgesUnchecked(
+            int vertexCount, Span<Endpoints<int>> endpointsByEdge) =>
+            throw new NotImplementedException();
 #endif
     }
 }
