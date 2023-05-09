7685a47caf7f73f26a35dd3177503dcb58976989
Viktor Ptitselov <qbit86@gmail.com>
2023-05-09 20:22:14 +0300

    [Tests] Add `EnumerateOutEdges_ExistingVertex_ReturnsKnownEdges()` to `Int32AdjacencyGraphTests`

--- tests/Tests.Models.Specialized/Adjacency/Int32AdjacencyGraphTests.cs
+++ tests/Tests.Models.Specialized/Adjacency/Int32AdjacencyGraphTests.cs
@@ -37,4 +37,40 @@ public sealed class Int32AdjacencyGraphTests
         // Assert
         Assert.Equal(expectedNeighbors, actualNeighbors);
     }
+
+    [Fact]
+    internal void EnumerateOutEdges_ExistingVertex_ReturnsKnownEdges()
+    {
+        // Arrange
+        using TextReader textReader = IndexedGraphs.GetTextReader("09");
+        IEnumerable<Int32Endpoints> rawEdges = IndexedEdgeListParser.ParseEdges(textReader);
+#if NET5_0_OR_GREATER
+        var edges = rawEdges.Select(Transform).ToList();
+#else
+        Endpoints<int>[] edges = rawEdges.Select(Transform).ToArray();
+#endif
+        int vertex = Base32.Parse("p");
+        var expectedEdges = new HashSet<Endpoints<int>>
+        {
+            Create("p", "f"),
+            Create("p", "q"),
+            Create("p", "m"),
+            Create("p", "r"),
+        };
+
+        static Endpoints<int> Transform(Int32Endpoints endpoints) => new(endpoints.Tail, endpoints.Head);
+
+        static Endpoints<int> Create(ReadOnlySpan<char> tail, ReadOnlySpan<char> head) =>
+            Endpoints.Create(Base32.Parse(tail), Base32.Parse(head));
+
+        // Act
+        Int32FrozenAdjacencyGraph graph = Int32FrozenAdjacencyGraphFactory.FromEdges(edges);
+        IncidenceEnumerator<int, ArraySegment<int>.Enumerator> edgeEnumerator = graph.EnumerateOutEdges(vertex);
+        HashSet<Endpoints<int>> actualEdges = new(4);
+        while (edgeEnumerator.MoveNext())
+            actualEdges.Add(edgeEnumerator.Current);
+
+        // Assert
+        Assert.Equal(expectedEdges, actualEdges);
+    }
 }
