b572a509e7f4bc03892cd70149c2390ec4162dc5
Viktor Ptitselov <qbit86@gmail.com>
2023-05-10 04:15:45 +0300

    [Tests] Add `TryGetEndpoints_AllEdges_ReturnsSameEndpoints()` to `Int32IncidenceGraphTests`

--- tests/Tests.Models.Specialized/Incidence/Int32IncidenceGraphTests.EntireGraph.cs
+++ tests/Tests.Models.Specialized/Incidence/Int32IncidenceGraphTests.EntireGraph.cs
@@ -52,4 +52,40 @@ public sealed partial class Int32IncidenceGraphTests
             }
         }
     }
+
+    [Theory]
+    [ClassData(typeof(GraphDefinitionCollection))]
+    internal void TryGetEndpoints_AllEdges_ReturnsSameEndpoints(GraphDefinitionParameter p)
+    {
+        IReadOnlyList<Int32Endpoints> expectedEndpointsByEdge = p.Edges;
+        int edgeCount = expectedEndpointsByEdge.Count;
+#if NET5_0_OR_GREATER
+        var edges = expectedEndpointsByEdge.Select(Transform).ToList();
+#else
+        Endpoints<int>[] edges = expectedEndpointsByEdge.Select(Transform).ToArray();
+#endif
+        Int32IncidenceGraph graph = Int32IncidenceGraphFactory.FromEdges(edges);
+        Assert.Equal(p.VertexCount, graph.VertexCount);
+        Assert.Equal(edgeCount, graph.EdgeCount);
+
+        for (int edge = 0; edge < edgeCount; ++edge)
+        {
+            if (!graph.TryGetTail(edge, out int actualTail))
+                Assert.Fail($"{nameof(edge)}: {edge}, {nameof(graph.TryGetTail)}: false");
+            if (!graph.TryGetHead(edge, out int actualHead))
+                Assert.Fail($"{nameof(edge)}: {edge}, {nameof(graph.TryGetHead)}: false");
+            Int32Endpoints expectedEndpoints = expectedEndpointsByEdge[edge];
+            if (expectedEndpoints.Tail != actualTail)
+            {
+                Assert.Fail(
+                    $"{nameof(expectedEndpoints)}.Tail: {expectedEndpoints.Tail}, {nameof(actualTail)}: {actualTail}");
+            }
+
+            if (expectedEndpoints.Head != actualHead)
+            {
+                Assert.Fail(
+                    $"{nameof(expectedEndpoints)}.Head: {expectedEndpoints.Head}, {nameof(actualHead)}: {actualHead}");
+            }
+        }
+    }
 }
