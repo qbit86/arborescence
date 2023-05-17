b77c384079f6a3cbef57d8299d4d3d7bf5a0782f
Viktor Ptitselov <qbit86@gmail.com>
2023-05-18 02:22:32 +0300

    [Samples] Replace IndexedIncidenceGraph with Int32IncidenceGraph in DemoRecursiveDfs()

--- samples/Samples.Playground/Program.DemoRecursiveDfs.cs
+++ samples/Samples.Playground/Program.DemoRecursiveDfs.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Buffers;
 using System.Collections.Generic;
-using Models;
+using Models.Specialized;
 using Traversal;
 using Traversal.Incidence;
 using EdgeEnumerator = System.ArraySegment<int>.Enumerator;
@@ -12,15 +12,15 @@ internal static partial class Program
 {
     private static void DemoRecursiveDfs()
     {
-        const int vertexCount = 10000;
+        const int vertexCount = 1000;
         const double densityPower = 1.5;
-        IndexedIncidenceGraph graph = GenerateIncidenceGraph(vertexCount, densityPower);
+        Int32IncidenceGraph graph = GenerateIncidenceGraph(vertexCount, densityPower);
 
         byte[] backingStore = ArrayPool<byte>.Shared.Rent(vertexCount);
         Array.Clear(backingStore, 0, backingStore.Length);
         IndexedColorDictionary colorByVertex = new(backingStore);
         List<int> steps = new();
-        DfsHandler<int, int, IndexedIncidenceGraph> dfsHandler = CreateDfsHandler(steps);
+        DfsHandler<int, int, Int32IncidenceGraph> dfsHandler = CreateDfsHandler(steps);
 
         RecursiveDfs<int, int, EdgeEnumerator>.Traverse(graph, 0, colorByVertex, dfsHandler);
         Console.WriteLine(steps.Count);
@@ -28,28 +28,28 @@ internal static partial class Program
         ArrayPool<byte>.Shared.Return(backingStore, true);
     }
 
-    private static DfsHandler<int, int, IndexedIncidenceGraph> CreateDfsHandler(ICollection<int> steps)
+    private static DfsHandler<int, int, Int32IncidenceGraph> CreateDfsHandler(ICollection<int> steps)
     {
-        DfsHandler<int, int, IndexedIncidenceGraph> result = new();
+        DfsHandler<int, int, Int32IncidenceGraph> result = new();
         result.TreeEdge += (_, e) => steps.Add(e);
         return result;
     }
 
-    private static IndexedIncidenceGraph GenerateIncidenceGraph(int vertexCount, double densityPower)
+    private static Int32IncidenceGraph GenerateIncidenceGraph(int vertexCount, double densityPower)
     {
         int edgeCount = (int)Math.Ceiling(Math.Pow(vertexCount, densityPower));
 
-        IndexedIncidenceGraph.Builder builder = new(vertexCount);
+        List<Endpoints<int>> endpointsByEdge = new(vertexCount);
         Random prng = new(1729);
 
         for (int e = 0; e < edgeCount; ++e)
         {
             int tail = prng.Next(vertexCount);
             int head = prng.Next(vertexCount);
-            builder.Add(tail, head);
+            endpointsByEdge.Add(new(tail, head));
         }
 
-        IndexedIncidenceGraph result = builder.ToGraph();
+        Int32IncidenceGraph result = Int32IncidenceGraphFactory.FromEdges(endpointsByEdge);
         return result;
     }
 }
