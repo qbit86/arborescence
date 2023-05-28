cbc21833aacdb6361f1525b504a6427b26f062be
Viktor Ptitselov <qbit86@gmail.com>
2023-05-29 00:41:51 +0300

    [Abstractions] Add XML-doc comments

--- src/Arborescence.Abstractions/Concepts/IDictionaryAddition.cs
+++ src/Arborescence.Abstractions/Concepts/IDictionaryAddition.cs
@@ -1,7 +1,19 @@
 namespace Arborescence
 {
+    /// <summary>
+    /// Provides a method to add an element with the provided key and value to the <typeparamref name="TDictionary"/>.
+    /// </summary>
+    /// <typeparam name="TDictionary">The type of the dictionary.</typeparam>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
     public interface IDictionaryAddition<in TDictionary, in TKey, in TValue>
     {
+        /// <summary>
+        /// Adds an element with the provided key and value to the <paramref name="dictionary"/>.
+        /// </summary>
+        /// <param name="dictionary">The dictionary to add an element to.</param>
+        /// <param name="key">The object to use as the key of the element to add.</param>
+        /// <param name="value">The object to use as the value of the element to add.</param>
         void Add(TDictionary dictionary, TKey key, TValue value);
     }
 }
--- src/Arborescence.Abstractions/Concepts/IEnumeratorProvider.cs
+++ src/Arborescence.Abstractions/Concepts/IEnumeratorProvider.cs
@@ -1,9 +1,23 @@
 namespace Arborescence
 {
+    /// <summary>
+    /// Exposes the enumerator, which supports a simple iteration over the <typeparamref name="TCollection"/>.
+    /// </summary>
+    /// <typeparam name="TCollection">The type of the collection.</typeparam>
+    /// <typeparam name="TEnumerator">The type of the enumerator.</typeparam>
     public interface IEnumeratorProvider<in TCollection, out TEnumerator>
     {
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <param name="collection">The collection to iterate through.</param>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
         TEnumerator GetEnumerator(TCollection collection);
 
+        /// <summary>
+        /// Returns an empty enumeration.
+        /// </summary>
+        /// <returns>An enumerator that indicates the end of the collection.</returns>
         TEnumerator GetEmptyEnumerator();
     }
 }
