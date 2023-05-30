e0f79951bac46e844e9e5ba317179ef702d4c13b
Viktor Ptitselov <qbit86@gmail.com>
2023-05-30 03:32:54 +0300

    [Models] Add XML-doc comments for `AdjacencyEnumerator<>`

--- src/Arborescence.Models/AdjacencyEnumerator.cs
+++ src/Arborescence.Models/AdjacencyEnumerator.cs
@@ -3,8 +3,21 @@ namespace Arborescence.Models
     using System.Collections;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Provides support for creating <see cref="AdjacencyEnumerator{TVertex, TEdge, TGraph, TEdgeEnumerator}"/> objects.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
     public static class AdjacencyEnumeratorFactory<TVertex, TEdge>
     {
+        /// <summary>
+        /// Creates a <see cref="AdjacencyEnumerator{TVertex, TEdge, TGraph, TEdgeEnumerator}"/>.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="edgeEnumerator">The enumerator for the collection of edges.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
+        /// <returns></returns>
         public static AdjacencyEnumerator<TVertex, TEdge, TGraph, TEdgeEnumerator> Create<TGraph, TEdgeEnumerator>(
             TGraph graph, TEdgeEnumerator edgeEnumerator)
             where TGraph : IHeadIncidence<TVertex, TEdge>
@@ -19,6 +32,13 @@ namespace Arborescence.Models
         }
     }
 
+    /// <summary>
+    /// Provides an enumerator for the heads of given edges.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    /// <typeparam name="TGraph">The type of the graph.</typeparam>
+    /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
     public struct AdjacencyEnumerator<TVertex, TEdge, TGraph, TEdgeEnumerator> :
         IEnumerable<TVertex>, IEnumerator<TVertex>
         where TGraph : IHeadIncidence<TVertex, TEdge>
@@ -35,12 +55,15 @@ namespace Arborescence.Models
             _current = default;
         }
 
+        /// <inheritdoc/>
         public TVertex Current => _current!;
 
         object? IEnumerator.Current => Current;
 
+        /// <inheritdoc/>
         public void Dispose() => _edgeEnumerator.Dispose();
 
+        /// <inheritdoc/>
         public bool MoveNext()
         {
             do
@@ -54,8 +77,13 @@ namespace Arborescence.Models
             } while (true);
         }
 
+        /// <inheritdoc/>
         public void Reset() => _edgeEnumerator.Reset();
 
+        /// <summary>
+        /// Returns the current enumerator instance.
+        /// </summary>
+        /// <returns>The current enumerator instance.</returns>
         public AdjacencyEnumerator<TVertex, TEdge, TGraph, TEdgeEnumerator> GetEnumerator() => this;
 
         IEnumerator<TVertex> IEnumerable<TVertex>.GetEnumerator() => this;
