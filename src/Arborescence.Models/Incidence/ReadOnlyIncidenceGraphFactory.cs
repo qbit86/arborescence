da1b4d4d32f21548c1849826cb9461f33fd8a4db
Viktor Ptitselov <qbit86@gmail.com>
2023-05-31 23:35:13 +0300

    [Models] Add XML-doc comments for `ReadOnlyIncidenceGraphFactory<>`

--- src/Arborescence.Models/Incidence/ReadOnlyIncidenceGraphFactory.cs
+++ src/Arborescence.Models/Incidence/ReadOnlyIncidenceGraphFactory.cs
@@ -2,9 +2,29 @@ namespace Arborescence.Models
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Provides support for creating
+    /// <see cref="ReadOnlyIncidenceGraph{TVertex, TEdge, TEdgeEnumerator, TEndpointMap, TEdgeMultimap, TEdgeMultimapConcept}"/>
+    /// objects.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
     public static class ReadOnlyIncidenceGraphFactory<TVertex, TEdge, TEdgeEnumerator>
         where TEdgeEnumerator : IEnumerator<TEdge>
     {
+        /// <summary>
+        /// Creates a
+        /// <see cref="ReadOnlyIncidenceGraph{TVertex, TEdge, TEdgeEnumerator, TEndpointMap, TEdgeMultimap, TEdgeMultimapConcept}"/>.
+        /// </summary>
+        /// <param name="tailByEdge">The object that provides the mapping from an edge to its tail.</param>
+        /// <param name="headByEdge">The object that provides the mapping from an edge to its head.</param>
+        /// <param name="outEdgesByVertex">The object that provides the mapping from a vertex to its out-edges.</param>
+        /// <param name="edgeMultimapConcept">The object that provides operations on the edge multimap.</param>
+        /// <typeparam name="TEndpointMap">The type of mapping from an edge to one of its endpoints.</typeparam>
+        /// <typeparam name="TEdgeMultimap">The type of mapping from a vertex to a sequence of its out-edges.</typeparam>
+        /// <typeparam name="TEdgeMultimapConcept">The type that provides operations on the edge multimap.</typeparam>
+        /// <returns>The read-only incidence graph.</returns>
         public static ReadOnlyIncidenceGraph<
                 TVertex, TEdge, TEdgeEnumerator, TEndpointMap, TEdgeMultimap, TEdgeMultimapConcept>
             CreateUnchecked<TEndpointMap, TEdgeMultimap, TEdgeMultimapConcept>(
@@ -26,9 +46,33 @@ namespace Arborescence.Models
         }
     }
 
+    /// <summary>
+    /// Provides support for creating
+    /// <see cref="ReadOnlyIncidenceGraph{TVertex, TEdge, TEdgeEnumerator, TEndpointMap, TEdgeMultimap, TEdgeMultimapConcept}"/>
+    /// objects, when <c>TEdgeMultimapConcept</c> is
+    /// <see cref="ReadOnlyMultimapConcept{TEdgeMultimap, TVertex, TEdgeCollection, TEdgeEnumerator, TEdgeEnumeratorProvider}"/>.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TEdge">The type of the edge.</typeparam>
+    /// <typeparam name="TEdgeCollection">The type of the edge collection.</typeparam>
+    /// <typeparam name="TEdgeEnumerator">The type of the edge enumerator.</typeparam>
     public static class ReadOnlyIncidenceGraphFactory<TVertex, TEdge, TEdgeCollection, TEdgeEnumerator>
         where TEdgeEnumerator : IEnumerator<TEdge>
     {
+        /// <summary>
+        /// Creates a
+        /// <see cref="ReadOnlyIncidenceGraph{TVertex, TEdge, TEdgeEnumerator, TEndpointMap, TEdgeMultimap, TEdgeMultimapConcept}"/>,
+        /// when <c>TEdgeMultimapConcept</c> is
+        /// <see cref="ReadOnlyMultimapConcept{TEdgeMultimap, TVertex, TEdgeCollection, TEdgeEnumerator, TEdgeEnumeratorProvider}"/>.
+        /// </summary>
+        /// <param name="tailByEdge">The object that provides the mapping from an edge to its tail.</param>
+        /// <param name="headByEdge">The object that provides the mapping from an edge to its head.</param>
+        /// <param name="outEdgesByVertex">The object that provides the mapping from a vertex to its out-edges.</param>
+        /// <param name="edgeEnumeratorProvider">The edge enumerator provider.</param>
+        /// <typeparam name="TEndpointMap">The type of mapping from an edge to one of its endpoints.</typeparam>
+        /// <typeparam name="TEdgeMultimap">The type of mapping from a vertex to a sequence of its out-edges.</typeparam>
+        /// <typeparam name="TEdgeEnumeratorProvider">The type of the edge enumerator provider.</typeparam>
+        /// <returns>The read-only incidence graph.</returns>
         public static ReadOnlyIncidenceGraph<
                 TVertex, TEdge, TEdgeEnumerator, TEndpointMap, TEdgeMultimap, ReadOnlyMultimapConcept<
                     TEdgeMultimap, TVertex, TEdgeCollection, TEdgeEnumerator, TEdgeEnumeratorProvider>>
