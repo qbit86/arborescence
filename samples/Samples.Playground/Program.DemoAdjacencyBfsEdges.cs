a33eff0b79c43cfb7f4c40211e643168ca302133
Viktor Ptitselov <qbit86@gmail.com>
2023-06-04 01:09:14 +0300

    [Samples] Refactor `DemoAdjacencyBfsEdges()`

--- samples/Samples.Playground/Program.DemoAdjacencyBfsEdges.cs
+++ samples/Samples.Playground/Program.DemoAdjacencyBfsEdges.cs
@@ -2,29 +2,29 @@ namespace Arborescence;
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Models.Specialized;
 using Traversal.Adjacency;
-using Workbench;
 
 internal static partial class Program
 {
     private static void DemoAdjacencyBfsEdges()
     {
-        using TextReader textReader = IndexedGraphs.GetTextReader("09");
-        var edges = Base32EdgeListParser.ParseEdges(textReader).ToList();
-        textReader.Dispose();
-
-        Int32AdjacencyGraph adjacencyGraph = Int32AdjacencyGraphFactory.FromEdges(edges);
-        IEnumerable<int> sources = "abcd".Select(it => Base32.Parse(it.ToString()));
-        IEnumerable<Endpoints<int>> arrows = EnumerableBfs<int, ArraySegment<int>.Enumerator>.EnumerateEdges(
-            adjacencyGraph, sources);
-
-        foreach (Endpoints<int> arrow in arrows)
+        Endpoints<int>[] edges =
         {
-            (int tail, int head) = arrow;
-            Console.WriteLine($"{V(tail)} -> {V(head)}");
-        }
+            new(2, 0),
+            new(4, 3),
+            new(0, 4),
+            new(3, 2),
+            new(4, 4),
+            new(0, 2),
+            new(2, 4)
+        };
+        Int32AdjacencyGraph graph =
+            Int32AdjacencyGraphFactory.FromEdges(edges);
+        IEnumerable<Endpoints<int>> treeEdges =
+            EnumerableBfs<int, ArraySegment<int>.Enumerator>.EnumerateEdges(
+                graph, source: 3);
+        foreach (Endpoints<int> edge in treeEdges)
+            Console.WriteLine(edge);
     }
 }
