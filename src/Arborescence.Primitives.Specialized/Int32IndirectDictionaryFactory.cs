788bcc4d90fed5fb178d4b9b613c7a12d7fd4548
Viktor Ptitselov <qbit86@gmail.com>
2023-06-08 01:06:47 +0300

    [Primitives.Specialized] Add XML-doc comments to `Int32IndirectDictionaryFactory<>` class

--- src/Arborescence.Primitives.Specialized/Int32IndirectDictionaryFactory.cs
+++ src/Arborescence.Primitives.Specialized/Int32IndirectDictionaryFactory.cs
@@ -11,6 +11,18 @@ namespace Arborescence
     /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
     public static class Int32IndirectDictionaryFactory<TKey, TValue>
     {
+        /// <summary>
+        /// Creates an <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// with the specified mapping from a key to an index and list of values.
+        /// </summary>
+        /// <param name="indexByKey">The mapping from a key to an index.</param>
+        /// <param name="values">The underlying list of the values.</param>
+        /// <typeparam name="TKeyToIndexMap">The type of the mapping from a key to an <see cref="int"/>.</typeparam>
+        /// <typeparam name="TValueList">The type of the backing list.</typeparam>
+        /// <returns>
+        /// An <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// that contains the specified values.
+        /// </returns>
         public static Int32IndirectDictionary<TKey, TValue, TKeyToIndexMap, Int32Dictionary<TValue, TValueList>>
             CreateFromList<TKeyToIndexMap, TValueList>(TKeyToIndexMap indexByKey, TValueList values)
             where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
@@ -24,6 +36,19 @@ namespace Arborescence
             return new(indexByKey, valueByIndex);
         }
 
+        /// <summary>
+        /// Creates an <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// with the specified mapping from a key to an index, list of values, and absence marker.
+        /// </summary>
+        /// <param name="indexByKey">The mapping from a key to an index.</param>
+        /// <param name="values">The underlying list of the values.</param>
+        /// <param name="absenceMarker">The object to use as the absence marker.</param>
+        /// <typeparam name="TKeyToIndexMap">The type of the mapping from a key to an <see cref="int"/>.</typeparam>
+        /// <typeparam name="TValueList">The type of the backing list.</typeparam>
+        /// <returns>
+        /// An <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// that contains the specified values.
+        /// </returns>
         public static Int32IndirectDictionary<
                 TKey, TValue, TKeyToIndexMap, Int32Dictionary<TValue, TValueList, EqualityComparer<TValue>>>
             CreateFromListWithAbsence<TKeyToIndexMap, TValueList>(
@@ -40,6 +65,22 @@ namespace Arborescence
             return new(indexByKey, valueByIndex);
         }
 
+        /// <summary>
+        /// Creates an <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// with the specified list of values
+        /// and the absence marker to compare values against with the specified comparer.
+        /// </summary>
+        /// <param name="indexByKey">The mapping from a key to an index.</param>
+        /// <param name="values">The underlying list of the values.</param>
+        /// <param name="absenceComparer">The comparer to compare the values with the absence marker.</param>
+        /// <param name="absenceMarker">The object to use as the absence marker.</param>
+        /// <typeparam name="TKeyToIndexMap">The type of the mapping from a key to an <see cref="int"/>.</typeparam>
+        /// <typeparam name="TValueList">The type of the backing list.</typeparam>
+        /// <typeparam name="TAbsenceComparer">The type that provides a method to distinguish missing elements.</typeparam>
+        /// <returns>
+        /// A <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// that contains the specified values.
+        /// </returns>
         public static Int32IndirectDictionary<
                 TKey, TValue, TKeyToIndexMap, Int32Dictionary<TValue, TValueList, TAbsenceComparer>>
             CreateFromListWithAbsence<TKeyToIndexMap, TValueList, TAbsenceComparer>(
@@ -60,6 +101,17 @@ namespace Arborescence
             return new(indexByKey, valueByIndex);
         }
 
+        /// <summary>
+        /// Creates an <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>
+        /// with the specified mapping from a key to an index and mapping from an index to a value.
+        /// </summary>
+        /// <param name="indexByKey">The mapping from a key to an index.</param>
+        /// <param name="valueByIndex">The mapping from an index to a value.</param>
+        /// <typeparam name="TKeyToIndexMap">The type of the mapping from a key to an <see cref="int"/>.</typeparam>
+        /// <typeparam name="TIndexToValueMap">The type of the mapping from an <see cref="int"/> to a value.</typeparam>
+        /// <returns>
+        /// An <see cref="Int32IndirectDictionary{TKey, TValue, TKeyToIndexMap, TIndexToValueMap}"/>.
+        /// </returns>
         public static Int32IndirectDictionary<TKey, TValue, TKeyToIndexMap, TIndexToValueMap>
             Create<TKeyToIndexMap, TIndexToValueMap>(TKeyToIndexMap indexByKey, TIndexToValueMap valueByIndex)
             where TKeyToIndexMap : IReadOnlyDictionary<TKey, int>
