437ae407b7cea3fa09c9a7eefa5ec270f7baa657
Viktor Ptitselov <qbit86@gmail.com>
2023-07-03 09:55:06 +0300

    [Tests] Add tests for absence for `Int32Dictionary<>`

---
+++ tests/Tests.Primitives.Specialized/Int32Dictionary_3_Tests.cs
@@ -0,0 +1,49 @@
+namespace Arborescence.Primitives.Specialized;
+
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+public sealed class Int32Dictionary_3_Tests
+{
+    private const string AbsenceMarkerLowerCase = "n/a";
+    private const string AbsenceMarkerUpperCase = "N/A";
+
+    [Fact]
+    public void TryGetValue_ForAbsenceMarker_ShouldReturnFalse()
+    {
+        Int32Dictionary<string, List<string>, StringComparer> dictionary = CreateSparseDictionary();
+        Assert.False(dictionary.TryGetValue(1, out _));
+        Assert.False(dictionary.TryGetValue(3, out _));
+    }
+
+    [Fact]
+    public void ContainsKey_ForAbsenceMarker_ShouldReturnFalse()
+    {
+        Int32Dictionary<string, List<string>, StringComparer> dictionary = CreateSparseDictionary();
+        Assert.False(dictionary.ContainsKey(1));
+        Assert.False(dictionary.ContainsKey(3));
+    }
+
+    [Fact]
+    public void GetCount_ForSparseDictionary_ShouldBeLessThanMaxCount()
+    {
+        Int32Dictionary<string, List<string>, StringComparer> dictionary = CreateSparseDictionary();
+        int count = dictionary.GetCount();
+        int maxCount = dictionary.MaxCount;
+        Assert.True(count <= maxCount);
+        Assert.Equal(2, count);
+        Assert.Equal(3, maxCount);
+    }
+
+    private static Int32Dictionary<string, List<string>, StringComparer> CreateSparseDictionary()
+    {
+        Int32Dictionary<string, List<string>, StringComparer> dictionary =
+            Int32DictionaryFactory<string>.CreateWithAbsence(
+                new List<string>(), StringComparer.InvariantCultureIgnoreCase, AbsenceMarkerUpperCase);
+        dictionary[0] = "Καλημέρα";
+        dictionary[1] = AbsenceMarkerLowerCase;
+        dictionary[2] = "Mañana";
+        return dictionary;
+    }
+}
