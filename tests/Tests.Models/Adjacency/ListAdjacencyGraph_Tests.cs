81fc893d44a8340a2837baa3d150a30747f34536
Viktor Ptitselov <qbit86@gmail.com>
2023-07-03 11:19:35 +0300

    [Tests] Add TryAddVertex_ShouldIncreaseVertexCount() to ListAdjacencyGraph_Tests

--- tests/Tests.Models/Adjacency/ListAdjacencyGraph_Tests.cs
+++ tests/Tests.Models/Adjacency/ListAdjacencyGraph_Tests.cs
@@ -1,5 +1,6 @@
 namespace Arborescence.Models;
 
+using System.Collections.Generic;
 using Xunit;
 
 public sealed class ListAdjacencyGraph_Tests
@@ -7,5 +8,9 @@ public sealed class ListAdjacencyGraph_Tests
     [Fact]
     public void TryAddVertex_ShouldIncreaseVertexCount()
     {
+        ListAdjacencyGraph<string, Dictionary<string, List<string>>> graph = ListAdjacencyGraphFactory<string>.Create();
+        Assert.True(graph.TryAddVertex("μηδέν"));
+        Assert.True(graph.TryAddVertex("δύο"));
+        Assert.Equal(2, graph.VertexCount);
     }
 }
