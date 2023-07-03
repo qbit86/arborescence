7ee2a5d32fc35dda1d330021bf5afe475cf09526
Viktor Ptitselov <qbit86@gmail.com>
2023-07-03 10:44:17 +0300

    [Tests] Add Tests.Models project

---
+++ tests/Tests.Models/Adjacency/ListAdjacencyGraph_Tests.cs
@@ -0,0 +1,11 @@
+namespace Arborescence.Models;
+
+using Xunit;
+
+public sealed class ListAdjacencyGraph_Tests
+{
+    [Fact]
+    public void TryAddVertex_ShouldIncreaseVertexCount()
+    {
+    }
+}
