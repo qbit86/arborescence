542bfea39361ef168e32aa28f4cf42a1da20c5e5
Viktor Ptitselov <qbit86@gmail.com>
2023-07-05 02:52:56 +0300

    [Traversal] Implement the rest of EagerBfs_2.MultipleSource

--- src/Arborescence.Traversal/Adjacency.EagerBfs/EagerBfs_2.MultipleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EagerBfs/EagerBfs_2.MultipleSource.cs
@@ -1,10 +1,102 @@
 namespace Arborescence.Traversal.Adjacency
 {
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
     using System.Threading;
 
     public static partial class EagerBfs<TVertex, TVertexEnumerator>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph> =>
+            TraverseChecked(graph, sources, handler, cancellationToken);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, IEqualityComparer<TVertex> comparer, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph> =>
+            TraverseChecked(graph, sources, comparer, handler, cancellationToken);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Traverse<TGraph, TSourceCollection, TColorMap, THandler>(
+            TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken = default)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph> =>
+            TraverseChecked(graph, sources, colorByVertex, handler, cancellationToken);
+
+        internal static void TraverseChecked<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (sources is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(sources));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            Dictionary<TVertex, Color> colorByVertex = new();
+            TraverseUnchecked(graph, sources, colorByVertex, handler, cancellationToken);
+        }
+
+        internal static void TraverseChecked<TGraph, TSourceCollection, THandler>(
+            TGraph graph, TSourceCollection sources, IEqualityComparer<TVertex> comparer, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (sources is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(sources));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            Dictionary<TVertex, Color> colorByVertex = new(comparer);
+            TraverseUnchecked(graph, sources, colorByVertex, handler, cancellationToken);
+        }
+
+        internal static void TraverseChecked<TGraph, TSourceCollection, TColorMap, THandler>(
+            TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TSourceCollection : IEnumerable<TVertex>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph>
+        {
+            if (graph is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(graph));
+
+            if (sources is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(sources));
+
+            if (colorByVertex is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(colorByVertex));
+
+            if (handler is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(handler));
+
+            TraverseUnchecked(graph, sources, colorByVertex, handler, cancellationToken);
+        }
+
         private static void TraverseUnchecked<TGraph, TSourceCollection, TColorMap, THandler>(
             TGraph graph, TSourceCollection sources, TColorMap colorByVertex, THandler handler,
             CancellationToken cancellationToken)
