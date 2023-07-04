73ee846bd0a217bf9b0381970fd3a7356fd5aed4
Viktor Ptitselov <qbit86@gmail.com>
2023-07-05 02:17:39 +0300

    [Traversal] Implement private Adjacency.EagerBfs<>.Traverse<>()

--- src/Arborescence.Traversal/Adjacency.EagerBfs/EagerBfs_2.cs
+++ src/Arborescence.Traversal/Adjacency.EagerBfs/EagerBfs_2.cs
@@ -1,6 +1,9 @@
 namespace Arborescence.Traversal.Adjacency
 {
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
 
     /// <summary>
     /// Represents the BFS algorithm — breadth-first traversal of the graph.
@@ -9,5 +12,67 @@ namespace Arborescence.Traversal.Adjacency
     /// <typeparam name="TVertexEnumerator">The type of the vertex enumerator.</typeparam>
     public static partial class EagerBfs<TVertex, TVertexEnumerator>
         where TVertex : notnull
-        where TVertexEnumerator : IEnumerator<TVertex> { }
+        where TVertexEnumerator : IEnumerator<TVertex>
+    {
+        private static void Traverse<TGraph, TColorMap, THandler>(
+            TGraph graph, ref ValueQueue<TVertex> queue, TColorMap colorByVertex, THandler handler,
+            CancellationToken cancellationToken)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TVertexEnumerator>
+            where TColorMap : IDictionary<TVertex, Color>
+            where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph>
+        {
+            while (queue.TryTake(out TVertex current))
+            {
+#if DEBUG
+                Debug.Assert(GetColorOrDefault(colorByVertex, current) != default);
+#endif
+                handler.OnExamineVertex(graph, current);
+                TVertexEnumerator outNeighbors = graph.EnumerateOutNeighbors(current);
+                try
+                {
+                    while (outNeighbors.MoveNext())
+                    {
+                        TVertex neighbor = outNeighbors.Current!;
+                        var edge = Endpoints.Create(current, neighbor);
+                        handler.OnExamineEdge(graph, edge);
+                        Color neighborColor = GetColorOrDefault(colorByVertex, neighbor);
+                        switch (neighborColor)
+                        {
+                            case Color.None or Color.White:
+                                handler.OnTreeEdge(graph, edge);
+                                colorByVertex[neighbor] = Color.Gray;
+                                handler.OnDiscoverVertex(graph, neighbor);
+                                if (cancellationToken.IsCancellationRequested)
+                                {
+                                    colorByVertex[current] = Color.Black;
+                                    handler.OnFinishVertex(graph, current);
+                                    return;
+                                }
+
+                                queue.Add(neighbor);
+                                break;
+                            case Color.Gray:
+                                handler.OnNonTreeGrayHeadEdge(graph, edge);
+                                break;
+                            default:
+                                handler.OnNonTreeBlackHeadEdge(graph, edge);
+                                break;
+                        }
+                    }
+                }
+                finally
+                {
+                    outNeighbors.Dispose();
+                }
+
+                colorByVertex[current] = Color.Black;
+                handler.OnFinishVertex(graph, current);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Color GetColorOrDefault<TColorMap>(TColorMap colorByVertex, TVertex vertex)
+            where TColorMap : IDictionary<TVertex, Color> =>
+            colorByVertex.TryGetValue(vertex, out Color result) ? result : Color.None;
+    }
 }
