52219980f261bdd6da694b253b04bb779d47e820
Viktor Ptitselov <qbit86@gmail.com>
2023-07-05 12:01:51 +0300

    Add ArgumentOutOfRangeExceptionHelpers shared project

---
+++ src/Shared/ArgumentOutOfRangeExceptionHelpers/ArgumentOutOfRangeExceptionHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace Arborescence
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    internal static class ArgumentOutOfRangeExceptionHelpers
+    {
+        [DoesNotReturn]
+        internal static void Throw(string? paramName) => throw new ArgumentOutOfRangeException(paramName);
+    }
+}
