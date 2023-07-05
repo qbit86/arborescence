7f48a18ca3dd67a0f3ba9ba75668688a1c81c3a2
Viktor Ptitselov <qbit86@gmail.com>
2023-07-05 09:32:31 +0300

    [Traversal] Add XML-doc comments for EagerBfs_2.SingleSource

--- src/Arborescence.Traversal/Adjacency.EagerBfs/EagerBfs_2.SingleSource.cs
+++ src/Arborescence.Traversal/Adjacency.EagerBfs/EagerBfs_2.SingleSource.cs
@@ -6,6 +6,24 @@ namespace Arborescence.Traversal.Adjacency
 
     public static partial class EagerBfs<TVertex, TVertexEnumerator>
     {
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source
+        /// until the search tree is built or until the search is canceled.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Traverse<TGraph, THandler>(
             TGraph graph, TVertex source, THandler handler,
@@ -14,6 +32,25 @@ namespace Arborescence.Traversal.Adjacency
             where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph> =>
             TraverseChecked(graph, source, handler, cancellationToken);
 
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source
+        /// until the search tree is built or until the search is canceled.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing vertices.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Traverse<TGraph, THandler>(
             TGraph graph, TVertex source, IEqualityComparer<TVertex> comparer, THandler handler,
@@ -22,6 +59,27 @@ namespace Arborescence.Traversal.Adjacency
             where THandler : IBfsHandler<TVertex, Endpoints<TVertex>, TGraph> =>
             TraverseChecked(graph, source, comparer, handler, cancellationToken);
 
+        /// <summary>
+        /// Traverses the graph in a breadth-first order starting from the single source
+        /// until the search tree is built or until the search is canceled.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="colorByVertex">The vertex color map.</param>
+        /// <param name="handler">
+        /// The <see cref="IBfsHandler{TGraph,TVertex,TEdge}"/> implementation to use
+        /// for the actions taken during the graph traversal.
+        /// </param>
+        /// <param name="cancellationToken">Optional token used to stop the traversal.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TColorMap">The type of the vertex color map.</typeparam>
+        /// <typeparam name="THandler">The type of the events handler.</typeparam>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="source"/> is <see langword="null"/>,
+        /// or <paramref name="colorByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="handler"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Traverse<TGraph, TColorMap, THandler>(
             TGraph graph, TVertex source, TColorMap colorByVertex, THandler handler,
