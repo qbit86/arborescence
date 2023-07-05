c98a6b0ce9fa887826eb65aa99741649aefd0fcc
Viktor Ptitselov <qbit86@gmail.com>
2023-07-05 10:02:15 +0300

    Add ArgumentNullExceptionHelpers shared project

---
+++ src/Shared/ArgumentNullExceptionHelpers/ArgumentNullExceptionHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace Arborescence
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    internal static class ArgumentNullExceptionHelpers
+    {
+        [DoesNotReturn]
+        internal static void Throw(string? paramName) => throw new ArgumentNullException(paramName);
+    }
+}
