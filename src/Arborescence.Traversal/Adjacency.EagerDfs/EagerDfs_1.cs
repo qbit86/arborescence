6a8f3b10b952c04adeda4325529916a08eafae6c
Viktor Ptitselov <qbit86@gmail.com>
2023-07-06 10:56:10 +0300

    [Traversal] Add Adjacency.EagerDfs_1.SingleSource

---
+++ src/Arborescence.Traversal/Adjacency.EagerDfs/EagerDfs_1.cs
@@ -0,0 +1,8 @@
+namespace Arborescence.Traversal.Adjacency
+{
+    /// <summary>
+    /// Represents the DFS algorithm — depth-first traversal of the graph in a non-recursive manner.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    public static partial class EagerDfs<TVertex> where TVertex : notnull { }
+}
