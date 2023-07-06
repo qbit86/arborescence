21109781e6e03025ba1f750283512754e1322121
Viktor Ptitselov <qbit86@gmail.com>
2023-07-06 12:38:29 +0300

    [Traversal] Add Adjacency.RecursiveDfs_1

---
+++ src/Arborescence.Traversal/Adjacency.RecursiveDfs/RecursiveDfs_1.cs
@@ -0,0 +1,8 @@
+namespace Arborescence.Traversal.Adjacency
+{
+    /// <summary>
+    /// Represents the DFS algorithm — depth-first traversal of the graph in a recursive manner using the program stack.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    public static partial class RecursiveDfs<TVertex> where TVertex : notnull { }
+}
