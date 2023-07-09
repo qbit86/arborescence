a8830c6add47d05d7932b61a40c7eda478d43cea
Viktor Ptitselov <qbit86@gmail.com>
2023-07-09 20:54:31 +0300

    [Traversal.Specialized] Add `Incidence.EnumerableBfs<TEdge, TEdgeEnumerator>.EnumerateEdges<>()`

--- src/Arborescence.Traversal.Specialized/Incidence.EnumerableBfs/EnumerableBfs_2.cs
+++ src/Arborescence.Traversal.Specialized/Incidence.EnumerableBfs/EnumerableBfs_2.cs
@@ -3,13 +3,21 @@ namespace Arborescence.Traversal.Specialized.Incidence
     using System;
     using System.Buffers;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public static class EnumerableBfs<TEdge, TEdgeEnumerator>
         where TEdgeEnumerator : IEnumerator<TEdge>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<int> EnumerateVertices<TGraph>(TGraph graph, int source, int vertexCount)
             where TGraph : IHeadIncidence<int, TEdge>, IOutEdgesIncidence<int, TEdgeEnumerator> =>
             EnumerateVerticesChecked(graph, source, vertexCount);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<TEdge> EnumerateEdges<TGraph>(
+            TGraph graph, int source, int vertexCount)
+            where TGraph : IHeadIncidence<int, TEdge>, IOutEdgesIncidence<int, TEdgeEnumerator> =>
+            EnumerateEdgesChecked(graph, source, vertexCount);
 
         internal static IEnumerable<int> EnumerateVerticesChecked<TGraph>(TGraph graph, int source, int vertexCount)
             where TGraph : IHeadIncidence<int, TEdge>, IOutEdgesIncidence<int, TEdgeEnumerator>
@@ -24,6 +32,26 @@ namespace Arborescence.Traversal.Specialized.Incidence
                 Int32Set exploredSet = new(arrayFromPool);
                 return Arborescence.Traversal.Incidence.EnumerableBfs<int, TEdge, TEdgeEnumerator>
                     .EnumerateVerticesIterator(graph, source, exploredSet);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(arrayFromPool);
+            }
+        }
+
+        internal static IEnumerable<TEdge> EnumerateEdgesChecked<TGraph>(TGraph graph, int source, int vertexCount)
+            where TGraph : IHeadIncidence<int, TEdge>, IOutEdgesIncidence<int, TEdgeEnumerator>
+        {
+            if (graph is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(graph));
+
+            byte[] arrayFromPool = ArrayPool<byte>.Shared.Rent(vertexCount);
+            Array.Clear(arrayFromPool, 0, vertexCount);
+            try
+            {
+                Int32Set exploredSet = new(arrayFromPool);
+                return Arborescence.Traversal.Incidence.EnumerableBfs<int, TEdge, TEdgeEnumerator>
+                    .EnumerateEdgesIterator(graph, source, exploredSet);
             }
             finally
             {
