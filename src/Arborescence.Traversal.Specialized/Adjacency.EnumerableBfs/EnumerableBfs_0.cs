d520f421ed1f283f5eb1bc5bdab004b67d9a88c8
Viktor Ptitselov <qbit86@gmail.com>
2023-07-11 02:44:31 +0300

    [Traversal.Specialized] Add `Arborescence.Traversal.Specialized.Adjacency.EnumerableBfs_0`

---
+++ src/Arborescence.Traversal.Specialized/Adjacency.EnumerableBfs/EnumerableBfs_0.cs
@@ -0,0 +1,32 @@
+namespace Arborescence.Traversal.Specialized.Adjacency
+{
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    public static class EnumerableBfs
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<int> EnumerateVertices<TGraph>(TGraph graph, int source, int vertexCount)
+            where TGraph : IOutNeighborsAdjacency<int, IEnumerator<int>> =>
+            EnumerableBfs<IEnumerator<int>>.EnumerateVerticesChecked(graph, source, vertexCount);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<int> EnumerateVertices<TGraph, TSourceCollection>(
+            TGraph graph, TSourceCollection sources, int vertexCount)
+            where TGraph : IOutNeighborsAdjacency<int, IEnumerator<int>>
+            where TSourceCollection : IEnumerable<int> =>
+            EnumerableBfs<IEnumerator<int>>.EnumerateVerticesChecked(graph, sources, vertexCount);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<Endpoints<int>> EnumerateEdges<TGraph>(TGraph graph, int source, int vertexCount)
+            where TGraph : IOutNeighborsAdjacency<int, IEnumerator<int>> =>
+            EnumerableBfs<IEnumerator<int>>.EnumerateEdgesChecked(graph, source, vertexCount);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<Endpoints<int>> EnumerateEdges<TGraph, TSourceCollection>(
+            TGraph graph, TSourceCollection sources, int vertexCount)
+            where TGraph : IOutNeighborsAdjacency<int, IEnumerator<int>>
+            where TSourceCollection : IEnumerable<int> =>
+            EnumerableBfs<IEnumerator<int>>.EnumerateEdgesChecked(graph, sources, vertexCount);
+    }
+}
