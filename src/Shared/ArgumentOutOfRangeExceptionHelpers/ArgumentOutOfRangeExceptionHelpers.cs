145278f02d2cb8b8ed3422188703a497142be784
Viktor Ptitselov <qbit86@gmail.com>
2023-07-13 22:42:31 +0300

    Add ArgumentOutOfRangeExceptionHelpers.ThrowNegative<T>()

--- src/Shared/ArgumentOutOfRangeExceptionHelpers/ArgumentOutOfRangeExceptionHelpers.cs
+++ src/Shared/ArgumentOutOfRangeExceptionHelpers/ArgumentOutOfRangeExceptionHelpers.cs
@@ -7,5 +7,12 @@
     {
         [DoesNotReturn]
         internal static void Throw(string? paramName) => throw new ArgumentOutOfRangeException(paramName);
+
+        [DoesNotReturn]
+        internal static void ThrowNegative<T>(string? paramName, T value)
+        {
+            string message = $"{paramName} ('{value}') must be a non-negative value.";
+            throw new ArgumentOutOfRangeException(paramName, value, message);
+        }
     }
 }
