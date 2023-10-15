f5aecde0a658e7fd38e5506912b7a11305054085
Viktor Ptitselov <qbit86@gmail.com>
2023-10-15 11:30:29 +0300

    [Search] Add EagerDijkstra`2

---
+++ src/Arborescence.Search/Adjacency.EagerDijkstra/EagerDijkstra_2.cs
@@ -0,0 +1,8 @@
+namespace Arborescence.Search.Adjacency
+{
+    using System.Collections.Generic;
+
+    public static class EagerDijkstra<TVertex, TNeighborEnumerator>
+        where TVertex : notnull
+        where TNeighborEnumerator : IEnumerator<TVertex> { }
+}
