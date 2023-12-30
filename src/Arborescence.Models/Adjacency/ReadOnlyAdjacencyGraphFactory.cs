8f8a26c291856ee7733c62f5c38646e5cb6c416d
Viktor Ptitselov <qbit86@gmail.com>
2023-12-30 23:51:35 +0200

    [Models] Add ReadOnlyAdjacencyGraphFactory<TVertex>.Create<>() overload for List<TVertex>

--- src/Arborescence.Models/Adjacency/ReadOnlyAdjacencyGraphFactory.cs
+++ src/Arborescence.Models/Adjacency/ReadOnlyAdjacencyGraphFactory.cs
@@ -2,6 +2,31 @@ namespace Arborescence.Models
 {
     using System.Collections.Generic;
 
+    public static class ReadOnlyAdjacencyGraphFactory<TVertex>
+    {
+        public static ReadOnlyAdjacencyGraph<
+                TVertex,
+                List<TVertex>.Enumerator,
+                TVertexMultimap,
+                ReadOnlyMultimapConcept<
+                    TVertexMultimap,
+                    TVertex,
+                    List<TVertex>,
+                    List<TVertex>.Enumerator,
+                    ListEnumeratorProvider<TVertex>>>
+            Create<TVertexMultimap>(TVertexMultimap neighborsByVertex)
+            where TVertexMultimap : IReadOnlyDictionary<TVertex, List<TVertex>>
+        {
+            if (neighborsByVertex is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(neighborsByVertex));
+
+            ReadOnlyMultimapConcept<
+                    TVertexMultimap, TVertex, List<TVertex>, List<TVertex>.Enumerator, ListEnumeratorProvider<TVertex>>
+                vertexMultimapConcept = new(default);
+            return new(neighborsByVertex, vertexMultimapConcept);
+        }
+    }
+
     /// <summary>
     /// Provides support for creating
     /// <see cref="ReadOnlyAdjacencyGraph{TVertex,TNeighborEnumerator,TVertexMultimap,TVertexMultimapConcept}"/>
