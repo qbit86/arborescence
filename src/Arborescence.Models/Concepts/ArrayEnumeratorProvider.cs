60991712745638692229b6b55ab6f8bb2ce787ce
Viktor Ptitselov <qbit86@gmail.com>
2023-12-31 20:57:41 +0200

    [Models] Add XML-doc comments for Array*EnumeratorProvider<T>

--- src/Arborescence.Models/Concepts/ArrayEnumeratorProvider.cs
+++ src/Arborescence.Models/Concepts/ArrayEnumeratorProvider.cs
@@ -4,11 +4,18 @@ namespace Arborescence.Models
     using System;
     using System.Runtime.CompilerServices;
 
+
+    /// <summary>
+    /// Provides an enumerator over the <see cref="T:T[]"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the array.</typeparam>
     public readonly struct ArrayEnumeratorProvider<T> : IEnumeratorProvider<T[], ArraySegment<T>.Enumerator>
     {
+        /// <inheritdoc/>
         public ArraySegment<T>.Enumerator GetEnumerator(T[] collection) =>
             collection is not null ? new ArraySegment<T>(collection).GetEnumerator() : GetEmptyEnumerator();
 
+        /// <inheritdoc/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArraySegment<T>.Enumerator GetEmptyEnumerator() => ArraySegment<T>.Empty.GetEnumerator();
     }
--- src/Arborescence.Models/Concepts/ArraySegmentEnumeratorProvider.cs
+++ src/Arborescence.Models/Concepts/ArraySegmentEnumeratorProvider.cs
@@ -4,12 +4,18 @@ namespace Arborescence.Models
     using System;
     using System.Runtime.CompilerServices;
 
+    /// <summary>
+    /// Provides an enumerator over the <see cref="ArraySegment{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the array segment.</typeparam>
     public readonly struct ArraySegmentEnumeratorProvider<T> :
         IEnumeratorProvider<ArraySegment<T>, ArraySegment<T>.Enumerator>
     {
+        /// <inheritdoc/>
         public ArraySegment<T>.Enumerator GetEnumerator(ArraySegment<T> collection) =>
             collection.Array is not null ? collection.GetEnumerator() : GetEmptyEnumerator();
 
+        /// <inheritdoc/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArraySegment<T>.Enumerator GetEmptyEnumerator() => ArraySegment<T>.Empty.GetEnumerator();
     }
