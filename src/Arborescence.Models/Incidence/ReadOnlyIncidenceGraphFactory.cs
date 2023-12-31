5c911a6cffa6d7cd98e840d4504d9252dec93ca6
Viktor Ptitselov <qbit86@gmail.com>
2023-12-31 09:45:31 +0200

    [Models] Add ReadOnlyIncidenceGraphFactory<>.FromLists<>() method skeleton

--- src/Arborescence.Models/Incidence/ReadOnlyIncidenceGraphFactory.cs
+++ src/Arborescence.Models/Incidence/ReadOnlyIncidenceGraphFactory.cs
@@ -1,6 +1,37 @@
 namespace Arborescence.Models
 {
+    using System;
     using System.Collections.Generic;
+
+    public static class ReadOnlyIncidenceGraphFactory<TVertex, TEdge>
+    {
+        public static ReadOnlyIncidenceGraph<
+                TVertex,
+                TEdge,
+                List<TEdge>.Enumerator,
+                TEndpointMap,
+                TEdgeMultimap,
+                ReadOnlyMultimapConcept<
+                    TEdgeMultimap,
+                    TVertex,
+                    List<TEdge>,
+                    List<TEdge>.Enumerator,
+                    ListEnumeratorProvider<TEdge>>>
+            FromLists<TEndpointMap, TEdgeMultimap>(
+                TEndpointMap tailByEdge, TEndpointMap headByEdge, TEdgeMultimap outEdgesByVertex)
+            where TEndpointMap : IReadOnlyDictionary<TEdge, TVertex>
+            where TEdgeMultimap : IReadOnlyDictionary<TVertex, List<TEdge>>
+        {
+            if (tailByEdge is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(tailByEdge));
+            if (headByEdge is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(headByEdge));
+            if (outEdgesByVertex is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(outEdgesByVertex));
+
+            throw new NotImplementedException();
+        }
+    }
 
     /// <summary>
     /// Provides support for creating
