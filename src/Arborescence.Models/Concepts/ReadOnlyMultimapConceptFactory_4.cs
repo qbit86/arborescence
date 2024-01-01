4198a0902c1f9af6e308549a047ec033de854e70
Viktor Ptitselov <qbit86@gmail.com>
2024-01-02 01:40:31 +0200

    [Models] Move ReadOnlyMultimapConceptFactory<> to separate file

--- src/Arborescence.Models/Concepts/ReadOnlyMultimapConceptFactory_4.cs
+++ src/Arborescence.Models/Concepts/ReadOnlyMultimapConceptFactory_4.cs
@@ -32,32 +32,4 @@ namespace Arborescence.Models
             return new(enumeratorProvider);
         }
     }
-
-    /// <summary>
-    /// Provides basic read-only methods for a generic multimap.
-    /// </summary>
-    /// <typeparam name="TMultimap">The type of the dictionary.</typeparam>
-    /// <typeparam name="TKey">The type of keys.</typeparam>
-    /// <typeparam name="TValueCollection">The type of the value collection.</typeparam>
-    /// <typeparam name="TValueEnumerator">The type of the value enumerator.</typeparam>
-    /// <typeparam name="TEnumeratorProvider">The type of the enumerator provider.</typeparam>
-    public readonly struct ReadOnlyMultimapConcept<
-        TMultimap, TKey, TValueCollection, TValueEnumerator, TEnumeratorProvider> :
-        IReadOnlyMultimapConcept<TMultimap, TKey, TValueEnumerator>
-        where TMultimap : IReadOnlyDictionary<TKey, TValueCollection>
-        where TEnumeratorProvider : IEnumeratorProvider<TValueCollection, TValueEnumerator>
-    {
-        private readonly TEnumeratorProvider _enumeratorProvider;
-
-        internal ReadOnlyMultimapConcept(TEnumeratorProvider enumeratorProvider) =>
-            _enumeratorProvider = enumeratorProvider;
-
-        /// <inheritdoc/>
-        public TValueEnumerator EnumerateValues(TMultimap multimap, TKey key) =>
-            ReadOnlyMultimapHelpers<TValueCollection, TValueEnumerator>.GetEnumerator(
-                multimap, key, _enumeratorProvider);
-
-        /// <inheritdoc/>
-        public int GetCount(TMultimap multimap) => multimap.Count;
-    }
 }
