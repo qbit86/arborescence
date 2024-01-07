9d26d0a5c147b26c79050e60108c9fc09260df57
Viktor Ptitselov <qbit86@gmail.com>
2024-01-07 12:09:01 +0200

    [Search] Add partial class AdditiveEnumerableDijkstra<>

--- src/Arborescence.Search/Adjacency.EnumerableDijkstra/AdditiveEnumerableDijkstra_3.cs
+++ src/Arborescence.Search/Adjacency.EnumerableDijkstra/AdditiveEnumerableDijkstra_3.cs
@@ -4,54 +4,9 @@ namespace Arborescence.Search.Adjacency
     using System.Collections.Generic;
     using System.Numerics;
 
-    public static class AdditiveEnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>
+    public static partial class AdditiveEnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>
         where TVertex : notnull
         where TNeighborEnumerator : IEnumerator<TVertex>
-        where TWeight : IAdditionOperators<TWeight, TWeight, TWeight>, IAdditiveIdentity<TWeight, TWeight>
-    {
-        // ReSharper disable once UnusedMember.Local
-        private static IEnumerable<Endpoints<TVertex>> EnumerateEdgesChecked<TGraph, TWeightMap>(
-            TGraph graph, TVertex source, TWeightMap weightByEdge)
-            where TGraph : IOutNeighborsAdjacency<TVertex, TNeighborEnumerator>
-            where TWeightMap : IReadOnlyDictionary<Endpoints<TVertex>, TWeight>
-        {
-            if (graph is null)
-                ArgumentNullExceptionHelpers.Throw(nameof(graph));
-
-            if (weightByEdge is null)
-                ArgumentNullExceptionHelpers.Throw(nameof(graph));
-
-            PriorityQueue<TVertex, TWeight> frontier = new();
-            Dictionary<TVertex, TWeight> distanceByVertex = new();
-            AdditiveMonoid<TWeight> weightMonoid = default;
-            Comparer<TWeight> weightComparer = Comparer<TWeight>.Default;
-            return EnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>.EnumerateEdgesIterator(
-                graph, source, weightByEdge, frontier, distanceByVertex, weightMonoid, weightComparer);
-        }
-
-        // ReSharper disable once UnusedMember.Local
-        private static IEnumerable<Endpoints<TVertex>> EnumerateEdgesChecked<
-            TGraph, TWeightMap, TWeightComparer>(
-            TGraph graph, TVertex source, TWeightMap weightByEdge, TWeightComparer weightComparer)
-            where TGraph : IOutNeighborsAdjacency<TVertex, TNeighborEnumerator>
-            where TWeightMap : IReadOnlyDictionary<Endpoints<TVertex>, TWeight>
-            where TWeightComparer : IComparer<TWeight>
-        {
-            if (graph is null)
-                ArgumentNullExceptionHelpers.Throw(nameof(graph));
-
-            if (weightByEdge is null)
-                ArgumentNullExceptionHelpers.Throw(nameof(graph));
-
-            if (weightComparer is null)
-                ArgumentNullExceptionHelpers.Throw(nameof(graph));
-
-            PriorityQueue<TVertex, TWeight> frontier = new();
-            Dictionary<TVertex, TWeight> distanceByVertex = new();
-            AdditiveMonoid<TWeight> weightMonoid = default;
-            return EnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>.EnumerateEdgesIterator(
-                graph, source, weightByEdge, frontier, distanceByVertex, weightMonoid, weightComparer);
-        }
-    }
+        where TWeight : IAdditionOperators<TWeight, TWeight, TWeight>, IAdditiveIdentity<TWeight, TWeight> { }
 }
 #endif
