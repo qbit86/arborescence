46574d54cd2e93d19a1a4c0130671971e6d3f10b
Viktor Ptitselov <qbit86@gmail.com>
2024-01-07 11:42:51 +0200

    [Search] Add EnumerableDijkstra<>.EnumerateEdgesChecked<>

--- src/Arborescence.Search/Adjacency.EnumerableDijkstra/EnumerableDijkstra_3.EnumerateEdges.cs
+++ src/Arborescence.Search/Adjacency.EnumerableDijkstra/EnumerableDijkstra_3.EnumerateEdges.cs
@@ -8,10 +8,38 @@ namespace Arborescence.Search.Adjacency
     public static partial class EnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>
     {
         // ReSharper disable once UnusedMember.Local
+        private static IEnumerable<Endpoints<TVertex>> EnumerateEdgesChecked<
+            TGraph, TWeightMap, TWeightMonoid, TWeightComparer>(
+            TGraph graph, TVertex source, TWeightMap weightByEdge,
+            TWeightMonoid weightMonoid, TWeightComparer weightComparer)
+            where TGraph : IOutNeighborsAdjacency<TVertex, TNeighborEnumerator>
+            where TWeightMap : IReadOnlyDictionary<Endpoints<TVertex>, TWeight>
+            where TWeightMonoid : IMonoid<TWeight>
+            where TWeightComparer : IComparer<TWeight>
+        {
+            if (graph is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(graph));
+
+            if (weightByEdge is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(graph));
+
+            if (weightMonoid is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(graph));
+
+            if (weightComparer is null)
+                ArgumentNullExceptionHelpers.Throw(nameof(graph));
+
+            PriorityQueue<TVertex, TWeight> frontier = new();
+            Dictionary<TVertex, TWeight> distanceByVertex = new();
+            return EnumerateEdgesIterator(
+                graph, source, weightByEdge, frontier, distanceByVertex, weightMonoid, weightComparer);
+        }
+
         private static IEnumerable<Endpoints<TVertex>> EnumerateEdgesIterator<
             TGraph, TWeightMap, TDistanceMap, TWeightMonoid, TWeightComparer>(
-            TGraph graph, TVertex source, PriorityQueue<TVertex, TWeight> frontier, TWeightMap weightByEdge,
-            TDistanceMap distanceByVertex, TWeightMonoid weightMonoid, TWeightComparer weightComparer)
+            TGraph graph, TVertex source, TWeightMap weightByEdge,
+            PriorityQueue<TVertex, TWeight> frontier, TDistanceMap distanceByVertex,
+            TWeightMonoid weightMonoid, TWeightComparer weightComparer)
             where TGraph : IOutNeighborsAdjacency<TVertex, TNeighborEnumerator>
             where TWeightMap : IReadOnlyDictionary<Endpoints<TVertex>, TWeight>
             where TDistanceMap : IDictionary<TVertex, TWeight>
@@ -24,7 +52,7 @@ namespace Arborescence.Search.Adjacency
             while (frontier.TryDequeue(out TVertex? current, out TWeight? priority))
             {
                 if (!distanceByVertex.TryGetValue(current, out TWeight? currentDistance))
-                    throw new InvalidOperationException();
+                    throw new InvalidOperationException(nameof(distanceByVertex));
                 if (weightComparer.Compare(priority, currentDistance) > 0)
                     continue;
                 TNeighborEnumerator neighbors = graph.EnumerateOutNeighbors(current);
@@ -35,7 +63,7 @@ namespace Arborescence.Search.Adjacency
                         TVertex neighbor = neighbors.Current;
                         Endpoints<TVertex> edge = new(current, neighbor);
                         if (!weightByEdge.TryGetValue(edge, out TWeight? weight))
-                            throw new InvalidOperationException();
+                            throw new InvalidOperationException(nameof(weightByEdge));
                         TWeight neighborDistanceCandidate = weightMonoid.Combine(currentDistance, weight);
                         if (!distanceByVertex.TryGetValue(neighbor, out TWeight? neighborDistance) ||
                             weightComparer.Compare(neighborDistanceCandidate, neighborDistance) < 0)
--- src/Arborescence.Search/Adjacency.EnumerableDijkstra/EnumerableDijkstra_3.cs
+++ src/Arborescence.Search/Adjacency.EnumerableDijkstra/EnumerableDijkstra_3.cs
@@ -3,5 +3,6 @@ namespace Arborescence.Search.Adjacency
     using System.Collections.Generic;
 
     public static partial class EnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>
+        where TVertex : notnull
         where TNeighborEnumerator : IEnumerator<TVertex> { }
 }
