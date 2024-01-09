92cbadde820b26bf65b52eaeaad5adba917d6aad
Viktor Ptitselov <qbit86@gmail.com>
2024-01-09 22:54:34 +0200

    [Search] Add XML-doc comments on `EnumerableDijkstra<>`

--- src/Arborescence.Search/Adjacency.EnumerableDijkstra/EnumerableDijkstra_3.cs
+++ src/Arborescence.Search/Adjacency.EnumerableDijkstra/EnumerableDijkstra_3.cs
@@ -2,6 +2,12 @@ namespace Arborescence.Search.Adjacency
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Represents Dijkstra's algorithm for solving the single source shortest paths problem.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of the vertex.</typeparam>
+    /// <typeparam name="TNeighborEnumerator">The type of the neighbor enumerator.</typeparam>
+    /// <typeparam name="TWeight">The type of edge weight.</typeparam>
     public static partial class EnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>
         where TVertex : notnull
         where TNeighborEnumerator : IEnumerator<TVertex> { }
