406dc56aeeb9878f1bf8aa8480188bf42de2d970
Viktor Ptitselov <qbit86@gmail.com>
2025-01-01 20:57:57 +0200

    [Search] Add XML-doc comments for AdditiveEnumerableDijkstra<>.EnumerateEdges<>() method overloads

--- src/Arborescence.Search/Adjacency.EnumerableDijkstra/AdditiveEnumerableDijkstra_3.EnumerateEdges.cs
+++ src/Arborescence.Search/Adjacency.EnumerableDijkstra/AdditiveEnumerableDijkstra_3.EnumerateEdges.cs
@@ -6,6 +6,19 @@ namespace Arborescence.Search.Adjacency
 
     public static partial class AdditiveEnumerableDijkstra<TVertex, TNeighborEnumerator, TWeight>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in an order defined by the Dijkstra algorithm, starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="weightByEdge">The weight of each edge in the graph.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TWeightMap">The type of the weight map.</typeparam>
+        /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="weightByEdge"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Endpoints<TVertex>> EnumerateEdges<TGraph, TWeightMap>(
             TGraph graph, TVertex source, TWeightMap weightByEdge)
@@ -13,6 +26,22 @@ namespace Arborescence.Search.Adjacency
             where TWeightMap : IReadOnlyDictionary<Endpoints<TVertex>, TWeight> =>
             EnumerateEdgesChecked(graph, source, weightByEdge);
 
+        /// <summary>
+        /// Enumerates edges of the graph in an order defined by the Dijkstra algorithm, starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="weightByEdge">The weight of each edge in the graph.</param>
+        /// <param name="distanceByVertex">The distance to each discovered vertex.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TWeightMap">The type of the weight map.</typeparam>
+        /// <typeparam name="TDistanceMap">The type of the distance map.</typeparam>
+        /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="weightByEdge"/> is <see langword="null"/>,
+        /// or <paramref name="distanceByVertex"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Endpoints<TVertex>> EnumerateEdges<TGraph, TWeightMap, TDistanceMap>(
             TGraph graph, TVertex source, TWeightMap weightByEdge, TDistanceMap distanceByVertex)
@@ -21,6 +50,25 @@ namespace Arborescence.Search.Adjacency
             where TDistanceMap : IDictionary<TVertex, TWeight> =>
             EnumerateEdgesChecked(graph, source, weightByEdge, distanceByVertex);
 
+        /// <summary>
+        /// Enumerates edges of the graph in an order defined by the Dijkstra algorithm, starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="weightByEdge">The weight of each edge in the graph.</param>
+        /// <param name="distanceByVertex">The distance to each discovered vertex.</param>
+        /// <param name="weightComparer">The weight comparer.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TWeightMap">The type of the weight map.</typeparam>
+        /// <typeparam name="TDistanceMap">The type of the distance map.</typeparam>
+        /// <typeparam name="TWeightComparer">The type of the weight comparer.</typeparam>
+        /// <returns>An enumerable collection of the endpoints of a search tree edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="weightByEdge"/> is <see langword="null"/>,
+        /// or <paramref name="distanceByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="weightComparer"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Endpoints<TVertex>> EnumerateEdges<
             TGraph, TWeightMap, TDistanceMap, TWeightComparer>(
