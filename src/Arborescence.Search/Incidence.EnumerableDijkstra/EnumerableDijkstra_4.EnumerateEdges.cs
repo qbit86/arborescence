94ca0f4936b4e2969434c12b6d499cd8544973b8
Viktor Ptitselov <qbit86@gmail.com>
2025-01-03 10:54:29 +0200

    [Search] Add XML-doc comments for Arborescence.Search.Incidence.EnumerableDijkstra<>.EnumerateEdges<>() method overloads

--- src/Arborescence.Search/Incidence.EnumerableDijkstra/EnumerableDijkstra_4.EnumerateEdges.cs
+++ src/Arborescence.Search/Incidence.EnumerableDijkstra/EnumerableDijkstra_4.EnumerateEdges.cs
@@ -4,6 +4,22 @@ namespace Arborescence.Search.Incidence
 
     public static partial class EnumerableDijkstra<TVertex, TEdge, TEdgeEnumerator, TWeight>
     {
+        /// <summary>
+        /// Enumerates edges of the graph in an order defined by the Dijkstra algorithm, starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="weightByEdge">The weight of each edge in the graph.</param>
+        /// <param name="weightMonoid">The weight monoid.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TWeightMap">The type of the weight map.</typeparam>
+        /// <typeparam name="TWeightMonoid">The type of the weight monoid.</typeparam>
+        /// <returns>An enumerable collection of the edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="weightByEdge"/> is <see langword="null"/>,
+        /// or <paramref name="weightMonoid"/> is <see langword="null"/>.
+        /// </exception>
         public static IEnumerable<TEdge> EnumerateEdges<
             TGraph, TWeightMap, TWeightMonoid>(
             TGraph graph, TVertex source, TWeightMap weightByEdge,
@@ -13,6 +29,25 @@ namespace Arborescence.Search.Incidence
             where TWeightMonoid : IMonoid<TWeight> =>
             EnumerateEdgesChecked(graph, source, weightByEdge, weightMonoid);
 
+        /// <summary>
+        /// Enumerates edges of the graph in an order defined by the Dijkstra algorithm, starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="weightByEdge">The weight of each edge in the graph.</param>
+        /// <param name="distanceByVertex">The distance to each discovered vertex.</param>
+        /// <param name="weightMonoid">The weight monoid.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TWeightMap">The type of the weight map.</typeparam>
+        /// <typeparam name="TDistanceMap">The type of the distance map.</typeparam>
+        /// <typeparam name="TWeightMonoid">The type of the weight monoid.</typeparam>
+        /// <returns>An enumerable collection of the edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="weightByEdge"/> is <see langword="null"/>,
+        /// or <paramref name="distanceByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="weightMonoid"/> is <see langword="null"/>.
+        /// </exception>
         public static IEnumerable<TEdge> EnumerateEdges<
             TGraph, TWeightMap, TDistanceMap, TWeightMonoid>(
             TGraph graph, TVertex source, TWeightMap weightByEdge, TDistanceMap distanceByVertex,
@@ -23,6 +58,28 @@ namespace Arborescence.Search.Incidence
             where TWeightMonoid : IMonoid<TWeight> =>
             EnumerateEdgesChecked(graph, source, weightByEdge, distanceByVertex, weightMonoid);
 
+        /// <summary>
+        /// Enumerates edges of the graph in an order defined by the Dijkstra algorithm, starting from the single source.
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="weightByEdge">The weight of each edge in the graph.</param>
+        /// <param name="distanceByVertex">The distance to each discovered vertex.</param>
+        /// <param name="weightMonoid">The weight monoid.</param>
+        /// <param name="weightComparer">The weight comparer.</param>
+        /// <typeparam name="TGraph">The type of the graph.</typeparam>
+        /// <typeparam name="TWeightMap">The type of the weight map.</typeparam>
+        /// <typeparam name="TDistanceMap">The type of the distance map.</typeparam>
+        /// <typeparam name="TWeightMonoid">The type of the weight monoid.</typeparam>
+        /// <typeparam name="TWeightComparer">The type of the weight comparer.</typeparam>
+        /// <returns>An enumerable collection of the edges.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="graph"/> is <see langword="null"/>,
+        /// or <paramref name="weightByEdge"/> is <see langword="null"/>,
+        /// or <paramref name="distanceByVertex"/> is <see langword="null"/>,
+        /// or <paramref name="weightMonoid"/> is <see langword="null"/>,
+        /// or <paramref name="weightComparer"/> is <see langword="null"/>.
+        /// </exception>
         public static IEnumerable<TEdge> EnumerateEdges<
             TGraph, TWeightMap, TDistanceMap, TWeightMonoid, TWeightComparer>(
             TGraph graph, TVertex source, TWeightMap weightByEdge, TDistanceMap distanceByVertex,
